437c37e3dc471d570ad0b73e5b858ea6d84dec10
Andrew Hsieh <andrewhsieh@google.com>
2014-05-07 19:48:31 +0800

    Set HOST_TOOLCHAIN_FOR_CLANG to x86_64-w64-mingw32-4.8 for windows
    Also
    1. Remove unnecessary HOST_C_INCLUDES and HOST_GLOBAL_LD_DIRS
    2. -D__USE_MINGW_ANSI_STDIO to use GNU style print*/scanf*
    3. Force the correct entry point to workaround a bug in binutils
       manifests with -pie
    
    Change-Id: If2c526c5f8b84bdfebd7ee10d1aa601156c548d4

--- core/clang/x86_common.mk
+++ core/clang/x86_common.mk
@@ -50,5 +50,32 @@ endif
 endif
 
 ifeq ($(HOST_OS),windows)
-# nothing required here yet
+CLANG_CONFIG_x86_LINUX_HOST_EXTRA_ASFLAGS :=
+
+CLANG_CONFIG_x86_LINUX_HOST_EXTRA_CFLAGS := \
+  -isystem $(HOST_TOOLCHAIN_FOR_CLANG)/x86_64-w64-mingw32/include
+
+ifneq ($(strip $(BUILD_HOST_64bit)),)
+CLANG_CONFIG_x86_LINUX_HOST_EXTRA_CPPFLAGS := \
+  -isystem $(HOST_TOOLCHAIN_FOR_CLANG)/x86_64-w64-mingw32/include/c++/4.8.2 \
+  -isystem $(HOST_TOOLCHAIN_FOR_CLANG)/x86_64-w64-mingw32/include/c++/4.8.2/x86_64-w64-mingw32 \
+  -isystem $(HOST_TOOLCHAIN_FOR_CLANG)/x86_64-w64-mingw32/include/c++/4.8.2/backward
+
+CLANG_CONFIG_x86_LINUX_HOST_EXTRA_LDFLAGS := \
+  -B$(HOST_TOOLCHAIN_FOR_CLANG)/x86_64-w64-mingw32/bin \
+  -B$(HOST_TOOLCHAIN_FOR_CLANG)/lib/gcc/x86_64-w64-mingw32/4.8.2 \
+  -L$(HOST_TOOLCHAIN_FOR_CLANG)/lib/gcc/x86_64-w64-mingw32/4.8.2 \
+  -L$(HOST_TOOLCHAIN_FOR_CLANG)/x86_64-w64-mingw32/lib64
+else
+CLANG_CONFIG_x86_LINUX_HOST_EXTRA_CPPFLAGS := \
+  -isystem $(HOST_TOOLCHAIN_FOR_CLANG)/x86_64-w64-mingw32/include/c++/4.8.2 \
+  -isystem $(HOST_TOOLCHAIN_FOR_CLANG)/x86_64-w64-mingw32/include/c++/4.8.2/x86_64-w64-mingw32/32 \
+  -isystem $(HOST_TOOLCHAIN_FOR_CLANG)/x86_64-w64-mingw32/include/c++/4.8.2/backward
+
+CLANG_CONFIG_x86_LINUX_HOST_EXTRA_LDFLAGS := \
+  -B$(HOST_TOOLCHAIN_FOR_CLANG)/x86_64-w64-mingw32/bin \
+  -B$(HOST_TOOLCHAIN_FOR_CLANG)/lib/gcc/x86_64-w64-mingw32/4.8.2/32 \
+  -L$(HOST_TOOLCHAIN_FOR_CLANG)/lib/gcc/x86_64-w64-mingw32/4.8.2/32 \
+  -L$(HOST_TOOLCHAIN_FOR_CLANG)/x86_64-w64-mingw32/lib32
+endif
 endif
