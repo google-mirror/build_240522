890ee7f100e78f6105e0fd03fa3718f98127b986
Duane Sand <duane.sand@imgtec.com>
2014-07-22 14:34:00 -0700

    [MIPS] Unite mipsel and mips64el 4.9 gcc toolchains
    Use 4.9 mips64el toolchain for both 64- and 32-bit builds.
    Tell ld when 32-bit links are required.
    Override 4.9's changed defaults for mips floating point
    register use, to get same assembler rules as 4.8 and earlier.
    
    Also: drop unused  soft-fp build targets, cleanout redundant
    compiler options, and remove extraneous Android.mk file.
    
    Change-Id: I86f1075266349edb2b08a7709b9f5472d8cfda32

--- core/combo/arch/mips64/mips64r2.mk
+++ core/combo/arch/mips64/mips64r2.mk
@@ -3,12 +3,6 @@
 ARCH_MIPS_HAS_FPU	:=true
 ARCH_HAVE_ALIGNED_DOUBLES :=true
 arch_variant_cflags := \
-    -EL \
-    -march=mips64r2 \
-    -mtune=mips64r2 \
     -mips64r2 \
-    -mhard-float \
     -msynci
 
-arch_variant_ldflags := \
-    -EL
