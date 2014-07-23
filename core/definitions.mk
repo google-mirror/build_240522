6773cf08e56b00a453d4e77f180d3b1fb427ab96
Dmitriy Ivanov <dimitry@google.com>
2014-07-23 15:27:21 -0700

    Store native libs aligned to PAGE_SIZE
     * Add optional list of files to zipalign
    
       This list may contain entry names as well as
       standard file patters with '*' and '?'s.
    
     * Add variable for jni lib compression and zipalign
    
       - LOCAL_JNI_SHARED_LIBRARIES_ZIP_OPTIONS variable extends
         zip options for shared jni libs.
       - LOCAL_PACKAGE_ALIGNMENT controls package zipalign value (default is 4)
       - LOCAL_JNI_SHARED_LIBRARIES_PACKAGE_ALIGNMENT controls
       native lib zipalign value (default is 4096)
    
    Bug: 8076853
    Change-Id: I1aa2c039bb2a590ae72f256acc9ba5401c2c59b1

--- core/definitions.mk
+++ core/definitions.mk
@@ -1831,7 +1831,7 @@ $(hide) mkdir -p $(addprefix $(dir $@)lib/,$(PRIVATE_JNI_SHARED_LIBRARIES_ABI))
 $(foreach abi,$(PRIVATE_JNI_SHARED_LIBRARIES_ABI),\
   $(call _add-jni-shared-libs-to-package-per-abi,$(abi),\
     $(patsubst $(abi):%,%,$(filter $(abi):%,$(PRIVATE_JNI_SHARED_LIBRARIES)))))
-$(hide) (cd $(dir $@) && zip -r $(notdir $@) lib)
+$(hide) (cd $(dir $@) && zip -r $(PRIVATE_JNI_SHARED_LIBRARIES_ZIP_OPTIONS) $(notdir $@) lib)
 $(hide) rm -rf $(dir $@)lib
 endef
 
@@ -1861,10 +1861,13 @@ endef
 
 # Align STORED entries of a package on 4-byte boundaries
 # to make them easier to mmap.
+# Align STORED native libraries to page-size boundary (0x1000)
 #
 define align-package
 $(hide) mv $@ $@.unaligned
-$(hide) $(ZIPALIGN) -f 4 $@.unaligned $@.aligned
+$(hide) $(ZIPALIGN) -f $(PRIVATE_PACKAGE_ALIGNMENT) $@.unaligned $@.aligned
+$(hide) mv $@.aligned $@.unaligned
+$(hide) $(ZIPALIGN) -f $(PRIVATE_JNI_SHARED_LIBRARIES_PACKAGE_ALIGNMENT) $@.unaligned $@.aligned lib/*/lib*.so
 $(hide) mv $@.aligned $@
 endef
 
