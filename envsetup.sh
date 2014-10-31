4dc6e1f32426c9c8524d5e52a34ebb61bd87e461
Dan Albert <danalbert@google.com>
2014-10-31 16:23:08 -0700

    Add a SANITIZE_HOST flag.
    We had discussed the idea of making all host tools default to using
    ASAN. Even if we don't make it the default, this makes it easy for the
    user to switch all host binaries over.
    
    Change-Id: I64a5c741b1b4e9aefed3a6be8dcd4f386e06b29c

--- envsetup.sh
+++ envsetup.sh
@@ -231,6 +231,7 @@ function set_stuff_for_environment()
     export ANDROID_BUILD_TOP=$(gettop)
     # With this environment variable new GCC can apply colors to warnings/errors
     export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+    export ASAN_OPTIONS=detect_leaks=0
 }
 
 function set_sequence_number()
