ddb0bc9828f6f7e58713836e97768a521d5b1e7f
Yabin Cui <yabinc@google.com>
2014-11-10 16:57:28 -0800

    remove __linux__ in AndroidConfig.h
    Bug: 18316409
    Change-Id: Idc573c480505cee733db0de376c0714306263721

--- core/combo/include/arch/linux-arm/AndroidConfig.h
+++ core/combo/include/arch/linux-arm/AndroidConfig.h
@@ -99,13 +99,6 @@
 #define HAVE_SCHED_SETSCHEDULER
 
 /*
- * Add any extra platform-specific defines here.
- */
-#ifndef __linux__
-#define __linux__
-#endif
-
-/*
  * Define if we have <malloc.h> header
  */
 #define HAVE_MALLOC_H
