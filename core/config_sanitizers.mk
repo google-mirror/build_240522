ea18c7cd86d378932876f71c6b466a5e25e9ab74
Dan Albert <danalbert@google.com>
2014-12-11 18:56:26 -0800

    WIP: Add support for ubsan.
    Rather than adding LOCAL_UB_SANITIZER, LOCAL_THREAD_SANITIZER, etc for
    each new sanitizer, deprecate LOCAL_ADDRESS_SANITIZER in favor of
    LOCAL_SANITZE that mirrors the behavior of -fsanitize=<sanitizers>.
    
    For example, the following will use both asan and ubsan:
    
        LOCAL_SANITIZE := address undefined
    
    We'll leave LOCAL_ADDRESS_SANITIZER around for compatibility until we
    can clean up the tree.
    
    Change-Id: I8a62315129d4753f8e992584ca6db1e5dfdd4d2a

--- core/config_sanitizers.mk
+++ core/config_sanitizers.mk
@@ -13,20 +13,54 @@ endif
 endif
 endif
 
-# Configure address sanitizer.
+my_sanitize := $(LOCAL_SANITIZE)
+
+# Keep compatibility for LOCAL_ADDRESS_SANITIZER until all targets have moved to
+# `LOCAL_SANITIZE := address`.
 ifeq ($(strip $(LOCAL_ADDRESS_SANITIZER)),true)
+  my_sanitize += address
+endif
+
+unknown_sanitizers := $(filter-out address, \
+                      $(filter-out undefined,$(my_sanitize)))
+
+ifneq ($(unknown_sanitizers),)
+  $(error Unknown sanitizers: $(unknown_sanitizers))
+endif
+
+ifneq ($(my_sanitize),)
   my_clang := true
+
+  comma := ,
+  empty :=
+  space := $(empty) $(empty)
+  fsanitize_arg := $(subst $(space),$(comma),$(my_sanitize)),
+  my_cflags += -fsanitize=$(fsanitize_arg)
+
+  ifdef LOCAL_IS_HOST_MODULE
+    my_ldflags += -fsanitize=$(fsanitize_arg)
+  endif
+endif
+
+ifneq ($(filter address,$(my_sanitize)),)
   # Frame pointer based unwinder in ASan requires ARM frame setup.
   LOCAL_ARM_MODE := arm
   my_cflags += $(ADDRESS_SANITIZER_CONFIG_EXTRA_CFLAGS)
   my_ldflags += $(ADDRESS_SANITIZER_CONFIG_EXTRA_LDFLAGS)
   ifdef LOCAL_IS_HOST_MODULE
-      my_ldflags += -fsanitize=address
-      # -nodefaultlibs (provided with libc++) prevents the driver from linking
-      # libraries needed with -fsanitize=address. http://b/18650275
-      my_ldlibs += -ldl -lpthread
+    # -nodefaultlibs (provided with libc++) prevents the driver from linking
+    # libraries needed with -fsanitize=address. http://b/18650275
+    my_ldlibs += -ldl -lpthread
   else
-      my_shared_libraries += $(ADDRESS_SANITIZER_CONFIG_EXTRA_SHARED_LIBRARIES)
-      my_static_libraries += $(ADDRESS_SANITIZER_CONFIG_EXTRA_STATIC_LIBRARIES)
+    my_shared_libraries += $(ADDRESS_SANITIZER_CONFIG_EXTRA_SHARED_LIBRARIES)
+    my_static_libraries += $(ADDRESS_SANITIZER_CONFIG_EXTRA_STATIC_LIBRARIES)
+  endif
+endif
+
+ifneq ($(filter undefined,$(my_sanitize)),)
+  ifdef LOCAL_IS_HOST_MODULE
+    my_ldlibs += -ldl
+  else
+    $(error ubsan is not yet supported on the target)
   endif
 endif
