d0d8cef91b18005d9c52e9d622caf47e29a827fe
Mark Salyzyn <salyzyn@google.com>
2015-04-01 14:41:29 -0700

    fs_config: Add fs_config_generate
    fs_config_generate_$(TARGET_DEVICE) is built based off the content
    of $(TARGET_DEVICE_DIR)/android_filesystem_config.h. We also add
    the rules fs_config_dirs and fs_config_file to utilize this command
    for target contents:
    
    fs_config_generate_$(TARGET_DEVICE) -D -o system/etc/fs_config_dir
    fs_config_generate_$(TARGET_DEVICE) -F -o system/etc/fs_config_file
    
    In order to use this feature, one must have the fs_config_dirs and
    fs_config_files in the $(PRODUCT_PACKAGES) list defined in the
    device make files in $(TARGET_DEVICE_DIR).
    
    Bug: 19908228
    Change-Id: Iee1543d99169f874e0915ae07962a7750ecb6342

--- tools/fs_config/Android.mk
+++ tools/fs_config/Android.mk
@@ -22,3 +22,53 @@ LOCAL_FORCE_STATIC_EXECUTABLE := true
 LOCAL_CFLAGS := -Werror
 
 include $(BUILD_HOST_EXECUTABLE)
+
+# To Build the custom target binary for the host to generate the fs_config
+# override files. The executable is hard coded to include the
+# $(TARGET_ANDROID_FILESYSTEM_CONFIG_H) file if it exists.
+# Expectations:
+#    device/<vendor>/<device>/android_filesystem_config.h
+#        fills in struct fs_path_config android_device_dirs[] and
+#                 struct fs_path_config android_device_files[]
+#    device/<vendor>/<device>/device.mk
+#        PRODUCT_PACKAGES += fs_config_dirs fs_config_files
+
+TARGET_ANDROID_FILESYSTEM_CONFIG_H := $(wildcard $(TARGET_DEVICE_DIR)/android_filesystem_config.h)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := fs_config_generate.c
+LOCAL_MODULE := fs_config_generate_$(TARGET_DEVICE)
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_IS_HOST_MODULE := true
+LOCAL_SHARED_LIBRARIES := libcutils
+
+LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_ANDROID_FILESYSTEM_CONFIG_H)
+LOCAL_CFLAGS := $(TARGET_DEVICE_DIR:%=-I%) -Werror
+LOCAL_CFLAGS += $(LOCAL_ADDITIONAL_DEPENDENCIES:%=-DANDROID_FILESYSTEM_CONFIG=%)
+
+include $(BUILD_HOST_EXECUTABLE)
+
+# Generate the system/etc/fs_config_dirs binary file for the target
+# Add fs_config_dirs to PRODUCT_PACKAGES in the device make file to enable
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := fs_config_dirs
+LOCAL_MODULE_CLASS := ETC
+include $(BUILD_SYSTEM)/base_rules.mk
+TARGET_fs_config_dirs := $(LOCAL_BUILT_MODULE)
+$(TARGET_fs_config_dirs): $(HOST_OUT_EXECUTABLES)/fs_config_generate_$(TARGET_DEVICE)
+	-mkdir -p $(TARGET_fs_config_dirs:%/fs_config_dirs=%)
+	$(HOST_OUT_EXECUTABLES)/fs_config_generate_$(TARGET_DEVICE) -D -o $(TARGET_fs_config_dirs)
+
+# Generate the system/etc/fs_config_files binary file for the target
+# Add fs_config_files to PRODUCT_PACKAGES in the device make file to enable
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := fs_config_files
+LOCAL_MODULE_CLASS := ETC
+include $(BUILD_SYSTEM)/base_rules.mk
+TARGET_fs_config_files := $(LOCAL_BUILT_MODULE)
+$(TARGET_fs_config_files): $(HOST_OUT_EXECUTABLES)/fs_config_generate_$(TARGET_DEVICE)
+	-mkdir -p $(TARGET_fs_config_files:%/fs_config_files=%)
+	$(HOST_OUT_EXECUTABLES)/fs_config_generate_$(TARGET_DEVICE) -F -o $(TARGET_fs_config_files)
