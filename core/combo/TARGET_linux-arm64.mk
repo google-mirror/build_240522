045861e91d20936d7b12eed97a92df89c60f7f96
Andrew Hsieh <andrewhsieh@google.com>
2015-04-21 11:02:17 +0800

    Use ld.gold for aarch64  (DO NOT SUBMIT)
    1. ld.gold for aarch64 isn't default since it's new.  Add -fuse-ld=gold
       explicitly to enable it for testing purpose
    2. parallel linking in ld.gold appears to be stable enough in binutils-2.25.
       Add -Wl,--threads
    
    Change-Id: Ieed374869a6d7de5fd1dd8eda1a2e3c8abf27cbd

--- core/combo/TARGET_linux-arm64.mk
+++ core/combo/TARGET_linux-arm64.mk
@@ -115,6 +115,7 @@ TARGET_GLOBAL_LDFLAGS += \
 			-Wl,-maarch64linux \
 			-Wl,--hash-style=gnu \
 			-Wl,--fix-cortex-a53-843419 \
+                        -fuse-ld=gold -Wl,--threads \
 			$(arch_variant_ldflags)
 
 # Disable transitive dependency library symbol resolving.
