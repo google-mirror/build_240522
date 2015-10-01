e88a94f5bd92b17b7c6a05ddbd629c15bdd2b32c
Colin Cross <ccross@android.com>
2015-10-01 16:09:15 -0700

    Add realpath tool for hosts that don't have it
    To use a locally built realpath replace $(realpath $(PATH)) with
    $$($(REALPATH) $(PATH)), and add a dependency on $(REALPATH).
    
    Bug: 24580686
    Change-Id: I5202c1b974fc6eb65e9de69529c5b6201b52687d

---
+++ tools/realpath/realpath.cpp
@@ -0,0 +1,20 @@
+#include <limits.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    printf("usage: %s <path>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  char *real = realpath(argv[1], NULL);
+  if (real == nullptr) {
+    perror(argv[1]);
+    exit(EXIT_FAILURE);
+  }
+
+  printf("%s\n", real);
+  return 0;
+}
+
