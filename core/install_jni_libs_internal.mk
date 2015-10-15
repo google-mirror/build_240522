3d1ffa87eaa6ca8029f38941da169ebd4a0b8ee9
Ying Wang <wangying@google.com>
2015-10-15 15:29:01 -0700

    Use direct dependency on the JNI so files.
    With the order-only dependency if the jni so files is updated,
    make won't rebuild the system.img, which has dependency only on the apk
    file.
    
    Bug: 24865400
    Change-Id: I9d5bee82b8a712a2c24dabaa0cd4c50174ea219f

--- core/install_jni_libs_internal.mk
+++ core/install_jni_libs_internal.mk
@@ -55,7 +55,8 @@ ifneq ($(my_jni_shared_libraries),)
 my_jni_filenames := $(notdir $(my_jni_shared_libraries))
 # Make sure the JNI libraries get installed
 my_shared_library_path := $($(my_2nd_arch_prefix)TARGET_OUT$(partition_tag)_SHARED_LIBRARIES)
-$(LOCAL_INSTALLED_MODULE) : | $(addprefix $(my_shared_library_path)/, $(my_jni_filenames))
+# Do not use order-only dependency, because we want to rebuild the image if an jni is updated.
+$(LOCAL_INSTALLED_MODULE) : $(addprefix $(my_shared_library_path)/, $(my_jni_filenames))
 
 # Create symlink in the app specific lib path
 ifdef LOCAL_POST_INSTALL_CMD
