08f206213184f723f0287cd460742143553ea587
Chih-Hung Hsieh <chh@google.com>
2015-12-10 14:13:43 -0800

    Change USE_CLANG_PLATFORM_BUILD default to true.
    When USE_CLANG_PLATFORM_BUILD is not set, default will be clang/llvm.
    USE_CLANG_PLATFORM_BUILD=false can be used to select gcc as default.
    
    BUG: 23163853
    BUG: 26102335
    
    Change-Id: I00604c2aef4849e8c3505b2c4002eb1c46cd1fd1

--- core/binary.mk
+++ core/binary.mk
@@ -209,11 +209,15 @@ ifdef LOCAL_IS_HOST_MODULE
         my_clang := true
     endif
     endif
-# Add option to make clang the default for device build
-else ifeq ($(USE_CLANG_PLATFORM_BUILD),true)
+# Add option to make gcc the default for device build
+else ifeq ($(USE_CLANG_PLATFORM_BUILD),false)
     ifeq ($(my_clang),)
-        my_clang := true
+        my_clang := false
     endif
+endif
+
+ifeq ($(my_clang),)
+  my_clang := true
 endif
 
 my_cpp_std_version := -std=gnu++14
