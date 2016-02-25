d996af24957eb72bad27ed3f3586e2dc023b2108
Chih-Hung Hsieh <chh@google.com>
2016-02-24 16:42:15 -0800

    Use new clang scan-build script.
    The actual clang compiler called for static analysis is decided
    by build/core/binary.mk, not the one given to --use-analyzer.
    
    BUG: 13287788
    Change-Id: I58105c20b56ce17ddf329a275c750d14284d1e25

--- envsetup.sh
+++ envsetup.sh
@@ -644,11 +644,16 @@ function getdriver()
     local T="$1"
     test "$WITH_STATIC_ANALYZER" = "0" && unset WITH_STATIC_ANALYZER
     if [ -n "$WITH_STATIC_ANALYZER" ]; then
+        # Use scan-build to collect all static analyzer reports into directory
+        # /tmp/scan-build-yyyy-mm-dd-hhmmss-*
+        # The clang compiler passed by --use-analyzer here is not important.
+        # build/core/binary.mk will set CLANG_CXX and CLANG before calling
+        # c++-analyzer and ccc-analyzer.
+        local CLANG_DIR="$T/prebuilts/clang/host/linux-x86/clang-2629532"
         echo "\
-$T/prebuilts/misc/linux-x86/analyzer/tools/scan-build/scan-build \
---use-analyzer $T/prebuilts/misc/linux-x86/analyzer/bin/analyzer \
---status-bugs \
---top=$T"
+${CLANG_DIR}/tools/scan-build/bin/scan-build \
+--use-analyzer ${CLANG_DIR}/bin/clang \
+--status-bugs"
     fi
 }
 
