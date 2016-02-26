cc6afda3a381a68297de1c1fcd617ebbb8f3819a
Steve Fung <stevefung@google.com>
2016-02-26 15:52:45 -0800

    Add brillo_tests build target
    Add a brillo_tests build target, and populate it with the tests
    from the device/generic/brillo/tests.txt file.
    
    Change-Id: Iaade411d4ba10fd030ebb16b679d38e21352203b

---
+++ core/tasks/brillo_tests.mk
@@ -0,0 +1,16 @@
+# Rules to generate a zip file that included test modules
+# based on the configuration.
+#
+
+my_modules := $(shell sed -n -e 's/\(^[^#].*\),.*/\1/p' device/generic/brillo/tests.txt)
+
+my_package_name := brillo_tests
+
+include $(BUILD_SYSTEM)/tasks/tools/package-modules.mk
+
+.PHONY: brillo_tests
+brillo_tests : $(my_package_zip)
+
+name := $(TARGET_PRODUCT)-brillo-tests-$(FILE_NAME_TAG)
+$(call dist-for-goals, brillo_tests, $(my_package_zip):$(name).zip)
+
