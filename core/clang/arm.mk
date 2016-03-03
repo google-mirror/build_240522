ea2f9e644a34c5fbf893d1ee79381c5d97a8a5cc
Stephen Hines <srhines@google.com>
2016-03-03 15:33:35 -0800

    Switch from -mcpu=krait back to -mcpu=cortex-a15.
    Bug: http://b/27482631
    
    Clang does the wrong thing for -mcpu=krait when also using
    -no-integrated-as (forcing our assembler to use just generic ARMv7 with
    NEON, instead of something more appropriate like cortex-a15).
    
    Change-Id: Idc6db93908e724b5129341419e628840f4ffeb5d

--- core/clang/arm.mk
+++ core/clang/arm.mk
@@ -7,7 +7,10 @@ CLANG_CONFIG_arm_EXTRA_CFLAGS :=
 ifneq (,$(filter krait,$(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)))
   # Android's clang support's krait as a CPU whereas GCC doesn't. Specify
   # -mcpu here rather than the more normal core/combo/arch/arm/armv7-a-neon.mk.
-  CLANG_CONFIG_arm_EXTRA_CFLAGS += -mcpu=krait -mfpu=neon-vfpv4
+  # Bug: http://b/27482631
+  # Unfortunately, passing -mcpu=krait doesn't work with -no-integrated-as,
+  # so we fall back to treating krait as a cortex-a15.
+  CLANG_CONFIG_arm_EXTRA_CFLAGS += -mcpu=cortex-a15 -mfpu=neon-vfpv4
 endif
 
 CLANG_CONFIG_arm_EXTRA_CPPFLAGS :=
