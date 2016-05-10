a0f40f9423f840ac0c4af83cf2814496cb28a51a
Dan Willemsen <dwillemsen@google.com>
2016-05-10 14:54:08 -0700

    Do not apply -Wl,--fix-cortex-a8 to Cortex-A9
    Change-Id: I11d380163423cb4aa0fee60ecbaa1e38fa5143a1

--- core/combo/arch/arm/armv7-a-neon.mk
+++ core/combo/arch/arm/armv7-a-neon.mk
@@ -32,9 +32,14 @@ ifneq (,$(filter cortex-a7 cortex-a53 cortex-a53.a57,$(TARGET_$(combo_2nd_arch_p
 		-Wl,--no-fix-cortex-a8
 else
 	arch_variant_cflags := -march=armv7-a
+ifeq ($(strip $(TARGET_$(combo_2nd_arch_prefix)CPU_VARIANT)),cortex-a9)
+	arch_variant_ldflags := \
+		-Wl,--no-fix-cortex-a8
+else
 	# Generic ARM might be a Cortex A8 -- better safe than sorry
 	arch_variant_ldflags := \
 		-Wl,--fix-cortex-a8
+endif
 endif
 endif
 endif
