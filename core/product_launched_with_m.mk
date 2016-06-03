f5b15f5b78885416922d8b44723fdfb7a6eea9ca
Vikas Marwaha <vikasmarwaha@google.com>
2016-06-03 15:02:06 -0700

    Add variable PRODUCT_SHIPPING_API_LEVEL and make files with the new read only product property ro.product.first_api_level.
    PRODUCT_SHIPPING_API_LEVEL declares which API level a device first
    shipped with. We use this variable to set a read-only property that
    can be used in CTS/GTS tests.
    
    Change-Id: If54202075d3b92ed380655cde8b3fc50c5ba73b3

---
+++ core/product_launched_with_m.mk
@@ -0,0 +1,5 @@
+#ro.product.first_api_level indicates the first api level, device has been commercially launced on.
+ifndef PRODUCT_SHIPPING_API_LEVEL
+PRODUCT_PROPERTY_OVERRIDES += \
+ ro.product.first_api_level=23
+endif
