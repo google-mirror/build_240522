8139025da27d6ce52f4771863bab6eedff180a58
Vijay Venkatraman <vijaykv@google.com>
2016-11-30 11:13:43 -0800

    Extends VNDK support in Make by setting LOCAL_USE_VNDK based on proprietary flags or module path
    Test: Any module that uses VNDK
    Change-Id: Ic90b165ade8d9845595f371f410635cae6477d7c

--- core/local_vndk.mk
+++ core/local_vndk.mk
@@ -1,3 +1,15 @@
+
+#Set LOCAL_VNDK if proprietary/odm/oem flag is true or module is going into vendor partition
+ifndef LOCAL_USE_VNDK
+  ifneq (,$(filter true,$(LOCAL_PROPRIETARY_MODULE) $(LOCAL_ODM_MODULE) $(LOCAL_OEM_MODULE)))
+    LOCAL_USE_VNDK:=true
+  else
+    ifneq (,$(filter $(TARGET_OUT_VENDOR),$(LOCAL_MODULE_PATH)))
+      LOCAL_USE_VNDK:=true
+    endif
+  endif
+endif
+
 # Verify LOCAL_USE_VNDK usage, and set LOCAL_SDK_VERSION if necessary
 
 ifdef LOCAL_IS_HOST_MODULE
