e1f73983d3ea0d7e034b02e599e33e4014211c89
Dan Willemsen <dwillemsen@google.com>
2016-12-09 16:05:09 -0800

    Implement LOCAL_TEST_DATA to ship data with tests
    This can be used to ship source data as test artifacts next to native
    tests. It works for both local builds and the test bundles using
    package_modules.mk.
    
    You just specify a file list relative to the local directory, and those
    files will be copied next to the executable under
    /data/nativetest*/<module>/...:
    
      LOCAL_MODULE := mytest
      LOCAL_TEST_DATA := data/file1 file2
    
      /data/nativetest/mytest/mytest
      /data/nativetest/mytest/data/file1
      /data/nativetest/mytest/file2
    
    I'll expand this to native benchmarks and fuzz tests in a later change,
    since they don't have their own module classes yet.
    
    Bug: 30564705
    Test: m -j minikin_tests; ls $OUT/data/nativetest*/minikin_tests
    Test: m -j continuous_native_tests dist; zipinfo -1 out/dist/*continuous_native_tests*.zip
    Change-Id: Ic76a7b62e7f567f259c4ab1510ee97d26600ba9a

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -461,6 +461,28 @@ $(my_all_targets) : $(my_compat_files)
 endif  # LOCAL_COMPATIBILITY_SUITE
 
 ###########################################################
+## Test Data
+###########################################################
+my_test_data_pairs :=
+my_installed_test_data :=
+
+ifneq ($(filter NATIVE_TESTS,$(LOCAL_MODULE_CLASS)),)
+ifneq ($(strip $(LOCAL_TEST_DATA)),)
+ifneq (true,$(LOCAL_UNINSTALLABLE_MODULE))
+
+my_test_data_pairs := $(strip $(foreach td,$(LOCAL_TEST_DATA), \
+    $(if $(findstring ..,$(td)),$(error $(LOCAL_MODULE_MAKEFILE): LOCAL_TEST_DATA may not include '..': $(td))) \
+    $(if $(filter /%,$(td)),$(error $(LOCAL_MODULE_MAKEFILE): LOCAL_TEST_DATA may not include absolute paths: $(td))) \
+    $(call append-path,$(LOCAL_PATH),$(td)):$(call append-path,$(my_module_path),$(td))))
+
+my_installed_test_data := $(call copy-many-files,$(my_test_data_pairs))
+$(LOCAL_INSTALLED_MODULE): $(my_installed_test_data)
+
+endif
+endif
+endif
+
+###########################################################
 ## Register with ALL_MODULES
 ###########################################################
 
@@ -481,11 +503,12 @@ ALL_MODULES.$(my_register_name).BUILT := \
 ifneq (true,$(LOCAL_UNINSTALLABLE_MODULE))
 ALL_MODULES.$(my_register_name).INSTALLED := \
     $(strip $(ALL_MODULES.$(my_register_name).INSTALLED) \
-    $(LOCAL_INSTALLED_MODULE) $(my_init_rc_installed) $(my_installed_symlinks))
+    $(LOCAL_INSTALLED_MODULE) $(my_init_rc_installed) $(my_installed_symlinks) \
+    $(my_installed_test_data))
 ALL_MODULES.$(my_register_name).BUILT_INSTALLED := \
     $(strip $(ALL_MODULES.$(my_register_name).BUILT_INSTALLED) \
     $(LOCAL_BUILT_MODULE):$(LOCAL_INSTALLED_MODULE) \
-    $(my_init_rc_pairs))
+    $(my_init_rc_pairs) $(my_test_data_pairs))
 endif
 ifdef LOCAL_PICKUP_FILES
 # Files or directories ready to pick up by the build system
