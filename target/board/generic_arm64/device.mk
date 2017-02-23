7f516c810309216b2f0aff4545057c5c14c853ec
Diego Wilson <diegowilson@google.com>
2017-02-22 16:02:01 -0800

    WIP: build aosp_arm64 for CTS on AOSP test on marlin
    This is very early WIP. Don't expect it will boot.
    
    Bug: 35127378
    
    Test: lunch aosp_arm64-userdebug; m -j 80 systemimage;
    fastboot -w flash system path/to/generated/system.img
    
    Change-Id: I91faceb107862f88e554e468fbe045c9b9fa4987

--- target/board/generic_arm64/device.mk
+++ target/board/generic_arm64/device.mk
@@ -20,7 +20,7 @@
 PRODUCT_PROPERTY_OVERRIDES := \
     ro.ril.hsxpa=1 \
     ro.ril.gprsclass=10 \
-    ro.adb.qemud=1
+#    ro.adb.qemud=1
 
 PRODUCT_COPY_FILES := \
     device/generic/goldfish/data/etc/apns-conf.xml:system/etc/apns-conf.xml \
@@ -30,6 +30,45 @@ PRODUCT_COPY_FILES := \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     device/generic/goldfish/camera/media_codecs.xml:system/etc/media_codecs.xml
 
+# display
+ PRODUCT_PROPERTY_OVERRIDES += \
+     ro.sf.lcd_density=420
+
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+     ro.config.vc_call_vol_steps=7
+
+# HWUI cache sizes
+PRODUCT_PROPERTY_OVERRIDES += \
+     ro.hwui.texture_cache_size=56 \
+     ro.hwui.layer_cache_size=32 \
+     ro.hwui.path_cache_size=16
+
+# Use Sdcardfs
+PRODUCT_PROPERTY_OVERRIDES += \
+     ro.sys.sdcardfs=1
+
+PRODUCT_COPY_FILES += \
+    device/google/marlin/init.common.rc:root/init.marlin.rc \
+    device/google/marlin/init.common.usb.rc:root/init.marlin.usb.rc \
+    device/google/marlin/fstab.common:root/fstab.marlin \
+    device/google/marlin/ueventd.common.rc:root/ueventd.marlin.rc \
+    device/google/marlin/init.recovery.common.rc:root/init.recovery.marlin.rc
+
+# Sensor hub init script
+PRODUCT_COPY_FILES += \
+    device/google/marlin/init.common.nanohub.rc:root/init.marlin.nanohub.rc
+
+
+AB_OTA_PARTITIONS := boot system
+AB_OTA_UPDATER := true
+PRODUCT_PACKAGES := update_engine update_verifier
+
+
 # Adjust the Dalvik heap to be appropriate for a tablet.
 $(call inherit-product-if-exists, frameworks/base/build/tablet-dalvik-heap.mk)
 $(call inherit-product-if-exists, frameworks/native/build/tablet-dalvik-heap.mk)
+
+PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/bootdevice/by-name/system
+PRODUCT_VENDOR_VERITY_PARTITION := /dev/block/bootdevice/by-name/vendor
+$(call inherit-product, build/target/product/verity.mk)
