06164202e2a633ff160bea720a99a22a8bcbec97
Mark Salyzyn <salyzyn@google.com>
2017-03-22 15:53:12 -0700

    fs_config_generator: Add odm tests
    Add tests for odm filtration as well.
    
    Test: gTest host fs_conf-unit-tests
    Bug: 36071012
    Change-Id: I89a539735c6fb68b39c2c73aad6e898d5b91c89e

--- tools/fs_config/fs_config_generate.c
+++ tools/fs_config/fs_config_generate.c
@@ -41,10 +41,12 @@ static const struct fs_path_config android_device_files[] = {
     {0000, AID_ROOT, AID_ROOT, 0, "system/etc/fs_config_dirs"},
     {0000, AID_ROOT, AID_ROOT, 0, "vendor/etc/fs_config_dirs"},
     {0000, AID_ROOT, AID_ROOT, 0, "oem/etc/fs_config_dirs"},
+    {0000, AID_ROOT, AID_ROOT, 0, "odm/etc/fs_config_dirs"},
 #endif
     {0000, AID_ROOT, AID_ROOT, 0, "system/etc/fs_config_files"},
     {0000, AID_ROOT, AID_ROOT, 0, "vendor/etc/fs_config_files"},
     {0000, AID_ROOT, AID_ROOT, 0, "oem/etc/fs_config_files"},
+    {0000, AID_ROOT, AID_ROOT, 0, "odm/etc/fs_config_files"},
 };
 #endif
 
