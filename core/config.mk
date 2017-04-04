248cc0f12a2cf5bceee32dd05adc8b8e29c3bc2f
Michael Schwartz <schwartzmi@google.com>
2017-04-04 13:01:18 -0700

    Rule to copy device manifest to device
    The device manifest defines the collection of HAL interfaces exposed by
    the vendor. Create a common rule so each target need not define their own.
    
    Bug: 36810913
    Test: Ran marlin build and inspected file was copied
    Change-Id: I9f53796fdd85e706f91664d275aff6dbd785213f

--- core/config.mk
+++ core/config.mk
@@ -739,6 +739,8 @@ else
   DEFAULT_SYSTEM_DEV_CERTIFICATE := build/target/product/security/testkey
 endif
 
+INSTALLED_SYSTEM_MANIFEST := $(PRODUCT_OUT)/system/manifest.xml
+
 # ###############################################################
 # Set up final options.
 # ###############################################################
--- target/product/embedded.mk
+++ target/product/embedded.mk
@@ -76,6 +76,8 @@ PRODUCT_PACKAGES += \
     surfaceflinger \
     tombstoned \
     tzdatacheck \
+    matrix.xml \
+    manifest.xml \
 
 # SELinux packages
 PRODUCT_PACKAGES += \
@@ -109,5 +111,4 @@ PRODUCT_COPY_FILES += \
     system/core/rootdir/etc/hosts:system/etc/hosts
 
 # Framework Manifest
-PRODUCT_COPY_FILES += \
-    system/libhidl/manifest.xml:system/manifest.xml
+PRODUCT_COPY_FILES += system/libhidl/manifest.xml:system/manifest.xml
