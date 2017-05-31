6aa35f1b51e989450ce082d8a49f2a70f9229c35
Steven Moreland <smoreland@google.com>
2017-05-31 12:48:55 -0700

    DO NOT SUBMIT: move math_gt_or_eq definition
    It was not defined early enough for use in config.mk.
    
    Test: wip
    Change-Id: Icdab4214a1e65ae202411613dbcb9c9ea5f43b09

--- core/definitions.mk
+++ core/definitions.mk
@@ -3152,11 +3152,6 @@ endef
 #
 # (SDK versions for example)
 ###########################################################
-__MATH_NUMBERS :=  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 \
-                  21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 \
-                  41 42 43 44 45 46 47 48 49 50 51 52 53 54 55 56 57 58 59 60 \
-                  61 62 63 64 65 66 67 68 69 70 71 72 73 74 75 76 77 78 79 80 \
-                  81 82 83 84 85 86 87 88 89 90 91 92 93 94 95 96 97 98 99 100
 
 # Returns true if $(1) is a positive integer <= 100, otherwise returns nothing.
 define math_is_number
@@ -3181,28 +3176,6 @@ endef
 #$(call _math_check_valid,101)
 #$(call _math_check_valid,)
 #$(call _math_check_valid,1 2)
-
-# Returns the greater of $1 or $2.
-# If $1 or $2 is not a positive integer <= 100, then an error is generated.
-define math_max
-$(strip $(call _math_check_valid,$(1)) $(call _math_check_valid,$(2)) \
-  $(lastword $(filter $(1) $(2),$(__MATH_NUMBERS))))
-endef
-
-#$(call math_max)
-#$(call math_max,1)
-#$(call math_max,1 2,3)
-#$(warning 1 == $(call math_max,1,1))
-#$(warning 42 == $(call math_max,5,42))
-#$(warning 42 == $(call math_max,42,5))
-
-define math_gt_or_eq
-$(if $(filter $(1),$(call math_max,$(1),$(2))),true)
-endef
-
-#$(warning $(call math_gt_or_eq, 2, 1))
-#$(warning $(call math_gt_or_eq, 1, 1))
-#$(warning $(if $(call math_gt_or_eq, 1, 2),false,true))
 
 # $1 is the variable name to increment
 define inc_and_print
