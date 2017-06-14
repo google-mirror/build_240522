4a7b51376b22f64281e897ae91a201be86068e1e
Tianjie Xu <xunchang@google.com>
2017-06-13 23:45:07 -0700

    Verify the contents in install-recovery.sh
    Test: validate on bullhead TF
    Change-Id: I5f07a869d9fa17fad26a22ef9ca3ecb06b1b28e3

--- tools/releasetools/validate_target_files.py
+++ tools/releasetools/validate_target_files.py
@@ -29,7 +29,6 @@ import os.path
 import sparse_img
 import sys
 
-
 def _GetImage(which, tmpdir):
   assert which in ('system', 'vendor')
 
@@ -42,13 +41,31 @@ def _GetImage(which, tmpdir):
   clobbered_blocks = '0'
   return sparse_img.SparseImage(path, mappath, clobbered_blocks)
 
-
-def ValidateFileConsistency(input_zip, input_tmp):
-  """Compare the files from image files and unpacked folders."""
+def _CalculateFileSha1(file_name, unpacked_name, round_up=False):
 
   def RoundUpTo4K(value):
     rounded_up = value + 4095
     return rounded_up - (rounded_up % 4096)
+
+  assert os.path.exists(unpacked_name)
+  with open(unpacked_name, 'r') as f:
+    file_data = f.read()
+  file_size = len(file_data)
+  if round_up:
+    file_size_rounded_up = RoundUpTo4K(file_size)
+    file_data += '\0' * (file_size_rounded_up - file_size)
+  return common.File(file_name, file_data).sha1
+
+def _ValidateFileAgainstSha1(input_tmp, file_name, file_path, expected_sha1):
+  logging.info('Validating the SHA1 of {}'.format(file_name))
+  unpacked_name = os.path.join(input_tmp, file_path)
+  assert os.path.exists(unpacked_name)
+  actual_sha1 = _CalculateFileSha1(file_name, unpacked_name, False)
+  assert actual_sha1 == expected_sha1, 'SHA1 mismatches for {}. ' \
+      'actual {}, expected {}'.format(file_name, actual_sha1, expected_sha1)
+
+def ValidateFileConsistency(input_zip, input_tmp):
+  """Compare the files from image files and unpacked folders."""
 
   def CheckAllFiles(which):
     logging.info('Checking %s image.', which)
@@ -66,12 +83,7 @@ def ValidateFileConsistency(input_zip, input_tmp):
       # The filename under unpacked directory, such as SYSTEM/bin/sh.
       unpacked_name = os.path.join(
           input_tmp, which.upper(), entry[(len(prefix) + 1):])
-      with open(unpacked_name) as f:
-        file_data = f.read()
-      file_size = len(file_data)
-      file_size_rounded_up = RoundUpTo4K(file_size)
-      file_data += '\0' * (file_size_rounded_up - file_size)
-      file_sha1 = common.File(entry, file_data).sha1
+      file_sha1 = _CalculateFileSha1(entry, unpacked_name, True)
 
       assert blocks_sha1 == file_sha1, \
           'file: %s, range: %s, blocks_sha1: %s, file_sha1: %s' % (
@@ -87,6 +99,46 @@ def ValidateFileConsistency(input_zip, input_tmp):
     CheckAllFiles('vendor')
 
   # Not checking IMAGES/system_other.img since it doesn't have the map file.
+
+
+def ValidateInstallRecoveryScript(input_tmp):
+  script_path = 'SYSTEM/bin/install-recovery.sh'
+  if not os.path.exists(os.path.join(input_tmp, script_path)):
+    logging.info('{} does not exist in input_tmp'.format(script_path))
+    return
+
+  logging.info('Checking {}'.format(script_path))
+  with open(os.path.join(input_tmp, script_path), 'r') as script:
+    lines = script.read().strip().split('\n')
+    assert len(lines) >= 6
+    trim_index = lines[2].find('&&')
+    assert trim_index != -1
+    applypatch_argv = lines[2][:trim_index].strip().split(' ')
+    assert len(applypatch_argv) >= 5 and applypatch_argv[0] == 'applypatch'
+
+    if len(applypatch_argv) == 5:
+      expected_recovery_sha1 = applypatch_argv[3].strip()
+      _ValidateFileAgainstSha1(input_tmp, 'recovery.img',
+          'SYSTEM/etc/recovery.img', expected_recovery_sha1)
+    else:
+      if applypatch_argv[1] == "-b":
+        assert len(applypatch_argv) == 8
+        boot_info_index = 3
+      else:
+        assert len(applypatch_argv) == 6
+        boot_info_index = 1
+      boot_info = applypatch_argv[boot_info_index].strip().split(':')
+      assert len(boot_info) == 4
+      _ValidateFileAgainstSha1(input_tmp, file_name='boot.img',
+          file_path='IMAGES/boot.img', expected_sha1=boot_info[3])
+
+      recovery_sha1_index = boot_info_index + 2
+      expected_recovery_sha1 = applypatch_argv[recovery_sha1_index]
+      _ValidateFileAgainstSha1(input_tmp, file_name='recovery.img',
+          file_path='IMAGES/recovery.img',
+          expected_sha1=expected_recovery_sha1)
+
+  logging.info('Done checking {}'.format(script_path))
 
 
 def main(argv):
@@ -110,17 +162,22 @@ def main(argv):
   logging.info("Unzipping the input target_files.zip: %s", args[0])
   input_tmp, input_zip = common.UnzipTemp(args[0])
 
-  ValidateFileConsistency(input_zip, input_tmp)
+  #ValidateFileConsistency(input_zip, input_tmp)
+
+  ValidateInstallRecoveryScript(input_tmp)
 
   # TODO: Check if the OTA keys have been properly updated (the ones on /system,
   # in recovery image).
 
-  # TODO(b/35411009): Verify the contents in /system/bin/install-recovery.sh.
+
+
 
   logging.info("Done.")
 
 
 if __name__ == '__main__':
+  import tempfile
+  tempfile.tempdir = "/usr/local/google/home/xunchang/1"
   try:
     main(sys.argv[1:])
   finally:
