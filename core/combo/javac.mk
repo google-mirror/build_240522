c5103f5bdf93f825a30ea3b29936ba95bbe7f677
Colin Cross <ccross@android.com>
2017-08-09 16:24:02 -0700

    Use java variables from soong
    Use TARGET_JAVAC and HOST_JAVAC exported from soong.
    
    Test: m -j checkbuild
    Change-Id: Ic094174149ea304c0b06ddc023417acf3602d5a3

--- core/combo/javac.mk
+++ core/combo/javac.mk
@@ -23,8 +23,6 @@ ifndef ANDROID_COMPILE_WITH_JACK
     endif
 endif
 
-common_jdk_flags := -Xmaxerrs 9999999
-
 ifeq ($(OVERRIDE_ANDROID_JAVA_HOME),)
 ANDROID_JAVA_HOME := prebuilts/jdk/jdk8/$(HOST_PREBUILT_TAG)
 else
@@ -33,31 +31,3 @@ ANDROID_JAVA_HOME := $(OVERRIDE_ANDROID_JAVA_HOME)
 endif
 ANDROID_JAVA_TOOLCHAIN := $(ANDROID_JAVA_HOME)/bin
 export JAVA_HOME := $(abspath $(ANDROID_JAVA_HOME))
-
-# Use the indexer wrapper to index the codebase instead of the javac compiler
-ifeq ($(ALTERNATE_JAVAC),)
-JAVACC := $(ANDROID_JAVA_TOOLCHAIN)/javac
-else
-JAVACC := $(ALTERNATE_JAVAC)
-endif
-
-JAVA := $(ANDROID_JAVA_TOOLCHAIN)/java
-JAVADOC := $(ANDROID_JAVA_TOOLCHAIN)/javadoc
-JAR := $(ANDROID_JAVA_TOOLCHAIN)/jar
-
-# The actual compiler can be wrapped by setting the JAVAC_WRAPPER var.
-ifdef JAVAC_WRAPPER
-    ifneq ($(JAVAC_WRAPPER),$(firstword $(JAVACC)))
-        JAVACC := $(JAVAC_WRAPPER) $(JAVACC)
-    endif
-endif
-
-COMMON_JAVAC := $(JAVACC) -J-Xmx2048M $(common_jdk_flags)
-
-GLOBAL_JAVAC_DEBUG_FLAGS := -g
-
-HOST_JAVAC ?= $(COMMON_JAVAC)
-TARGET_JAVAC ?= $(COMMON_JAVAC)
-
-#$(info HOST_JAVAC=$(HOST_JAVAC))
-#$(info TARGET_JAVAC=$(TARGET_JAVAC))
