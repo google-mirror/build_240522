6e22d5c0b3c14d2050664cebbce9f18eb2c69eec
Jeff Gaston <jeffrygaston@google.com>
2017-08-21 18:27:00 -0700

    Support 'tapas help' for more-detailed tapas help
    Bug: 64844028
    Test: tapas help
    Change-Id: I5629ca781e8cacfa6bb09828acf4160bbca3c3a5

---
+++ tapasHelp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# locate some directories
+cd "$(dirname $0)"
+SCRIPT_DIR="${PWD}"
+cd ../..
+TOP="${PWD}"
+
+message='usage: tapas [<App1> <App2> ...] [arm|x86|mips|armv5|arm64|x86_64|mips64] [eng|userdebug|user]
+
+tapas selects unbundled apps to be built by the Android build system.
+
+Unbundled apps are distinguished from bundled apps, because the former are
+built against the same SDK that is made available to third-party developers,
+and avoids the risk of such an app inadvertently accessing an internal API.
+
+The names <App1> <App2> ... should match LOCAL_PACKAGE_NAME as defined in an
+Android.mk
+
+The usage of the other arguments matches that of the rest of the platform
+build system and can be found by running `m help`'
+
+echo "$message"
