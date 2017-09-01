2fcbcd2d6ff81c0860e5fbb79156091df7694f89
Roland Levillain <rpl@google.com>
2017-09-01 14:29:23 +0000

    Revert "Revert "Revert "Add Turbine as part of java build toolchain in Make."""
    This is to address build breakages in
    - mirror-aosp-master-with-vendor/aosp_angler-userdebug
    - mirror-aosp-master-with-vendor/aosp_bullhead-userdebug
    - mirror-aosp-master-with-vendor/aosp_fugu-userdebug.
    
    This reverts commit b3ec534bd1f9a7046172fbad33afb378e3622c84.
    
    Change-Id: I9381b530092231e14412ed463a822ec70214c4ee

--- core/host_java_library.mk
+++ core/host_java_library.mk
@@ -35,7 +35,6 @@ full_classes_compiled_jar := $(intermediates.COMMON)/classes-full-debug.jar
 full_classes_jarjar_jar := $(intermediates.COMMON)/classes-jarjar.jar
 full_classes_jar := $(intermediates.COMMON)/classes.jar
 java_source_list_file := $(intermediates.COMMON)/java-source-list
-full_classes_header_jar := $(intermediates.COMMON)/classes-header.jar
 
 LOCAL_INTERMEDIATE_TARGETS += \
     $(full_classes_compiled_jar) \
@@ -84,7 +83,6 @@ $(full_classes_compiled_jar): \
     $(annotation_processor_deps) \
     $(NORMALIZE_PATH) \
     $(ZIPTIME) \
-    $(JAR_ARGS) \
     | $(SOONG_JAVAC_WRAPPER)
 	$(transform-host-java-to-package)
 	$(remove-timestamps-from-package)
@@ -112,4 +110,3 @@ include $(BUILD_SYSTEM)/jacoco.mk
 $(eval $(call copy-one-file,$(LOCAL_FULL_CLASSES_JACOCO_JAR),$(LOCAL_BUILT_MODULE)))
 $(eval $(call copy-one-file,$(LOCAL_FULL_CLASSES_JACOCO_JAR),$(full_classes_jar)))
 
-$(eval $(call copy-one-file,$(LOCAL_FULL_CLASSES_JACOCO_JAR),$(full_classes_header_jar)))
