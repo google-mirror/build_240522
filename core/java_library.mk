c5e34dbbb279c84c809fe63b67c594a46ecc3818
Nicolas Geoffray <ngeoffray@google.com>
2017-09-19 13:10:31 +0100

    Add a product variable for priv-apps dependencies.
    Those dependencies are jars/apks on which we want to
    uncompress their dexs.
    
    bug: 30972906
    bug: 63920015
    
    Test: sailfish build
    
    Change-Id: Ic96ffe9dbe39abc1c28e7de134892d689207c9ca

--- core/java_library.mk
+++ core/java_library.mk
@@ -80,7 +80,7 @@ else # !LOCAL_IS_STATIC_JAVA_LIBRARY
 $(common_javalib.jar): PRIVATE_DEX_FILE := $(built_dex)
 $(common_javalib.jar): PRIVATE_SOURCE_ARCHIVE := $(full_classes_pre_proguard_jar)
 $(common_javalib.jar): PRIVATE_DONT_DELETE_JAR_DIRS := $(LOCAL_DONT_DELETE_JAR_DIRS)
-$(common_javalib.jar) : $(built_dex) $(java_resource_sources) | $(ZIPTIME)
+$(common_javalib.jar) : $(built_dex) $(java_resource_sources) | $(ZIPTIME) $(ZIPALIGN)
 	@echo "target Jar: $(PRIVATE_MODULE) ($@)"
 ifdef LOCAL_JACK_ENABLED
 	$(call create-empty-package-at,$@.tmp)
@@ -93,6 +93,10 @@ ifdef LOCAL_JACK_ENABLED
 endif
 	$(hide) $(ZIPTIME) $@.tmp
 	$(call commit-change-for-toc,$@)
+ifneq (,$(filter $(PRODUCT_LOADED_BY_PRIVILEGED_MODULES), $(LOCAL_MODULE)))
+	$(uncompress-dexs)
+	$(align-package)
+endif  # PRODUCT_LOADED_BY_PRIVILEGED_MODULES
 
 .KATI_RESTAT: $(common_javalib.jar)
 
