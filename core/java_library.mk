0d62b1face0b1a32db50fbd880372502344d7b88
Nicolas Geoffray <ngeoffray@google.com>
2017-09-19 13:10:31 +0100

    Add a product variable for priv-apps dependencies.
    Those dependencies are jars/apks on which we want to
    uncompress their dexs.
    
    bug: 30972906
    bug: 63920015
    
    Test: sailfish build
    
    Change-Id: Ic96ffe9dbe39abc1c28e7de134892d689207c9ca

--- core/java_library.mk
+++ core/java_library.mk
@@ -93,6 +93,9 @@ ifdef LOCAL_JACK_ENABLED
 endif
 	$(hide) $(ZIPTIME) $@.tmp
 	$(call commit-change-for-toc,$@)
+ifneq (,$(filter $(PRODUCT_LOADED_BY_PRIVILEGED_MODULES), $(LOCAL_MODULE)))
+	$(uncompress-dexs)
+endif  # PRODUCT_LOADED_BY_PRIVILEGED_MODULES
 
 .KATI_RESTAT: $(common_javalib.jar)
 
