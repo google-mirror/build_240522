cd1f2930ff47eec82eeb5672c3ecff725cae4a8a
Jaekyun Seok <jaekyun@google.com>
2017-11-27 17:04:47 +0900

    Support product partition
    This CL adds the following build flags to support building product partition
    from Android build system.
    - BOARD_AVB_PRODUCT_ADD_HASHTREE_FOOTER_ARGS
    - BOARD_AVB_PRODUCT_KEY_PATH
    - BOARD_AVB_PRODUCT_ALGORITHM
    - BOARD_AVB_PRODUCT_ROLLBACK_INDEX_LOCATION
    - BOARD_PREBUILT_PRODUCTIMAGE
    - BOARD_PRODUCTIMAGE_EXTFS_INODE_COUNT
    - BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE
    - BOARD_PRODUCTIMAGE_PARTITION_SIZE
    - BOARD_PRODUCTIMAGE_SQUASHFS_BLOCK_SIZE
    - BOARD_PRODUCTIMAGE_SQUASHFS_COMPRESSOR
    - BOARD_PRODUCTIMAGE_SQUASHFS_COMPRESSOR_OPT
    - BOARD_PRODUCTIMAGE_SQUASHFS_DISABLE_4K_ALIGN
    - BOARD_USES_PRODUCTIMAGE
    - LOCAL_PRODUCT_MODULE
    - PRODUCT_FOOTER_ARGS
    - PRODUCT_PRODUCT_BASE_FS_PATH
    - PRODUCT_PRODUCT_VERITY_PARTITION
    - TARGET_COPY_OUT_PRODUCT
    
    Bug: 64195575
    Test: succeeded building oem.img with BOARD_OEMIMAGE_PARTITION_SIZE,
    BOARD_OEMIMAGE_FILE_SYSTEM_TYPE and PRODUCT_OEM_VERITY_PARTITION.
    Change-Id: I3f42ec19d609d98fd1576951ac79effd4170f2ae

--- core/local_vsdk.mk
+++ core/local_vsdk.mk
@@ -2,7 +2,7 @@
 ifdef BOARD_VSDK_VERSION
 # Set LOCAL_SDK_VERSION to system_current, If LOCAL_SDK_VERSION is not defined and LOCAL_VENDOR_MODULE is true
   _is_vendor_app :=
-  ifneq (,$(filter true,$(LOCAL_VENDOR_MODULE) $(LOCAL_ODM_MODULE) $(LOCAL_OEM_MODULE) $(LOCAL_PROPRIETARY_MODULE)))
+  ifneq (,$(filter true,$(LOCAL_VENDOR_MODULE) $(LOCAL_ODM_MODULE) $(LOCAL_OEM_MODULE) $(LOCAL_PROPRIETARY_MODULE) $(LOCAL_PRODUCT_MODULE)))
     _is_vendor_app := true
   else
     ifneq (,$(filter $(TARGET_OUT_VENDOR)%,$(LOCAL_MODULE_PATH) $(LOCAL_MODULE_PATH_32) $(LOCAL_MODULE_PATH_64)))
