ba106fd4bddf8ef38e6d8ee6e15d07bae0fc74b8
Sundong Ahn <sundongahn@google.com>
2017-12-01 19:20:30 +0900

    Add link type check for java modules
    Vendor-to-system dependency must not be allowed for system-only-OTA. So
    any java module (my_modulle_is_vendor_module := true) in the vendor
    partition is tagged as java:vendor and java vendor can link to other
    java:vendor or link to static libraries which are java:sdk and java:system.
    Static link type check is added to check static link.
    
    Bug: 69899800
    Test: 1. make -j40 && boot on pixel
    2. BOARD_SYSTEM_SDK_VERSION := current && make -j40 && check error
    message about link type check.
    
    Change-Id: Ie57edcc0fcc94003a955a15fe241afc5a82f485a

--- core/local_systemsdk.mk
+++ core/local_systemsdk.mk
@@ -1,17 +1,17 @@
 
-ifdef BOARD_VSDK_VERSION
+ifdef BOARD_ENFORCE_SYSTEM_SDK
 # Set LOCAL_SDK_VERSION to system_current, If LOCAL_SDK_VERSION is not defined and LOCAL_VENDOR_MODULE is true
-  _is_vendor_app :=
+  my_vendor_module :=
   ifneq (,$(filter true,$(LOCAL_VENDOR_MODULE) $(LOCAL_ODM_MODULE) $(LOCAL_OEM_MODULE) $(LOCAL_PROPRIETARY_MODULE)))
-    _is_vendor_app := true
+    my_vendor_module := true
   else
     ifneq (,$(filter $(TARGET_OUT_VENDOR)%,$(LOCAL_MODULE_PATH) $(LOCAL_MODULE_PATH_32) $(LOCAL_MODULE_PATH_64)))
-      _is_vendor_app := true
+      my_vendor_module := true
     endif
   endif
   ifneq (,$(filter JAVA_LIBRARIES APPS,$(LOCAL_MODULE_CLASS)))
     ifndef LOCAL_SDK_VERSION
-      ifeq ($(_is_vendor_app),true)
+      ifeq ($(my_vendor_module),true)
         LOCAL_SDK_VERSION := system_current
       endif
     endif
