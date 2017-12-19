7c22184d75ddffb0ee6942a81d6469a110c6c087
Dan Albert <danalbert@google.com>
2017-12-19 11:27:39 -0800

    Also restrict NDK linking by static/shared STL.
    Test: make native
    Bug: None
    Change-Id: I0eb29ec483c069d1c12f444fd2e94cc6eb320f93

--- core/allowed_ndk_types.mk
+++ core/allowed_ndk_types.mk
@@ -1,27 +1,39 @@
 # Determines the types of NDK modules the current module is allowed to link to.
 # Input variables:
-#   ???
+#   LOCAL_MODULE
+#   LOCAL_MODULE_CLASS
+#   LOCAL_NDK_STL_VARIANT
+#   LOCAL_SDK_VERSION
 # Output variables:
 #   my_ndk_stl_family: Family of the NDK STL.
+#   my_ndk_stl_link_type: STL link type, static or shared.
 #   my_allowed_ndk_types: Types of NDK modules that may be linked.
 
 ifdef LOCAL_SDK_VERSION
     ifeq ($(LOCAL_NDK_STL_VARIANT),)
         my_ndk_stl_family := system
+        my_ndk_stl_link_type := shared
     else ifeq ($(LOCAL_NDK_STL_VARIANT),system)
         my_ndk_stl_family := system
+        my_ndk_stl_link_type := shared
     else ifeq ($(LOCAL_NDK_STL_VARIANT),c++_shared)
         my_ndk_stl_family := libc++
+        my_ndk_stl_link_type := shared
     else ifeq ($(LOCAL_NDK_STL_VARIANT),c++_static)
         my_ndk_stl_family := libc++
+        my_ndk_stl_link_type := static
     else ifeq ($(LOCAL_NDK_STL_VARIANT),gnustl_static)
         my_ndk_stl_family := gnustl
+        my_ndk_stl_link_type := static
     else ifeq ($(LOCAL_NDK_STL_VARIANT),stlport_shared)
         my_ndk_stl_family := stlport
+        my_ndk_stl_link_type := shared
     else ifeq ($(LOCAL_NDK_STL_VARIANT),stlport_static)
         my_ndk_stl_family := stlport
+        my_ndk_stl_link_type := static
     else ifeq ($(LOCAL_NDK_STL_VARIANT),none)
         my_ndk_stl_family := none
+        my_ndk_stl_link_type := none
     else
         $(error $(LOCAL_MODULE): invalid LOCAL_NDK_STL_VARIANT: $(LOCAL_NDK_STL_VARIANT))
     endif
@@ -45,7 +57,19 @@ ifdef LOCAL_SDK_VERSION
     ifeq ($(LOCAL_MODULE_CLASS),APPS)
         my_allowed_ndk_types += \
             native:ndk:gnustl native:ndk:libc++ native:ndk:stlport
+
+    ifeq ($(LOCAL_MODULE_CLASS),STATIC_LIBRARIES)
+        # Since static libraries are not actually linked, the decision between
+        # static/shared isn't actually meaningful here. Only the family of the
+        # STL matters.
+        my_allowed_ndk_types += native:ndk:$(my_ndk_stl_family):shared
+        my_allowed_ndk_types += native:ndk:$(my_ndk_stl_family):static
+    else ifeq ($(my_ndk_stl_link_type),shared)
+        # Modules linked a shared STL can only use another shared STL.
+        my_allowed_ndk_types += native:ndk:$(my_ndk_stl_family):shared
     endif
+    # Else we are a non-static library that uses a static STL, and are
+    # incompatible with all other shared libraries that use an STL.
 else
     my_allowed_ndk_types := native:ndk:none native:ndk:system
 endif
