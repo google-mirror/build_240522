6d68c9a92b2108df3d9e0960cf31bac6d46ca692
Dan Albert <danalbert@google.com>
2017-12-19 11:26:05 -0800

    Restrict NDK link type to matching STLs.
    Test: make native
    Bug: None
    Change-Id: Ie9d9107fe0eeb425843ae2db197e1c60d14a59ca

---
+++ core/allowed_ndk_types.mk
@@ -0,0 +1,54 @@
+# Determines the types of NDK modules the current module is allowed to link to.
+# Input variables:
+#   LOCAL_MODULE
+#   LOCAL_MODULE_CLASS
+#   LOCAL_NDK_STL_VARIANT
+#   LOCAL_SDK_VERSION
+# Output variables:
+#   my_ndk_stl_family: Family of the NDK STL.
+#   my_allowed_ndk_types: Types of NDK modules that may be linked.
+
+ifdef LOCAL_SDK_VERSION
+    ifeq ($(LOCAL_NDK_STL_VARIANT),)
+        my_ndk_stl_family := system
+    else ifeq ($(LOCAL_NDK_STL_VARIANT),system)
+        my_ndk_stl_family := system
+    else ifeq ($(LOCAL_NDK_STL_VARIANT),c++_shared)
+        my_ndk_stl_family := libc++
+    else ifeq ($(LOCAL_NDK_STL_VARIANT),c++_static)
+        my_ndk_stl_family := libc++
+    else ifeq ($(LOCAL_NDK_STL_VARIANT),gnustl_static)
+        my_ndk_stl_family := gnustl
+    else ifeq ($(LOCAL_NDK_STL_VARIANT),stlport_shared)
+        my_ndk_stl_family := stlport
+    else ifeq ($(LOCAL_NDK_STL_VARIANT),stlport_static)
+        my_ndk_stl_family := stlport
+    else ifeq ($(LOCAL_NDK_STL_VARIANT),none)
+        my_ndk_stl_family := none
+    else
+        $(error $(LOCAL_MODULE): invalid LOCAL_NDK_STL_VARIANT: $(LOCAL_NDK_STL_VARIANT))
+    endif
+    my_allowed_ndk_types :=
+
+    # The system STL is only the C++ ABI layer, so it's compatible with any STL.
+    my_allowed_ndk_types += native:ndk:system
+
+    # Libaries that don't use the STL can be linked to anything.
+    my_allowed_ndk_types += native:ndk:none
+
+    # And it's okay to link your own STL type. Strictly speaking there are more
+    # restrictions depending on static vs shared STL, but that will be a follow
+    # up patch.
+    my_allowed_ndk_types += native:ndk:$(my_ndk_stl_family)
+
+    # For an app package, it's actually okay to depend on any set of STLs. If
+    # any of the individual libraries depend on each other they've already been
+    # checked for consistency, and if they don't they'll be kept isolated by
+    # RTLD_LOCAL anyway.
+    ifeq ($(LOCAL_MODULE_CLASS),APPS)
+        my_allowed_ndk_types += \
+            native:ndk:gnustl native:ndk:libc++ native:ndk:stlport
+    endif
+else
+    my_allowed_ndk_types := native:ndk:none native:ndk:system
+endif
