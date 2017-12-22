15082a4e30176f20a94990777d50eeda26f94bac
Jiyong Park <jiyong@google.com>
2017-12-22 17:57:27 +0900

    Fix dangling rule
    The newly added check_emu_boot rule requires userdata.img regardless of
    whether the target product builds the image or not, and this is detected
    by soong as a dangline rule, which stops the build.
    
    Dependencies in out found with no rule to create them:
    out/target/product/generic_arm64_ab/userdata.img
    
    Fix this by requiring userdata.img only when the target product actually
    builds it (= BOARD_USERDATAIMAGE_PARTITION_SIZE is set)
    
    Test: choosecombo 1 aosp_arm64_ab userdebug
          m -j checkbuild tests
    
    Change-Id: Icb92b1dfe2ec797dab5b8afe4c7803f28aaa2bf0

--- core/tasks/check_emu_boot.mk
+++ core/tasks/check_emu_boot.mk
@@ -5,7 +5,8 @@ $(check_emu_boot0) : PRIVATE_BOOT_COMPLETE_STRING := "emulator: INFO: boot compl
 $(check_emu_boot0) : PRIVATE_BOOT_FAIL_STRING := "emulator: ERROR: fail to boot after"
 $(check_emu_boot0) : PRIVATE_SUCCESS_FILE := $(DIST_DIR)/$(PRIVATE_PREFIX)-BOOT-SUCCESS.txt
 $(check_emu_boot0) : PRIVATE_FAIL_FILE := $(DIST_DIR)/$(PRIVATE_PREFIX)-BOOT-FAIL.txt
-$(check_emu_boot0) : $(INSTALLED_QEMU_SYSTEMIMAGE)  $(INSTALLED_QEMU_VENDORIMAGE) $(PRODUCT_OUT)/userdata.img \
+$(check_emu_boot0) : $(INSTALLED_QEMU_SYSTEMIMAGE)  $(INSTALLED_QEMU_VENDORIMAGE) \
+                 $(if $(BOARD_USERDATAIMAGE_PARTITION_SIZE),$(PRODUCT_OUT)/userdata.img) \
                  $(PRODUCT_OUT)/ramdisk.img device/generic/goldfish/tools/emulator_boot_test.sh
 	@mkdir -p $(dir $(check_emu_boot0))
 	$(hide) rm -f $(check_emu_boot0)
