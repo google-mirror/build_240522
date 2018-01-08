88d6af4b5c0e2840cf7506511e7ae7eb0a8ac695
Dan Shi <dshi@google.com>
2018-01-08 14:16:51 -0800

    Stop *ts suites from auto-generating test configs.
    Bug: 71714007
    Test: m -j <module for *ts suite>
    m -j NotificationStressTests
    confirm NotificationStressTests.config generated
    
    Change-Id: I372a227a07633ab8d335a8435baf5dd6081bb27e

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -542,9 +542,9 @@ ifeq (,$(test_config))
       is_instrumentation_test := false
     endif
   endif
-  # CTS modules can be used for test data, so test config files must be
-  # explicitly created using AndroidTest.xml
-  ifeq (,$(filter cts, $(LOCAL_COMPATIBILITY_SUITE)))
+  # Only allow auto-generating test config files if the module belongs to
+  # device-tests or general-tests suite.
+  ifeq (,$(filter-out device-tests general-tests,$(LOCAL_COMPATIBILITY_SUITE)))
     ifeq (true, $(filter true,$(is_native) $(is_instrumentation_test)))
       include $(BUILD_SYSTEM)/autogen_test_config.mk
       test_config := $(autogen_test_config_file)
