fa6a358a81d8dfa5e41825c9286995a28900911a
Bruno Martins <bgcngm@gmail.com>
2018-02-23 10:58:45 +0000

    Aggregate all filesystems tools in a dedicated section
    Change-Id: I412a9ed4bbdec07a80715d7991d8fe66917e97e2

--- target/product/core_tiny.mk
+++ target/product/core_tiny.mk
@@ -71,10 +71,6 @@ PRODUCT_PACKAGES += \
     libwilhelm \
     libdrmframework_jni \
     libdrmframework \
-    mke2fs \
-    e2fsck \
-    resize2fs \
-    tune2fs \
     nullwebview \
     screencap \
     sensorservice \
@@ -88,6 +84,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     wifi-service \
     wificond \
+
+# Filesystems tools
+    e2fsck \
+    mke2fs \
+    resize2fs \
+    tune2fs
 
 ifeq ($(TARGET_CORE_JARS),)
 $(error TARGET_CORE_JARS is empty; cannot initialize PRODUCT_BOOT_JARS variable)
