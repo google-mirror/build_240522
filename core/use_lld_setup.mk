c4ce0b4be9bebcc77175ede6adbd1e7c5f02b1a5
Chih-Hung Hsieh <chh@google.com>
2018-03-27 15:30:59 -0700

    DO NOT SUBMIT. test USE_CLANG_LLD
    * Set USE_CLANG_LLD to true to run all tests.
    
    Bug: 73768157
    Test: make checkbuild and boot
    Change-Id: Idcf2d2333bd3e996e683c213efb081d777713cff

--- core/use_lld_setup.mk
+++ core/use_lld_setup.mk
@@ -4,6 +4,9 @@
 ## Output variables: my_use_clang_lld
 #############################################################
 
+# FAKED for TreeHugger Test. DO NOT SUBMIT.
+USE_CLANG_LLD := true
+
 # Use LLD only if it's not disabled by LOCAL_USE_CLANG_LLD,
 # and enabled by LOCAL_USE_CLANG_LLD or USE_CLANG_LLD.
 my_use_clang_lld := false
