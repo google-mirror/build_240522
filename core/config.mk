827c0692fe534f672668608807685e73bbf406e8
Yifan Hong <elsk@google.com>
2018-05-18 17:46:04 -0700

    Add BOARD_USE_LOGICAL_PARTITIONS
    Test: Set a device with this variable, boot,
          examine /proc/cmdline
    
    Fixes: 79885414
    
    Change-Id: I593b457786f3cf1bc50dedbc9b603e5408461bcc

--- core/config.mk
+++ core/config.mk
@@ -913,6 +913,17 @@ PLATFORM_SEPOLICY_COMPAT_VERSIONS := \
     PLATFORM_SEPOLICY_VERSION \
     TOT_SEPOLICY_VERSION \
 
+ifndef BOARD_USE_LOGICAL_PARTITIONS
+  BOARD_USE_LOGICAL_PARTITIONS := false
+endif
+# Logical and Resizable Partitions feature flag. Set this to true if a board should enable
+# this feature.
+.KATI_READONLY := BOARD_USE_LOGICAL_PARTITIONS
+
+ifeq ($(BOARD_USE_LOGICAL_PARTITIONS),true)
+  BOARD_KERNEL_CMDLINE += androidboot.lrap=1
+endif
+
 # ###############################################################
 # Set up final options.
 # ###############################################################
