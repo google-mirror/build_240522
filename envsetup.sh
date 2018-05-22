f3b0acc700738c22ded5fa5f934aa4bb04ac855a
Kevin Cheng <kevcheng@google.com>
2018-05-22 14:07:50 -0700

    Add acloud shortcut in envsetup.sh
    Bug: None
    Test: source envsetup.sh and ran acloud successfully.
    Change-Id: Ie75c2a6ea002fe3d909afd1760f4ab05c7c94a26

--- envsetup.sh
+++ envsetup.sh
@@ -1588,6 +1588,25 @@ function validate_current_shell() {
     esac
 }
 
+function acloud()
+{
+    # Let's use the built version over the prebuilt.
+    local built_acloud=$(which acloud)
+    if [ ! -z $built_acloud ]; then
+        $built_acloud "$@"
+        return $?
+    fi
+
+    local host_os_arch=$(get_build_var HOST_OS)-$(get_build_var HOST_ARCH)
+    case $host_os_arch in
+        linux-x86|linux-x86_64) "$(gettop)"/prebuilts/asuite/acloud/linux-x86/acloud "$@"
+        ;;
+    *)
+        echo "acloud is not supported on your host arch: $host_os_arch"
+        ;;
+    esac
+}
+
 # Execute the contents of any vendorsetup.sh files we can find.
 function source_vendorsetup() {
     for dir in device vendor product; do
