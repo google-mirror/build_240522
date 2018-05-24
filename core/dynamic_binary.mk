da57ccb016cfc350be2b4deaf33452ecb6e8e391
Chih-Hung Hsieh <chh@google.com>
2018-05-24 15:13:19 -0700

    Call lld with --pack-dyn-relocs=none if my_pack_dyn_relocs is false.
    * Factor out the logic that sets up my_pack_module_relocations
      from dynamic_binary.mk to pack_dyn_relocs_setup.mk.
    * Copy my_pack_module_relocations to my_pack_dyn_relocs before
      my_pack_module_relocations is set to false when using clang lld.
    
    Bug: 80093890
    Bug: 73768157
    Test: build and boot with USE_CLANG_LLD=true
    Change-Id: I7c4b5fcdce0754c57cff4acf86185cac65a26c40

--- core/dynamic_binary.mk
+++ core/dynamic_binary.mk
@@ -44,33 +44,7 @@ include $(BUILD_SYSTEM)/binary.mk
 relocation_packer_input := $(linked_module)
 relocation_packer_output := $(intermediates)/PACKED/$(my_built_module_stem)
 
-my_pack_module_relocations := false
-ifneq ($(DISABLE_RELOCATION_PACKER),true)
-    my_pack_module_relocations := $(firstword \
-      $(LOCAL_PACK_MODULE_RELOCATIONS_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) \
-      $(LOCAL_PACK_MODULE_RELOCATIONS))
-endif
-
-ifeq ($(my_pack_module_relocations),)
-  my_pack_module_relocations := $($(LOCAL_2ND_ARCH_VAR_PREFIX)TARGET_PACK_MODULE_RELOCATIONS)
-endif
-
-# Do not pack relocations for executables. Because packing results in
-# non-zero p_vaddr which causes kernel to load executables to lower
-# address (starting at 0x8000) http://b/20665974
-ifneq ($(filter EXECUTABLES NATIVE_TESTS,$(LOCAL_MODULE_CLASS)),)
-  my_pack_module_relocations := false
-endif
-
-# TODO (dimitry): Relocation packer is not yet available for darwin
-ifneq ($(HOST_OS),linux)
-  my_pack_module_relocations := false
-endif
-
-# Relocation packer does not work with LLD yet.
-ifeq ($(my_use_clang_lld),true)
-  my_pack_module_relocations := false
-endif
+include $(BUILD_SYSTEM)/pack_dyn_relocs_setup.mk
 
 ifeq (true,$(my_pack_module_relocations))
 # Pack relocations
