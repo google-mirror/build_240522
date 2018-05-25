7a24cd0aa8539d07b81fa9b7a6b3f5b27d97a8c3
Jaekyun Seok <jaekyun@google.com>
2018-05-25 14:26:39 +0900

    Support installing a RRO package into product partition
    Test: succeeded building
    Change-Id: Iac4c6275c123064e295aa0b38d5068c4ad7b9b18

--- core/build_rro_package.mk
+++ core/build_rro_package.mk
@@ -15,10 +15,24 @@ ifneq ($(LOCAL_SRC_FILES),)
   $(error runtime resource overlay package should not contain sources)
 endif
 
-ifeq ($(LOCAL_RRO_THEME),)
-  LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/overlay
+ifeq (true,$(LOCAL_VENDOR_MODULE))
+  my_target_out := $(TARGET_OUT_VENDOR)
+else ifeq (true,$(LOCAL_PROPRIETARY_MODULE))
+  my_target_out := $(TARGET_OUT_VENDOR)
+else ifeq (true,$(LOCAL_OEM_MODULE))
+  $(error $(LOCAL_MODULE_MAKEFILE): $(LOCAL_MODULE): runtime resource overlay package should not be installed in oem/overlay)
+else ifeq (true,$(LOCAL_ODM_MODULE))
+  $(error $(LOCAL_MODULE_MAKEFILE): $(LOCAL_MODULE): runtime resource overlay package should not be installed in odm/overlay)
+else ifeq (true,$(LOCAL_PRODUCT_MODULE))
+  my_target_out := $(TARGET_OUT_PRODUCT)
 else
-  LOCAL_MODULE_PATH := $(TARGET_OUT_VENDOR)/overlay/$(LOCAL_RRO_THEME)
+  my_target_out := $(TARGET_OUT_VENDOR)
+endif
+
+ifeq ($(LOCAL_RRO_THEME),)
+  LOCAL_MODULE_PATH := $(my_target_out)/overlay
+else
+  LOCAL_MODULE_PATH := $(my_target_out)/overlay/$(LOCAL_RRO_THEME)
 endif
 
 include $(BUILD_SYSTEM)/package.mk
