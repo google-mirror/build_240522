967cd49671b51b009e03c6181b2cb088889878b4
Anton Hansson <hansson@google.com>
2018-06-01 14:18:14 +0100

    Add a new macro for products making isolation claims.
    When a product makes an isolation claim, the build system will verify
    that all its artifacts are produced inside its isolation claim, and
    fail otherwise.
    
    The claims are all relative to PRODUCT_OUT, and the macro also supports
    giving a whitelist, for known existing offenders in the hierarchy. The
    build will fail if redundant whitelist entries are present.
    
    Example invocation:
    _claims := $(TARGET_COPY_OUT_SYSTEM)/
    _whitelist := root/init root/init.rc
    $(call make-isolation-claim, $(_claims), $(_whitelist))
    
    Bug: 80410283
    Test: Downstream with a new product definition.
    Change-Id: I235de681f65254087a42e345af69b2113b682072

--- core/main.mk
+++ core/main.mk
@@ -921,6 +921,32 @@ define product-installed-files
   $(call module-installed-files, $(_pif_modules))
 endef
 
+# Verify the isolation claims made by included products.
+$(foreach makefile,$(ISOLATION_CLAIM_PRODUCTS),\
+  $(eval claims := $(PRODUCTS.$(makefile).ISOLATION_CLAIMS)) \
+  $(eval whitelist := $(PRODUCTS.$(makefile).ISOLATION_WHITELIST)) \
+  $(eval files := $(call product-installed-files, $(makefile))) \
+  $(eval files := $(filter-out $(HOST_OUT)/%,$(files))) \
+  $(eval ok_paths := $(foreach claim,$(claims),$(PRODUCT_OUT)/$(claim)%)) \
+  $(eval ok_paths += $(foreach f,$(whitelist),$(PRODUCT_OUT)/$(f))) \
+  $(eval ok_paths := $(subst $(_product_path_placeholder),$(TARGET_COPY_OUT_PRODUCT),$(ok_paths))) \
+  $(eval ok_paths := $(subst $(_vendor_path_placeholder),$(TARGET_COPY_OUT_VENDOR),$(ok_paths))) \
+  $(eval offending_files := $(filter-out $(ok_paths),$(files))) \
+  $(if $(offending_files), \
+    $(warning $(makefile) produces files outside its isolation claim.) \
+    $(info Offending files:) \
+    $(foreach f,$(sort $(offending_files)),$(info    $(f))) \
+    $(error Build failed) \
+  ) \
+  $(eval unused_whitelist := $(filter-out $(files),$(foreach f,$(whitelist),$(PRODUCT_OUT)/$(f)))) \
+  $(if $(unused_whitelist), \
+    $(warning $(makefile) includes redundant whitelist entries in its isolation claim.) \
+    $(info Offending entries:) \
+    $(foreach f,$(sort $(unused_whitelist)),$(info    $(f))) \
+    $(error Build failed) \
+  ) \
+)
+
 ifdef FULL_BUILD
   product_FILES := $(call product-installed-files, $(INTERNAL_PRODUCT))
 else
