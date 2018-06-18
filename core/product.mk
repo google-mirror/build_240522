c4ff640b260dd596a333098467e549ef2e76e7f7
Yifan Hong <elsk@google.com>
2018-06-18 16:32:35 -0700

    build_image.py recognize BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE
    - Copy "use_logical_partitions" to *_image_info.txt before sending
      it to build_image.py, so that the script can use this variable.
    
    - build_image.py emits an additional properties file to inform
      the build system about the system image size.
    
    Test: `make dist`
    
    Test: `make dist` with the following:
        - install a large file to system image
        fails as expected (because _PARTITION_SIZE is exceeded)
    
    Test: `make dist` with the following:
        - set PRODUCT_USE_LOGICAL_PARTITIONS to true
        - set BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE
        fails as expected (BOARD_SYSTEMIMAGE_PARTITION_SIZE needs
        to be undefined)
    
    Test: `make dist` with the following:
        - install a large file to system image
        - set PRODUCT_USE_LOGICAL_PARTIIONS to true
        - add a small BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE
        - remove BOARD_SYSTEMIMAGE_PARTITION_SIZE
        build succeeds.
    
    Bug: 79106666
    
    Change-Id: I574062882acd1ecd633ac38c5a8c5351b90a32d8

--- core/product.mk
+++ core/product.mk
@@ -364,6 +364,9 @@ _product_stash_var_list += \
 	WITH_DEXPREOPT \
 	WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY
 
+_product_stash_var_list += \
+	BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE \
+
 #
 # Mark the variables in _product_stash_var_list as readonly
 #
