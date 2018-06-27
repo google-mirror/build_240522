1bfafab3e1580e6cddc6bcba2309fdcc68726dc4
Anton Hansson <hansson@google.com>
2018-06-27 18:30:07 +0100

    Add some experimental mainline targets.
    These are not really realistic at the moment, but it's nice to get some
    targets checked in that can be iterated upon.
    
    - mainline_system: base mk for mainline systemimage. It will evolve to
      inherit other mks than base, and probably get a bunch of things added.
    - mainline_system_arm: variant of mainline_system, specifically for
      the 'generic' device.
    - mainline_arm: experimental product that also includes the non-system
      parts of aosp. Useful to experiment with the mk split.
    
    Bug: 80410283
    Test: lunch mainline_arm && m nothing && lunch mainline_system_arm && m nothing
    Change-Id: I9beaa6f5f9c683733ed76a6d4e5c70b9376a5bb2
    Merged-In: I9beaa6f5f9c683733ed76a6d4e5c70b9376a5bb2

---
+++ target/product/mainline_system_arm.mk
@@ -0,0 +1,7 @@
+
+$(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
+
+PRODUCT_NAME := mainline_system_arm
+PRODUCT_DEVICE := generic
+PRODUCT_BRAND := generic
+PRODUCT_SHIPPING_API_LEVEL := 28
