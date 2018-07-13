59594430160da50f8aab9bd5a67be7b13b46113d
yangbill <yangbill@google.com>
2018-07-13 22:55:36 +0800

    ATest: Sync auto_test_config share same compat_files.
    auto_test_config will only be set once for the same
    $(my_prefix)$(LOCAL_MODULE_CLASS)_$(LOCAL_MODULE)
    ATest should also treat those modules as testable.
    
    BUG: 111397357
    BUG: 110820867
    
    Test: . build/envseup.sh
          lunch aosp_x86_64-eng
          make -j out/target/product/generic_x86_64/module-info.json
    
    Change-Id: I69895b6373476c0053f2c6e657770d606ccfbcd1

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -561,6 +561,13 @@ $(foreach suite, $(LOCAL_COMPATIBILITY_SUITE), \
     $(LOCAL_BUILT_MODULE):$(dir)/$(my_installed_module_stem))) \
   $(eval my_compat_dist_config_$(suite) := ))
 
+# Modules with same compat_files sync their auto_test_config
+ifdef $(my_prefix)$(LOCAL_MODULE_CLASS)_$(LOCAL_MODULE)_compat_files
+  ifdef $(my_prefix)$(LOCAL_MODULE_CLASS)_$(LOCAL_MODULE)_autogen
+    ALL_MODULES.$(my_register_name).auto_test_config := true
+  endif
+endif
+
 # Make sure we only add the files once for multilib modules.
 ifndef $(my_prefix)$(LOCAL_MODULE_CLASS)_$(LOCAL_MODULE)_compat_files
 $(my_prefix)$(LOCAL_MODULE_CLASS)_$(LOCAL_MODULE)_compat_files := true
