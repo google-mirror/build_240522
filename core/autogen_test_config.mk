8a09e7f68fe477de4dac3693c7a41b0eb1a1a972
yangbill <yangbill@google.com>
2018-07-13 22:55:36 +0800

    ATest: Sync auto_test_config for multilib modules
    auto_test_config will only be set once for the same
    $(my_prefix)$(LOCAL_MODULE_CLASS)_$(LOCAL_MODULE)
    ATest should also treat the multilib variant modules as testable.
    
    Fix auto_generated config may have chance to be set as 2nd_arch name
    
    BUG: 111397357
    BUG: 110820867
    BUG: 80298359
    
    Test: . build/envseup.sh
          lunch aosp_x86_64-eng
          make -j out/target/product/generic_x86_64/module-info.json
          (check module-info.json for hello_world_test)
          make -j hello_world_test
          cat out/target/product/generic_x86_64/testcases/hello_world_test/hello_world_test.config
    
    Change-Id: I1bc9e9e6821a776863ef83f3dbd4d47153e1e75e

--- core/autogen_test_config.mk
+++ core/autogen_test_config.mk
@@ -29,9 +29,10 @@ else
 autogen_test_config_template := $(NATIVE_TEST_CONFIG_TEMPLATE)
 endif
 # Auto generating test config file for native test
+$(autogen_test_config_file): PRIVATE_MODULE_NAME := $(LOCAL_MODULE)
 $(autogen_test_config_file) : $(autogen_test_config_template)
 	@echo "Auto generating test config $(notdir $@)"
-	$(hide) sed 's&{MODULE}&$(PRIVATE_MODULE)&g' $< > $@
+	$(hide) sed 's&{MODULE}&$(PRIVATE_MODULE_NAME)&g' $< > $@
 my_auto_generate_config := true
 else
 # Auto generating test config file for instrumentation test
@@ -48,10 +49,13 @@ my_auto_generate_config := true
 endif # ifneq (,$(full_android_manifest))
 endif # ifneq (true,$(is_native))
 
+
+
 ifeq (true,$(my_auto_generate_config))
   LOCAL_INTERMEDIATE_TARGETS += $(autogen_test_config_file)
   $(LOCAL_BUILT_MODULE): $(autogen_test_config_file)
   ALL_MODULES.$(my_register_name).auto_test_config := true
+  $(my_prefix)$(LOCAL_MODULE_CLASS)_$(LOCAL_MODULE)_autogen := true
 else
   autogen_test_config_file :=
 endif
