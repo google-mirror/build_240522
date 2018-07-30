ce4c12b2156bae0642ad192ae9d309abee5bd58c
SzuWei Lin <szuweilin@google.com>
2018-07-30 13:34:32 +0800

    Enable dynamic partition size without logical patition
    Originally, to enable BOARD_*_PARTITION_RESERVED_SIZE must also
    enable PRODUCT_USE_LOGICAL_PARTITIONS. The patch fix the rule to
    let dynamic partition size is able to be used independently.
    
    Bug: 111966003
    Test: `make systemimage` with the following:
        - install a large file to system image
        - add a small BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE
        - remove BOARD_SYSTEMIMAGE_PARTITION_SIZE
    
    Change-Id: Idf22230c599b5f88b8f3b8cd0da43afce949578d

--- tools/releasetools/build_image.py
+++ tools/releasetools/build_image.py
@@ -546,8 +546,7 @@ def BuildImage(in_dir, prop_dict, out_file, target_out=None):
   verity_supported = prop_dict.get("verity") == "true"
   verity_fec_supported = prop_dict.get("verity_fec") == "true"
 
-  if (prop_dict.get("use_logical_partitions") == "true" and
-      "partition_size" not in prop_dict):
+  if ("partition_size" not in prop_dict):
     # if partition_size is not defined, use output of `du' + reserved_size
     success, size = GetDiskUsage(in_dir)
     if not success:
