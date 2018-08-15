4712b74313c0e0ba92569aacd7bf7626a73e4ec9
Hridya Valsaraju <hridya@google.com>
2018-08-15 16:43:46 -0700

    set PRODUCT_USE_FASTBOOTD in mainline_system.mk
    Bug: 78793464
    Test: fastbootd binary present in recovery
    
    Change-Id: Ifef3d7224a41c74c5e9ae336d4bc774a3a8a9ad7

--- target/product/mainline_system.mk
+++ target/product/mainline_system.mk
@@ -17,6 +17,7 @@
 # This makefile is the basis of a generic system image for a handheld
 # device with no telephony.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/fastbootd.mk)
 
 # Enable dynamic partition size
 PRODUCT_USE_DYNAMIC_PARTITION_SIZE := true
@@ -27,48 +28,45 @@ PRODUCT_SHIPPING_API_LEVEL := 28
 
 _base_mk_whitelist := \
   recovery/root/etc/mke2fs.conf \
+  recovery/root/system/bin/fastbootd \
   recovery/root/system/bin/init \
   recovery/root/system/bin/ueventd \
+  recovery/root/system/lib64/android.hardware.boot@1.0.so \
+  recovery/root/system/lib64/hw/android.hardware.boot@1.0-impl.so \
+  recovery/root/system/lib64/hw/bootctrl.default.so \
   recovery/root/system/lib64/ld-android.so \
+  recovery/root/system/lib64/libadbd.so \
+  recovery/root/system/lib64/libadbd_services.so \
+  recovery/root/system/lib64/libasyncio.so \
   recovery/root/system/lib64/libbase.so \
+  recovery/root/system/lib64/libbootloader_message.so \
   recovery/root/system/lib64/libc++.so \
   recovery/root/system/lib64/libc.so \
   recovery/root/system/lib64/libcrypto.so \
+  recovery/root/system/lib64/libcrypto_utils.so \
   recovery/root/system/lib64/libcutils.so \
   recovery/root/system/lib64/libdl.so \
+  recovery/root/system/lib64/libext2_uuid.so \
+  recovery/root/system/lib64/libext4_utils.so \
+  recovery/root/system/lib64/libfec.so \
+  recovery/root/system/lib64/libfs_mgr.so \
+  recovery/root/system/lib64/libhardware.so \
+  recovery/root/system/lib64/libhidlbase.so \
+  recovery/root/system/lib64/libhidltransport.so \
+  recovery/root/system/lib64/libhwbinder.so \
+  recovery/root/system/lib64/libkeyutils.so \
   recovery/root/system/lib64/liblog.so \
+  recovery/root/system/lib64/liblogwrap.so \
+  recovery/root/system/lib64/liblp.so \
   recovery/root/system/lib64/libm.so \
+  recovery/root/system/lib64/libmdnssd.so \
   recovery/root/system/lib64/libpackagelistparser.so \
   recovery/root/system/lib64/libpcre2.so \
   recovery/root/system/lib64/libselinux.so \
-  recovery/root/system/lib64/libz.so \
-
-ifeq ($(PRODUCT_USE_FASTBOOTD), true)
-  _base_mk_whitelist += \
-    recovery/root/system/bin/fastbootd \
-    recovery/root/system/lib64/android.hardware.boot@1.0.so \
-    recovery/root/system/lib64/hw/bootctrl.default.so \
-    recovery/root/system/lib64/libadbd.so \
-    recovery/root/system/lib64/libadbd_services.so \
-    recovery/root/system/lib64/libasyncio.so \
-    recovery/root/system/lib64/libbootloader_message.so \
-    recovery/root/system/lib64/libcrypto_utils.so \
-    recovery/root/system/lib64/libext2_uuid.so \
-    recovery/root/system/lib64/libext4_utils.so \
-    recovery/root/system/lib64/libfec.so \
-    recovery/root/system/lib64/libfec_rs.so \
-    recovery/root/system/lib64/libfs_mgr.so \
-    recovery/root/system/lib64/libhidlbase.so \
-    recovery/root/system/lib64/libhidltransport.so \
-    recovery/root/system/lib64/libhwbinder.so \
-    recovery/root/system/lib64/libkeyutils.so \
-    recovery/root/system/lib64/liblogwrap.so \
-    recovery/root/system/lib64/liblp.so \
-    recovery/root/system/lib64/libmdnssd.so \
-    recovery/root/system/lib64/libsparse.so \
-    recovery/root/system/lib64/libsquashfs_utils.so \
-    recovery/root/system/lib64/libutils.so
-endif
+  recovery/root/system/lib64/libsparse.so \
+  recovery/root/system/lib64/libsquashfs_utils.so \
+  recovery/root/system/lib64/libutils.so \
+  recovery/root/system/lib64/libz.so
 
 _my_whitelist := $(_base_mk_whitelist)
 
