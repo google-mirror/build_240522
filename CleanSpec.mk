01f5f6aaf29caf8b6c97b538e0d3609cc8a3a40e
Tao Bao <tbao@google.com>
2018-08-17 16:27:01 -0700

    Install recovery etc files to recovery/root/system/etc.
    They used to be under recovery/root/etc. This CL moves the files to the
    new location and creates a symlink from /etc to /system/etc (done by the
    rule in system/core/rootdir). This gives similar layout between normal
    boot and recovery, and allows installing prebuilt_etc files with Soong
    (`recovery_available: true`).
    
    Bug: 112780007
    Test: Build with other changes in the topic (aosp_taimen-userdebug).
          Check the generated files under recovery (/etc being a symlink to
          /system/etc).
    Test: Boot into recovery. Verify basic functionalities (`adb shell` and
          `adb sideload`, factory reset).
    Change-Id: Ibb6dea6f179a339f0c2d0fd8ba05ec0085b79a12

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -499,6 +499,9 @@ $(call add-clean-step, rm -rf $(SOONG_OUT_DIR)/.intermediates/system/vold)
 $(call add-clean-step, find $(PRODUCT_OUT) -type f -name "*product-services*" -print0 | xargs -0 rm -rf)
 $(call add-clean-step, find $(PRODUCT_OUT) -type d -name "*product-services*" -print0 | xargs -0 rm -rf)
 
+# Remove obsolete recovery etc files
+$(call add-clean-step, rm -rf $(TARGET_RECOVERY_ROOT_OUT)/etc)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
