398b01c8a476831151306bbe679694cbbde27d25
Colin Cross <ccross@android.com>
2018-09-14 14:12:59 -0700

    Remove LOCAL_DONT_DELETE_JAR_DIRS
    There are no users left in Make.
    
    Test: m checkbuild
    Change-Id: I896fc6b889aab556c6983c374421e4c5381af54d

--- core/definitions.mk
+++ core/definitions.mk
@@ -2362,7 +2362,6 @@ define initialize-package-file
 @mkdir -p $(dir $(2))
 $(hide) cp -f $(1) $(2)
 $(hide) zip -qd $(2) "*.class" \
-    $(if $(strip $(PRIVATE_DONT_DELETE_JAR_DIRS)),,"*/") \
     || true # Ignore the error when nothing to delete.
 endef
 
