c564e7a40fe2aa6b0f63b5e06ac19d2ebf6084f3
Dan Shi <dshi@google.com>
2018-10-10 11:42:05 -0700

    Copy shared libraries used by tests to testcases directory
    This change collects the shared libraries files used by tests in
    suite, and deploy the files to testcases/lib*. The shared libraries then
    can be zipped in general-tests.zip and device-tests.zip. And the host
    test can be run in TradeFed host based on build artifacts.
    
    Bug: 111486845
    Test: m -j general-tests
    Confirm the test can run in testcases directory:
    out/host/linux-x86/testcases$ ./net_test_avrcp/x86_64/net_test_avrcp
    Confirm shared libraries exist in out/host/linux-x86/testcases/lib and lib64
    run: out/host/linux-x86/testcases/net_test_avrcp/x86_64/net_test_avrcp
    
    Change-Id: I4f9322118aa4891226ea318cbd1bcee6ca48b050

--- core/tasks/device-tests.mk
+++ core/tasks/device-tests.mk
@@ -20,8 +20,14 @@ device-tests-zip := $(PRODUCT_OUT)/device-tests.zip
 device-tests-list-zip := $(PRODUCT_OUT)/device-tests_list.zip
 $(device-tests-zip) : .KATI_IMPLICIT_OUTPUTS := $(device-tests-list-zip)
 $(device-tests-zip) : PRIVATE_device_tests_list := $(PRODUCT_OUT)/device-tests_list
-
+$(general_tests_zip) : PRIVATE_SHARED_LIB_FILES := $(COMPATIBILITY.general-tests.shared_lib.FILES)
 $(device-tests-zip) : $(COMPATIBILITY.device-tests.FILES) $(SOONG_ZIP)
+	$(hide) for shared_lib in $(PRIVATE_SHARED_LIB_FILES); do \
+	  src=$$(cut -f1 -d":" <<< $$shared_lib); \
+	  dest=$$(cut -f2 -d":" <<< $$shared_lib); \
+		mkdir -p $$(dirname $$dest); \
+		cp -fp $$src $$dest || echo Failed to copy $$src to $$dest; \
+	done
 	echo $(sort $(COMPATIBILITY.device-tests.FILES)) | tr " " "\n" > $@.list
 	grep $(HOST_OUT_TESTCASES) $@.list > $@-host.list || true
 	grep $(TARGET_OUT_TESTCASES) $@.list > $@-target.list || true
