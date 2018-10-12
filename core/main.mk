cde945a858d146b7832beb19218dc3b534b17584
Anton Hansson <hansson@google.com>
2018-10-12 16:15:49 +0100

    Allow a hint together with artifact path enforcement
    This enables specifying a product-specific error message, to
    accompany the otherwise potentially cryptic message.
    
    Bug: 117637311
    Bug: 80410283
    Test: downstream product
    Test: lunch mainline_arm64; m nothing
    Change-Id: I9953d44d6338e5a79c5ed651716d3e39647ec87a

--- core/main.mk
+++ core/main.mk
@@ -1036,7 +1036,9 @@ ifdef FULL_BUILD
     $(eval offending_files := $(filter-out $(whitelist_patterns),$(files_in_requirement))) \
     $(eval enforcement := $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_ENFORCE_ARTIFACT_PATH_REQUIREMENTS)) \
     $(if $(enforcement),\
-      $(call maybe-print-list-and-error,$(offending_files),$(INTERNAL_PRODUCT) produces files inside $(makefile)s artifact path requirement.) \
+      $(call maybe-print-list-and-error,$(offending_files),\
+        $(INTERNAL_PRODUCT) produces files inside $(makefile)s artifact path requirement. \
+        $(PRODUCTS.$(INTERNAL_PRODUCT).PRODUCT_ARTIFACT_PATH_REQUIREMENT_HINT)) \
       $(eval unused_whitelist := $(if $(filter true strict,$(enforcement)),$(filter-out $(extra_files),$(whitelist_patterns)))) \
       $(call maybe-print-list-and-error,$(unused_whitelist),$(INTERNAL_PRODUCT) includes redundant artifact path requirement whitelist entries.) \
     ) \
