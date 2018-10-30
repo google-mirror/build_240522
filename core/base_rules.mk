c8f9caa97be573390b5f926f662367925665ea3f
nelsonli <nelsonli@google.com>
2018-10-30 22:29:39 +0800

    Build System: Solve dependency problem for test
    1. A test can add a runtime dependent test module by just setting
       LOCAL_REQUIRED_MODULES or LOCAL_TARGET_REQUIRED_MODULES. Then the dependent test
       module will be copied to testcase folder.
    2. Solve some dependency problem like below:
       If a module named module_A.
         (a) Set their install path to $(my_prefix)OUT_TESTCASES.
             LOCAL_MODULE := module_A
             LOCAL_MODULE_PATH := $(TARGET_OUT_TESTCASES)/module_A
             LOCAL_COMPATIBILITY_SUITE := cts ... (not null)
         (b) Other modules set their LOCAL_TARGET_REQUIRED_MODULES to module_A
             LOCAL_TARGET_REQUIRED_MODULES := test_module_A
    Then, The following build break will happen because the overriding target.
    overriding commands for target `out/target/product/generic_arm64/testcases/module_A/module_A',
    previously defined at build/make/core/base_rules.mk:420
    
    Test: 1. (a) vi cts/tests/tests/text/Android.mk
             (b) add LOCAL_REQUIRED_MODULES := CtsPrintTestCases
             (c) m -j CtsTextTestCases
             (d) Then, CtsPrintTestCases should also be built to testcase folder like below.
                 ./target/product/generic_arm64/testcases/CtsPrintTestCases
          2. (a) Apply patch
                 https://android-review.googlesource.com/c/platform/platform_testing/+/797192
             (b) m -j hello_world_test_mk
             (c) The dependency error should not happen.
    
    Change-Id: I0ba0521c1c9376d256cb90bf08aabaf6f1b7ef36

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -559,9 +559,15 @@ multi_arch :=
 # The module itself.
 $(foreach suite, $(LOCAL_COMPATIBILITY_SUITE), \
   $(eval my_compat_dist_$(suite) := $(foreach dir, $(call compatibility_suite_dirs,$(suite),$(arch_dir)), \
-    $(LOCAL_BUILT_MODULE):$(dir)/$(my_installed_module_stem))) \
+    $(call compat-copy-pair,$(LOCAL_BUILT_MODULE),$(dir)/$(my_installed_module_stem)))) \
   $(eval my_compat_dist_config_$(suite) := ))
 
+# Copy to testcase folder
+LOCAL_DEFAULT_TEST_MODULE := $($(my_prefix)OUT_TESTCASES)/$(LOCAL_MODULE)$(arch_dir)/$(my_built_module_stem)
+$(LOCAL_INSTALLED_MODULE) : $(LOCAL_DEFAULT_TEST_MODULE)
+$(foreach suite, $(LOCAL_COMPATIBILITY_SUITE), \
+  $(eval my_compat_dist_$(suite) += \
+    $(call compat-copy-pair,$(LOCAL_BUILT_MODULE),$(LOCAL_DEFAULT_TEST_MODULE))))
 
 # Auto-generate build config.
 ifneq (,$(LOCAL_FULL_TEST_CONFIG))
@@ -614,25 +620,25 @@ else
       $(eval s := $(word 1,$(p))) \
       $(eval n := $(or $(word 2,$(p)),$(notdir $(word 1, $(p))))) \
       $(foreach dir, $(call compatibility_suite_dirs,$(suite)), \
-        $(s):$(dir)/$(n)))))
+        $(call compat-copy-pair,$(s),$(dir)/$(n))))))
 
   ifneq (,$(test_config))
     $(foreach suite, $(LOCAL_COMPATIBILITY_SUITE), \
       $(eval my_compat_dist_config_$(suite) += $(foreach dir, $(call compatibility_suite_dirs,$(suite)), \
-        $(test_config):$(dir)/$(LOCAL_MODULE).config)))
+        $(call compat-copy-pair,$(test_config),$(dir)/$(LOCAL_MODULE).config))))
   endif
 
   ifneq (,$(wildcard $(LOCAL_PATH)/DynamicConfig.xml))
     $(foreach suite, $(LOCAL_COMPATIBILITY_SUITE), \
       $(eval my_compat_dist_config_$(suite) += $(foreach dir, $(call compatibility_suite_dirs,$(suite)), \
-        $(LOCAL_PATH)/DynamicConfig.xml:$(dir)/$(LOCAL_MODULE).dynamic)))
+        $(call compat-copy-pair,$(LOCAL_PATH)/DynamicConfig.xml,$(dir)/$(LOCAL_MODULE).dynamic))))
   endif
 
   ifneq (,$(wildcard $(LOCAL_PATH)/$(LOCAL_MODULE)_*.config))
   $(foreach extra_config, $(wildcard $(LOCAL_PATH)/$(LOCAL_MODULE)_*.config), \
     $(foreach suite, $(LOCAL_COMPATIBILITY_SUITE), \
       $(eval my_compat_dist_config_$(suite) += $(foreach dir, $(call compatibility_suite_dirs,$(suite)), \
-        $(extra_config):$(dir)/$(notdir $(extra_config))))))
+        $(call compat-copy-pair,$(extra_config),$(dir)/$(notdir $(extra_config)))))))
   endif
 endif # $(my_prefix)$(LOCAL_MODULE_CLASS)_$(LOCAL_MODULE)_compat_files
 
@@ -654,7 +660,7 @@ $(foreach pair, $(my_test_data_file_pairs), \
   $(eval file := $(word 2,$(parts))) \
   $(foreach suite, $(LOCAL_COMPATIBILITY_SUITE), \
     $(eval my_compat_dist_$(suite) += $(foreach dir, $(call compatibility_suite_dirs,$(suite),$(arch_dir)), \
-      $(src_path):$(call append-path,$(dir),$(file))))))
+      $(call compat-copy-pair,$(src_path),$(call append-path,$(dir),$(file)))))))
 endif
 
 arch_dir :=
