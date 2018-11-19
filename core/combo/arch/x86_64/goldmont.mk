b8d4288617b91a67ba66aee629aa51058e277437
Shalini Salomi Bodapati <shalini.salomi.bodapati@intel.com>
2018-11-19 09:57:56 +0530

    Add x86 goldmont architecture
    Test: m checkbuild
    Change-Id: I74c7a63bbaba993eeb8bdd8c663ffa142c908279
    Signed-off-by: Shalini Salomi Bodapati <shalini.salomi.bodapati@intel.com>
    (cherry picked from commit f327f5f3f5d03471d69e757fe33899a7fd653f18)

---
+++ core/combo/arch/x86_64/goldmont.mk
@@ -0,0 +1,11 @@
+# Configuration for Linux on x86.
+# Generating binaries for goldmont processors.
+#
+ARCH_X86_HAVE_SSSE3  := true
+ARCH_X86_HAVE_SSE4   := true
+ARCH_X86_HAVE_SSE4_1 := true
+ARCH_X86_HAVE_SSE4_2 := true
+ARCH_X86_HAVE_AVX    := true
+ARCH_X86_HAVE_AVX2   := true
+ARCH_X86_HAVE_POPCNT := true
+ARCH_X86_HAVE_MOVBE  := true
