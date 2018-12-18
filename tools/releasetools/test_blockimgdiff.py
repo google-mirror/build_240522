0be84d66815ca155de5a9ce03bc9d36f3801112b
xunchang <xunchang@google.com>
2018-12-18 10:38:01 -0800

    Factor out the code to load block file map
    As we want to support file_map for DataImage class, we can reuse the
    same logic in SparseImage. To avoid duplications, factor out the
    common code and put it in the base class.
    
    Bug: 79617342
    Test: run validate_target_files on multiple target files
    Change-Id: I986adae3c097d7d5a7f67797901b1c9290bf8228

--- tools/releasetools/test_blockimgdiff.py
+++ tools/releasetools/test_blockimgdiff.py
@@ -16,8 +16,9 @@
 
 import common
 from blockimgdiff import (
-    BlockImageDiff, EmptyImage, HeapItem, ImgdiffStats, Transfer)
+    BlockImageDiff, EmptyImage, DataImage, HeapItem, ImgdiffStats, Transfer)
 from rangelib import RangeSet
+from sparse_img import SparseImage
 from test_utils import ReleaseToolsTestCase
 
 
@@ -261,3 +262,57 @@ class ImgdiffStatsTest(ReleaseToolsTestCase):
 
     self.assertRaises(AssertionError, imgdiff_stats.Log, "/system/app/app1.apk",
                       "invalid reason")
+
+
+class ImageLoadFileMapTest(ReleaseToolsTestCase):
+  """Checks the loaded block file map function for DataImage and SparseImage."""
+
+  @staticmethod
+  def _build_image_data():
+    # block 0 :    zeros
+    # blocks 1-3:  filled with 'a'
+    # block 4:     first 10 bytes 'b', followed by zeros
+    # blocks 5-8:  filled with 'c'
+    # blocks 9-10: zeros
+    data = ['\0' * 4096, 'a' * 4096 * 3, 'b' * 10 + '\0' * 4086, 'c' * 4096 * 4,
+            '\0' * 4096 * 2]
+    file_map_data = ["file_a 1-3", "file_c 5-8"]
+    file_map_fn = common.MakeTempFile(suffix=".map")
+    with open(file_map_fn, "wb") as f:
+      f.write('\n'.join(file_map_data))
+
+    return ''.join(data), file_map_fn
+
+  def test_LoadFileMap_dataImage(self):
+    image_data, file_map = self._build_image_data()
+
+    image = DataImage(image_data, pad=True, file_map_fn=file_map)
+
+    expected_file_map = {
+      "file_a": RangeSet.parse("1-3"),
+      "file_c": RangeSet.parse("5-8"),
+      "__ZERO": RangeSet.parse("0 9-10"),
+      "__NONZERO-0": RangeSet.parse("4")
+    }
+    self.assertEqual(expected_file_map, image.GetFileMap())
+
+  def test_LoadFileMap_sparseImage(self):
+    image_data, file_map = self._build_image_data()
+
+    raw_image_file = common.MakeTempFile(prefix="raw-", suffix=".img")
+    with open(raw_image_file, "wb") as f:
+      f.write(image_data)
+
+    simg_file = common.MakeTempFile(prefix="sparse-", suffix=".img")
+    common.RunAndCheckOutput(["img2simg", raw_image_file, simg_file])
+    image = SparseImage(simg_file, file_map_fn=file_map,
+                        clobbered_blocks=[0, 1])
+
+    expected_file_map = {
+      "file_a": RangeSet.parse("1-3"),
+      "file_c": RangeSet.parse("5-8"),
+      "__COPY": RangeSet.parse("0"),
+      "__ZERO": RangeSet.parse("9-10"),
+      "__NONZERO-0": RangeSet.parse("4")
+    }
+    self.assertEqual(expected_file_map, image.GetFileMap())
