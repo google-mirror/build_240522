8b1a075757283207680ff4ced6ba52904f99ad4f
Jiyong Park <jiyong@google.com>
2019-01-09 14:16:15 +0900

    Add TARGET_BIONIC_UPDATABLE config
    The config allows the build to be conditional on whether the device
    supports updating of bionic libraries via APEX or not.
    
    Currently, it is not set (i.e. false) for all devices.
    
    Bug: 122559956
    Test: m
    Change-Id: I8bc8007b2a1bff5e36b3d8f0d9ae60beb62f3980

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -145,6 +145,7 @@ $(call add_json_bool, FlattenApex,                       $(filter true,$(TARGET_
 $(call add_json_str,  DexpreoptGlobalConfig,             $(DEX_PREOPT_CONFIG))
 
 $(call add_json_list, ManifestPackageNameOverrides,      $(PRODUCT_MANIFEST_PACKAGE_NAME_OVERRIDES))
+$(call add_json_bool, BionicUpdatable,                   $(filter true,$(TARGET_BIONIC_UPDATABLE)))
 
 $(call add_json_map, VendorVars)
 $(foreach namespace,$(SOONG_CONFIG_NAMESPACES),\
