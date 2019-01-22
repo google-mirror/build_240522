ffad4204530156cfe69bd414831317c818603fb2
Nicolas Geoffray <ngeoffray@google.com>
2019-01-22 11:36:36 +0000

    Use the jars, not the classes.dex for passing to hiddenapi.
    The instructions to look up for classes.dex don't work when looking
    at different jar versions (aligned, unaligned, etc).
    
    Test: m
    Change-Id: I5f178ca0d00497edbab892722a5ee84f0c47d0a0

--- core/dex_preopt.mk
+++ core/dex_preopt.mk
@@ -77,7 +77,7 @@ $(INTERNAL_PLATFORM_HIDDENAPI_STUB_FLAGS): $(HIDDENAPI) $(HIDDENAPI_STUBS) \
                                            $(HIDDENAPI_STUBS_SYSTEM) $(HIDDENAPI_STUBS_TEST) \
                                            $(HIDDENAPI_STUBS_CORE_PLATFORM)
 	for INPUT_DEX in $(PRIVATE_DEX_INPUTS); do \
-		find `dirname $${INPUT_DEX}` -maxdepth 1 -name "classes*.dex"; \
+		echo $${INPUT_DEX}; \
 	done | sort | sed 's/^/--boot-dex=/' | xargs $(HIDDENAPI) list \
 	    --public-stub-classpath=$(call normalize-path-list, $(PRIVATE_HIDDENAPI_STUBS)) \
 	    --public-stub-classpath=$(call normalize-path-list, $(PRIVATE_HIDDENAPI_STUBS_SYSTEM)) \
