10c233157e878ac6355bad310dfa66c9ec78ab42
Colin Cross <ccross@android.com>
2019-02-06 16:36:32 -0800

    Make manifest and APK agree on uncompressed native libs
    Only put uncompressed native libs in an APK if the min_sdk_version
    supports it (>= 23, Marshmallow), and set
    android:extractNativeLibs="false" in the AndroidManifest.xml so
    that the platform won't extract them anyways.
    
    Bug: 117618214
    Test: m checkbuild
    Change-Id: Iae26e4676c29c68fa3f76187512c82786bfa0522

--- core/android_manifest.mk
+++ core/android_manifest.mk
@@ -1,5 +1,6 @@
 # Handle AndroidManifest.xmls
-# Input: LOCAL_MANIFEST_FILE, LOCAL_FULL_MANIFEST_FILE, LOCAL_FULL_LIBS_MANIFEST_FILES
+# Input: LOCAL_MANIFEST_FILE, LOCAL_FULL_MANIFEST_FILE, LOCAL_FULL_LIBS_MANIFEST_FILES,
+#        LOCAL_USE_EMBEDDED_NATIVE_LIBS
 # Output: full_android_manifest
 
 ifeq ($(strip $(LOCAL_MANIFEST_FILE)),)
@@ -63,6 +64,18 @@ endif
 
 ifeq (true,$(LOCAL_USE_EMBEDDED_DEX))
     my_manifest_fixer_flags += --use-embedded-dex
+endif
+
+ifeq ($(LOCAL_MODULE_CLASS),APPS)
+  ifeq (true,$(call math_gt_or_eq,$(patsubst $(PLATFORM_VERSION_CODENAME),100,$(module-min-sdk-version)),23))
+    ifeq (true,$(LOCAL_USE_EMBEDDED_NATIVE_LIBS))
+      my_manifest_fixer_flags += --extract-native-libs=false
+    else
+      my_manifest_fixer_flags += --extract-native-libs=true
+    endif
+  else ifeq (true,$(LOCAL_USE_EMBEDDED_NATIVE_LIBS))
+    $(call pretty-error,module attempted to store uncompressed native libraries, but minSdkVersion does not support it)
+  endif
 endif
 
 $(fixed_android_manifest): PRIVATE_MANIFEST_FIXER_FLAGS := $(my_manifest_fixer_flags)
