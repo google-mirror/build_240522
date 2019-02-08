a238bcfb84bacd9af35c1fe700892e50f5ce34b6
Roman Kiryanov <rkir@google.com>
2019-02-07 17:22:49 -0800

    Add a lunch entry for minimal kernel tests
    Bug: 79601115
    Test: make -j
    Change-Id: Ia1eb770fcc265ac786f3d4568e2f22208086c18c
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- target/product/AndroidProducts.mk
+++ target/product/AndroidProducts.mk
@@ -71,6 +71,7 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/sdk_phone_x86.mk \
     $(LOCAL_DIR)/sdk_x86_64.mk \
     $(LOCAL_DIR)/sdk_x86.mk \
+    device/generic/goldfish/kernel-tests/goldfish_kernel_tests_x86_64.mk \
 
 endif
 
