0fadf128c135fb2a13e8244c1364da3f66184ef1
Victor Chang <vichang@google.com>
2019-03-06 17:46:05 +0000

    Remove libicu*.so from /system
    Bug: 120659668
    Test: CtsJniTestCases
    Change-Id: I1c2ab41aa0b19f71025e2dd09be9b9bd306e8616

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -615,6 +615,10 @@ $(call add-clean-step, rm -rf $(HOST_OUT)/framework/vts-tradefed.jar)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/security/avb/)
 
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/super.img)
+
+# Clean up libicuuc.so and libicui18n.so
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib*/libicu*)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
