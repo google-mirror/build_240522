e908641e3e03f2c913b8b71715ad850245208068
bohu <bohu@google.com>
2019-03-07 10:03:41 -0800

    emulator: support dynamic partitions
    BUG: 125540538
    
    Change-Id: I9d486f57ac4ec11b1bbe9ab97890d12441c3054e

--- target/board/BoardConfigEmuCommon.mk
+++ target/board/BoardConfigEmuCommon.mk
@@ -12,6 +12,9 @@ USE_CAMERA_STUB := true
 
 NUM_FRAMEBUFFER_SURFACE_BUFFERS := 3
 
+# emulator does not use AB, make sure it is empty
+AB_OTA_PARTITIONS :=
+
 # Build OpenGLES emulation guest and host libraries
 BUILD_EMULATOR_OPENGL := true
 BUILD_QEMU_IMAGES := true
@@ -20,15 +23,18 @@ BUILD_QEMU_IMAGES := true
 # the GLES renderer disables itself if host GL acceleration isn't available.
 USE_OPENGL_RENDERER := true
 
-# ~140 MB vendor image. Please adjust system image / vendor image sizes
-# when finalizing them. The partition size needs to be a multiple of image
-# block size: 4096.
-BOARD_VENDORIMAGE_PARTITION_SIZE := 140963840
+
+BOARD_EXT4_SHARE_DUP_BLOCKS := true
+
+BOARD_SUPER_PARTITION_SIZE := 8145338368
+BOARD_SUPER_PARTITION_GROUPS := emulator_dynamic_partitions
+BOARD_EMULATOR_DYNAMIC_PARTITIONS_PARTITION_LIST := \
+    system \
+    vendor
+
+BOARD_EMULATOR_DYNAMIC_PARTITIONS_SIZE := 4068474880
 BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_FLASH_BLOCK_SIZE := 512
 DEVICE_MATRIX_FILE   := device/generic/goldfish/compatibility_matrix.xml
 
 BOARD_SEPOLICY_DIRS += device/generic/goldfish/sepolicy/common
-
-# TODO(b/125540538): Remove when emulator uses dynamic partitions
-BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
