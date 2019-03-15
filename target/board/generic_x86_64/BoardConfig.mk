2bf4236ceea46132dbcddab7523d4cc3b12d0c6a
bohu <bohu@google.com>
2019-03-15 09:20:12 -0700

    emulator: support recovery image on x86_64
    Recovery image is useful for lower level
    testing such as kernel.
    
    BUG: 127669078
    Change-Id: Idcae0e266b9df58e9a91f6512fe3cb7f85d451c7

--- target/board/generic_x86_64/BoardConfig.mk
+++ target/board/generic_x86_64/BoardConfig.mk
@@ -26,6 +26,12 @@ TARGET_PRELINK_MODULE := false
 
 include build/make/target/board/BoardConfigEmuCommon.mk
 include build/make/target/board/BoardConfigGsiCommon.mk
+TARGET_NO_KERNEL := false
+TARGET_NO_RECOVERY := false
+
+BOARD_BOOTIMAGE_PARTITION_SIZE := 0x04000000
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x04000000
+INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
 
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 576716800
 
