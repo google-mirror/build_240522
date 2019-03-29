18ec817edbf129177d58318f62375b6e02a4f4e5
Tao Bao <tbao@google.com>
2019-03-29 13:32:03 -0700

    Move the location of the built super.img.
    `m superimage` used to write the output at $(PRODUCT_OUT)/super.img.
    However, the system/vendor/... images within the super.img don't match
    the ones at $(PRODUCT_OUT). For the same reason, flashing this image
    along with $(PRODUCT_OUT)/vbmeta.img would fail the verity check.
    
    Because the super image generated via `m superimage` or `m dist` is
    designed for dist / factory flashing purpose, whose contents should
    match the ones in <product>-img.zip.
    
    This CL moves the image to intermediate dir instead to avoid the
    confusion. `m superimage-nodeps` will keep writing the output to
    $(PRODUCT_OUT)/super.img, because it contains the matching images as in
    $(PRODUCT_OUT).
    
    Test: `m superimage` on DAP launch device. Check the location of the
          generated image.
    Test: `m superimage-nodeps` on DAP launch device.
    Change-Id: Id63f6326a78924d0ba9a8bb12f2a77ac5e848617

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -613,6 +613,10 @@ $(call add-clean-step, rm -rf $(HOST_OUT)/framework/vts-tradefed.jar)
 
 # Clean up old location of system_other.avbpubkey
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/security/avb/)
+
+# Clean up old location of super.img
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/super.img)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
