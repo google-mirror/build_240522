6bd4a819f49736356ca88c71625d9873f83e752d
Bowgo Tsai <bowgotsai@google.com>
2019-04-20 15:01:19 +0800

    Revert "Adding adb_debug.prop"
    This reverts commit a280a66b5ab2a3e738230d994a91778afa95befb.
    
    /init now switched to read adb_debug.prop from debug ramdisk instead
    of GSI or other system.img. No need to keep the file in GSI.
    
    See the following for more details:
      https://android-review.googlesource.com/c/platform/system/core/+/946517
    
    Bug: 126493225
    Test: tree hugger
    Change-Id: I981db8e13216fbe0f066f4d3684ee149b1177d22

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -620,6 +620,9 @@ $(call add-clean-step, find $(PRODUCT_OUT) -type f -name "generated_*_image_info
 
 # Clean up libicuuc.so and libicui18n.so
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib*/libicu*)
+
+# Clean up adb_debug.propr
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/etc/adb_debug.prop)
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
