7bdddcaf004ca6d1b057b9d11d498e62b2bbcb56
David Brazdil <dbrazdil@google.com>
2019-05-17 17:10:52 +0100

    Pass info relevant to hidden API to dexpreopt_gen
    Bug: 79680348
    Change-Id: I6084695872d3c694b9fce187ecf3a360df54b788

--- core/dex_preopt_odex_install.mk
+++ core/dex_preopt_odex_install.mk
@@ -227,6 +227,9 @@ ifdef LOCAL_DEX_PREOPT
 
   $(call add_json_bool, NoStripping,                    $(filter nostripping,$(LOCAL_DEX_PREOPT)))
 
+  $(call add_json_bool, UsesNonSdkApis,                 $(filter true,$(LOCAL_PRIVATE_PLATFORM_APIS)))
+  $(call add_json_bool, SignedWithPlatformCertificate,  $(filter platform,$(LOCAL_CERTIFICATE)))
+
   $(call json_end)
 
   my_dexpreopt_config := $(intermediates)/dexpreopt.config
