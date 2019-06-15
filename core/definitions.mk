ecff0f40486cc69d55862c09f41c750354972529
David Srbecky <dsrbecky@google.com>
2019-06-15 14:10:37 +0100

    Fix symlink dependencies.
    The order-only dependency is not sufficient. If the target of the
    symlink is rebuild, anything depending on it would not be rebuild.
    
    This fixes incremental builds.  Clean build will build everything.
    
    Technically, the comment is correct, however the timestamp change
    would be noticed on the next build, requiring building in a loop.
    
    Test: Fixes ART tests for incremental builds.
    Change-Id: Ic3b93ee98fda2dc707435d35053b437f81efb7b0

--- core/definitions.mk
+++ core/definitions.mk
@@ -2597,10 +2597,8 @@ define symlink-file
 $(eval $(_symlink-file))
 endef
 
-# Order-only dependency because make/ninja will follow the link when checking
-# the timestamp, so the file must exist
 define _symlink-file
-$(3): | $(1)
+$(3): $(1)
 	@echo "Symlink: $$@ -> $(2)"
 	@mkdir -p $(dir $$@)
 	@rm -rf $$@
