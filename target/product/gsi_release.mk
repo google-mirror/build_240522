bb744fcdcd1df4741f32d426b657e391b2432360
SzuWei Lin <szuweilin@google.com>
2019-07-31 18:21:07 +0800

    Handle zygote_32_64 in gsi_release.mk
    64 bits GSI expects supporting both zygote_64_32 and zygote_32_64.
    
    The patch adds including init.zygote32_64.rc in gsi_release.mk
    direclty, and also revised all GSI releasing build targets.
    
    Bug: 138625977
    Test: build aosp_arm64-userdebug and has init.zygote32_64.rc
    Test: build aosp_arm-userdebug and no init.zygote32_64.rc
    Change-Id: Ia61db1315321798bae8902b35ddaa5e5f3e7dd3e

--- target/product/gsi_release.mk
+++ target/product/gsi_release.mk
@@ -56,3 +56,16 @@ PRODUCT_COPY_FILES += \
 
 # Support addtional P VNDK packages
 PRODUCT_EXTRA_VNDK_VERSIONS := 28
+
+# Copy different zygote settings for vendor.img to select by setting property
+# ro.zygote=zygote64_32 or ro.zygote=zygote32_64:
+#   1. 64-bit primary, 32-bit secondary OR
+#   2. 32-bit primary, 64-bit secondary
+# init.zygote64_32.rc is in the core_64_bit.mk below
+ifeq (true|true,$(TARGET_SUPPORTS_32_BIT_APPS)|$(TARGET_SUPPORTS_64_BIT_APPS))
+PRODUCT_COPY_FILES += \
+    system/core/rootdir/init.zygote32_64.rc:root/init.zygote32_64.rc
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_WHITELIST += \
+    root/init.zygote32_64.rc
+endif
