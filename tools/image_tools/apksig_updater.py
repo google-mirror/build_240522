9149fb46f0abd7c70d8e46164e8eec90fdf796a7
XiaoguangDong <dongxiaoguang0@gmail.com>
2019-08-20 19:13:10 +0800

    Resign all apks in system image
    Change-Id: I980a4e0bcc7e226ca92ddecd69f23d167f6c70bd

---
+++ tools/image_tools/apksig_updater.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+import os,sys
+import struct
+from apkverify import ApkSignature
+
+key_id_map = {
+    1: "platform",
+    2: "media",
+    3: "shared",
+    4: "testkey",
+    5: "verity",
+    6: "releasekey",
+}
+
+key_name_map = {
+    "platform": 1,
+    "media": 2,
+    "shared": 3,
+    "testkey": 4,
+    "verity": 5,
+    "releasekey": 6,
+}
+
+def write_key_id(apk_path, key_name):
+    key_id = key_name_map.get(key_name, -1)
+    _,_,_,_,_,offset = ApkSignature(apk_path).v2_zipfindsig()
+    # ApkSignature(sys.argv[1]).verify()
+    f = open(apk_path,'rb+')
+    f.seek(offset)
+    # binary_format = bytearray(byte_arr)
+    f.write(bytearray([key_id]))
+    f.close()
+    return
+
+def get_key_name(apk_path):
+    _,_,_,_,_,offset = ApkSignature(apk_path).v2_zipfindsig()
+    if offset == -1:
+        print("%s not has verify padding block", apk_path)
+        return
+    f = open(apk_path,'rb+')
+    print("get_key_name ^^^^^^^^^^^^^^^ %s", offset)
+    f.seek(offset)
+    key_id = f.read(1)
+    print("%s" % struct.unpack("<B", key_id)[0])
+    f.close()
+    return key_id_map.get(struct.unpack("<B", key_id)[0], "UNKNOWN")
+
+if __name__ == '__main__':
+    """ Confirm input (very) briefly """
+    if len(sys.argv) != 3 and len(sys.argv) != 2:
+        print("Usage: %s apk_path key_name" % sys.argv[0])
+        exit(2)
+    if len(sys.argv) == 3:
+        write_key_id(sys.argv[1], sys.argv[2])
+    key_name = get_key_name(sys.argv[1])
+    print("%s ^^^^^^^^^^^" % key_name)
+    exit(0)
---
+++ tools/image_tools/image_comparator.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python
+import os,sys,hashlib,re
+import subprocess
+from apkverify import is_apk_file
+
+global_mismatch_result = []
+oneimage_unique_items = []
+twoimage_unique_items = []
+'''default mount point for image '''
+onedir = '/mnt/oneimage'
+twodir = '/mnt/twoimage'
+
+def sumfile(fobj):
+    m = hashlib.md5()
+    while True:
+	d = fobj.read(8096)
+	if not d:
+	    break
+	m.update(d)
+    return m.hexdigest()
+
+def md5sum(fname):
+    if fname == '-':
+	ret = sumfile(sys.stdin)
+    else:
+	try:
+	    f = file(fname, 'rb')
+	except:
+  	    return 'Failed to open file'
+  	ret = sumfile(f)
+	f.close()
+    return ret
+
+def walkdirs(dir1, dir2):
+    dirsa = []
+    dirsb = []
+
+    for directory in os.walk(dir1):
+	relativedirname = pathsep + re.sub(dir1, "", directory[0])
+	if relativedirname != pathsep:
+		dirsa.append(relativedirname)
+    for directory in os.walk(dir2):
+	relativedirname = pathsep + re.sub(dir2, "", directory[0])
+	if relativedirname != pathsep:
+		dirsb.append(relativedirname)
+	# Return matches for further inspection.
+    return set(dirsa).intersection(set(dirsb))
+
+def complists(dir1, dir2):
+    print("#######################################")
+    print("Comparing \n%s \n%s" % (dir1, dir2))
+    print("#######################################")
+    dira = []
+    dirb = []
+    for file in os.listdir(dir1):
+	dira.append(file)
+    for file in os.listdir(dir2):
+	dirb.append(file)
+    indir1 = set(dira).difference(set(dirb))
+    indir2 = set(dirb).difference(set(dira))
+    return (indir1, indir2, set(dira).intersection(set(dirb)))
+
+def comfiles(files, fdir, sdir):
+    firstdir = {}
+    secdir = {}
+    # Create an absolute path to them from the relative filename and get the md5.
+    for f in files:
+        of = fdir + pathsep + f
+        sf = sdir + pathsep + f
+        if os.path.isdir(of) and os.path.isdir(sf):
+            continue
+        if os.path.islink(of) and os.path.islink(sf):
+            continue
+        lite_result_check, firstdir[f], lite_errors = is_apk_file(of, validate=True)
+        if lite_result_check:
+            full_result_check, secdir[f], full_errors = is_apk_file(sf, validate=True)
+        else:
+            firstdir[f] = md5sum(of)
+            secdir[f] = md5sum(sf)
+    for x in firstdir:
+        print("firstdir %s :::: %s:" % (x, firstdir[x]))
+        print("secdir %s :::: %s:" % (x, secdir[x]))
+        if firstdir[x] != secdir[x]:
+            global_mismatch_result.append(fdir[len(onedir)+1:] + pathsep + x)
+            print("File %s in both targets but does not match!" % x)
+    print("\n")
+
+def outp(datum, fdir, sdir):
+    if len(datum[0]) > 0:
+	print("Items in \"%s\" and not in \"%s\":" % (fdir, sdir))
+	for z in datum[0]:
+            oneimage_unique_items.append(fdir[len(onedir)+1:] + pathsep + z)
+	    print("   ", z)
+	print("\n------------------------------")
+    else:
+	print("No unique items in \"%s\"" % (fdir))
+    if len(datum[1]) > 0:
+	print("Items in \"%s\" and not in \"%s\":" %(sdir, fdir))
+	for z in datum[1]:
+            twoimage_unique_items.append(sdir[len(twodir)+1:] + pathsep + z)
+	    print("   ", z)
+	print("\n------------------------------")
+    else:
+	print("No unique items in \"%s\"" % (sdir))
+    if len(datum[2]) > 0:
+	comfiles(datum[2], fdir, sdir)
+
+def execute(cmd):
+    print(cmd)
+    p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = map(lambda b: b.decode('utf-8'), p.communicate())
+    return p.returncode == 0, out, err
+
+def make_mount_cmd(onedir, twodir, oneimage, twoimage):
+    return ['mkdir -p ' + onedir + ' ' + twodir,
+            'mount -o rw ' + oneimage + ' ' + onedir,
+            'mount -o rw ' + twoimage + ' ' + twodir]
+
+def make_umount_cmd(onedir, twodir):
+    return ['umount ' + onedir,
+            'umount ' + twodir]
+
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print("Usage: %s img1 img2" % sys.argv[0])
+        sys.exit(1)
+    if os.name == "posix":
+        pathsep = "/"
+    elif os.name == "nt":
+        pathsep = "\\"
+    else:
+        sys.exit(1)
+
+    (oneimage, twoimage) = sys.argv[1], sys.argv[2]
+    if oneimage.endswith('.img') and twoimage.endswith('.img'):
+        execute(make_umount_cmd(onedir, twodir))
+        for cmd in make_mount_cmd(onedir, twodir, oneimage, twoimage):
+            print(cmd)
+            success, out, error_msg = execute(cmd)
+            if not success:
+                print(error_msg)
+                sys.exit(1)
+    elif os.path.isfile(oneimage) and os.path.isfile(twoimage): 
+        one_result_check, one_result, one_errors = is_apk_file(oneimage, validate=True)
+        if one_result_check:
+            two_result_check, two_result, two_errors = is_apk_file(twoimage, validate=True)
+        else:
+            one_result = md5sum(oneimage)
+            two_result = md5sum(twoimage)
+        print("onefile %s:::: %s:" % (oneimage, one_result))
+        print("twofile %s:::: %s:" % (twoimage, two_result))
+        if  one_result != two_result:
+            print("File %s in both targets but does not match!\n" % onedir)
+        else:
+            print("Exact match!")
+        sys.exit(0)
+    else:
+        print("please check the input param!")
+        sys.exit(1)
+
+    subdirs = walkdirs(onedir, twodir)
+    setinfo = complists(onedir, twodir)
+    outp(setinfo, onedir, twodir)
+    for subdir in subdirs:
+        setinfo = complists(onedir + subdir, twodir + subdir)
+        outp(setinfo, onedir + subdir, twodir + subdir)
+
+    for cmd in make_umount_cmd(onedir, twodir):
+        success, out, error_msg = execute(cmd)
+        if not success:
+            print(error_msg)
+            sys.exit(1)
+
+    print("\n\n\n\n\n")
+    print("**************************************************")
+    print("**************** Comparing Result ****************")
+    print("**************************************************")
+    print("Unique files in %s :" % oneimage)
+    print("###########################")
+    for item in oneimage_unique_items:
+        print("%s " % item)
+    print("###########################\n\n\n")
+
+    print("Unique files in %s :" % twoimage)
+    print("###########################")
+    for item in twoimage_unique_items:
+        print("%s " % item)
+    print("###########################\n\n\n")
+
+    print("Mismatch files:")
+    print("###########################")
+    for item in global_mismatch_result:
+        print("%s " % item)
+    print("###########################")
