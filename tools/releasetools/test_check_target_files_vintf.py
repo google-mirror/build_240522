69a842622fdca65efb3d3ca06b49c77dd4975439
Yifan Hong <elsk@google.com>
2019-08-22 15:52:26 -0700

    releasetools: add tests for check_target_files_vintf
    Test: run it
    Bug: 139300422
    Change-Id: I265d1e6313177dc661bb5cdf1415b11684dec0a0

---
+++ tools/releasetools/test_check_target_files_vintf.py
@@ -0,0 +1,56 @@
+#
+# Copyright (C) 2019 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import os.path
+
+import common
+import test_utils
+from check_target_files_vintf import CheckVintf
+
+class CheckTargetFilesVintfTest(test_utils.ReleaseToolsTestCase):
+
+  def setUp(self):
+    self.testdata_dir = test_utils.get_testdata_dir()
+
+  def test_CheckVintf_skip_non_treble(self):
+    self.assertTrue(CheckVintf(os.path.join(self.testdata_dir,
+                                            "vintf/skip_non_treble")))
+
+  def test_CheckVintf_sanity(self):
+    self.assertTrue(CheckVintf(os.path.join(self.testdata_dir,
+                                            "vintf/sanity")))
+
+  def test_CheckVintf_bad_matrix(self):
+    self.assertFalse(CheckVintf(os.path.join(self.testdata_dir,
+                                             "vintf/bad_matrix")))
+
+  def test_CheckVintf_kernel_compat(self):
+    self.assertTrue(CheckVintf(os.path.join(self.testdata_dir,
+                                            "vintf/kernel_compat")))
+
+  def test_CheckVintf_kernel_incompat(self):
+    self.assertFalse(CheckVintf(os.path.join(self.testdata_dir,
+                                             "vintf/kernel_incompat")))
+
+  def test_CheckVintf_sku_compat(self):
+    self.assertFalse(CheckVintf(os.path.join(self.testdata_dir,
+                                             "vintf/sku_compat")))
+
+  def test_CheckVintf_sku_incompat(self):
+    self.assertFalse(CheckVintf(os.path.join(self.testdata_dir,
+                                             "vintf/sku_incompat")))
+
+
