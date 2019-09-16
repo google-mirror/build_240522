b48e51f16038b53404730ab5919435329e0d0756
Tao Bao <tbao@google.com>
2019-09-16 12:10:43 -0700

    Also install verity_key to ramdisk for non-system-as-root target.
    The commit in d14b895665f9fb122f93edb16655fd3a49510032 (
    https://android-review.googlesource.com/c/platform/build/+/728287)
    changed partition layout, to always build the root dir into system.img,
    even for devices not using system-as-root.
    
    However, for non-system-as-root devices (i.e. the ones with a separate
    boot ramdisk) that use VB 1.0, we need an extra copy of verity_key in
    ramdisk to establish the chain of trust.
     - bootloader uses the baked-in key to verify boot.img
     - bootloader loads the ramdisk from the verified boot.img
     - First stage init uses /verity_key (in ramdisk) to verify and mount
       system.img at /system; it then pivots to it
     - Second stage init uses /verity_key (in sysgtem.img) to verify and
       mount other partitions
    
    This CL adds rules to additionally install verity_key into ramdisk for
    such devices.
    
    Bug: 139770257
    Test: Set up a target to use non-system-as-root
          (BOARD_BUILD_SYSTEM_ROOT_IMAGE != true). `m dist`.
    Test: Check that both ROOT/verity_key and BOOT/RAMDISK/verity_key exist
          in the built target_files.zip.
    Test: Run validate_target_files to validate the above target_files.zip.
          $ validate_target_files \
              --verity_key_mincrypt /path/to/verity_key \
              target_files.zip
    Test: Run sign_target_files_apks to sign the above target. Re-run
          validate_target_files on the signed target_files.zip.
    Test: python -m unittest test_validate_target_files
    Change-Id: Ibe7e771c8c376429add85851ac86055564765d3c

--- target/product/security/Android.mk
+++ target/product/security/Android.mk
@@ -1,13 +1,30 @@
 LOCAL_PATH:= $(call my-dir)
 
 #######################################
-# verity_key
+# verity_key (installed to /, i.e. part of system.img)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := verity_key
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
+
+# For devices using a separate ramdisk, we need a copy there to establish the chain of trust.
+ifneq ($(BOARD_BUILD_SYSTEM_ROOT_IMAGE),true)
+LOCAL_REQUIRED_MODULES := verity_key_ramdisk
+endif
+
+include $(BUILD_PREBUILT)
+
+#######################################
+# verity_key (installed to ramdisk)
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := verity_key_ramdisk
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES := verity_key
+LOCAL_MODULE_STEM := verity_key
+LOCAL_MODULE_PATH := $(TARGET_RAMDISK_OUT)
 
 include $(BUILD_PREBUILT)
 
