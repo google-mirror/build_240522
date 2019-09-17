ce752278a8328c05101c3854130542e30663a639
Logan Chien <loganchien@google.com>
2019-09-17 09:45:09 -0700

    Always check ELF files for `check-elf-files`
    With this commit, the build system always checks ELF files when
    `check-elf-files` is one of the make goals regardless
    `PRODUCT_CHECK_ELF_FILES` is specified or not.
    
    This commit also adds `BUILD_BROKEN_PREBUILT_ELF_FILES` for targets that
    haven't been cleared.
    
    If this commit breaks your target (usually breaks the targets running
    `make checkbuild`), you may triage the breakage by adding the following
    setting to your `BoardConfig.mk`:
    
        BUILD_BROKEN_PREBUILT_ELF_FILES := true
    
    Bug: 141176116
    Test: lunch aosp_crosshatch-userdebug && make check-elf-files
    Change-Id: Ibe65c977a4ab3d40ba35892ed177eca62c837c35

--- core/board_config.mk
+++ core/board_config.mk
@@ -87,6 +87,7 @@ _board_strip_readonly_list += $(_dynamic_partitions_var_list)
 _build_broken_var_list := \
   BUILD_BROKEN_DUP_RULES \
   BUILD_BROKEN_USES_NETWORK \
+  BUILD_BROKEN_PREBUILT_ELF_FILES \
 
 _build_broken_var_list += \
   $(foreach m,$(AVAILABLE_BUILD_MODULE_TYPES) \
