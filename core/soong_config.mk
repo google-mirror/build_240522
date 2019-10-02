aab46f135096439612f3b4c9df4e1b4511bb6253
Steven Moreland <smoreland@google.com>
2019-10-02 10:24:14 -0700

    Expose PRODUCT_SHIPPING_API_LEVEL to Soong.
    This is in order to support deprecating some framework HAL services.
    Since they are served as part of binaries, including/unincluding a
    service in PRODUCT_PACKAGES which has vintf_fragments is not feasible.
    Instead, the service is decided to either exist or not at build time.
    
    Bug: 37226359
    Bug: 141930622
    Test: use this shipping api level to conditionally include a service
    Change-Id: Ia3cc403f5ee5d7c1240c9091443037ae6d285b4b

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -128,6 +128,7 @@ $(call add_json_list, DeviceSystemSdkVersions,           $(BOARD_SYSTEMSDK_VERSI
 $(call add_json_list, Platform_systemsdk_versions,       $(PLATFORM_SYSTEMSDK_VERSIONS))
 $(call add_json_bool, Malloc_not_svelte,                 $(call invert_bool,$(filter true,$(MALLOC_SVELTE))))
 $(call add_json_str,  Override_rs_driver,                $(OVERRIDE_RS_DRIVER))
+$(call add_json_str,  Product_shipping_api_level,        $(PRODUCT_SHIPPING_API_LEVEL))
 
 $(call add_json_bool, UncompressPrivAppDex,              $(call invert_bool,$(filter true,$(DONT_UNCOMPRESS_PRIV_APPS_DEXS))))
 $(call add_json_list, ModulesLoadedByPrivilegedModules,  $(PRODUCT_LOADED_BY_PRIVILEGED_MODULES))
