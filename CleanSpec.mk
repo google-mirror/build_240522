2502465257a1d93c09d59d26ea9d5b0b9d1bf04c
Dario Freni <dariofreni@google.com>
2019-12-02 20:46:03 +0000

    Move CtsShim and CtsPrivShim APKs out of system.
    These packages are now available in the shim apex package, which is made
    available in both flattened and unflattened apex configurations.
    
    Bug: 138429615
    Test: atest CtsPrivilegedUpdateTests CtsPermissionTestCases
    CtsStagedInstallHostTestCases; (on both flattened and unflattened
    configurations)
    Change-Id: I03939f6c3e711d5887da201e32173f86a6b0289e

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -699,6 +699,13 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/lib*/vndk-*)
 # again, as the original change removing them was reverted.
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/framework/*.jar)
 
+# Remove CtsShim apks from system partition, since the have been moved inside
+# the cts shim apex. Also remove the cts shim apex prebuilt since it has been
+# removed in flattened apexs configurations.
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/CtsShimPrivPrebuilt)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/CtsShimPrebuilt)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/apex/com.android.apex.cts.shim.apex)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
