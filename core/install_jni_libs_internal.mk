1cc512cc0892c2bd8f706f3bfc9dc657fe9fbec1
Dan Willemsen <dwillemsen@google.com>
2020-01-06 09:55:11 -0800

    Use symlinks in the build graph for jni libs
    Now that ninja uses lstat and can support installing arbitrary symlinks,
    switch jni lib symlinks from LOCAL_POST_INSTALL_CMDS to real rules.
    
    Bug: 128577186
    Test: List of files under PRODUCT_OUT is the same before/after this change
    Test: out/target/product/generic/.installable_files now includes the symlinks
    Test: m installclean; m NfcNci -> symlinks installed with correct dest
    Test: m NfcNci; m NfcNci -> ninja: no work to do
    Change-Id: I078dca53ab3d93f74c36fa66d5577e6e3e0640d6

--- core/install_jni_libs_internal.mk
+++ core/install_jni_libs_internal.mk
@@ -57,21 +57,15 @@ else ifneq ($(my_jni_shared_libraries),) # not my_embed_jni
   # Create symlink in the app specific lib path
   # Skip creating this symlink when running the second part of a target sanitization build.
   ifeq ($(filter address,$(SANITIZE_TARGET)),)
-    ifdef LOCAL_POST_INSTALL_CMD
-      # Add a shell command separator
-      LOCAL_POST_INSTALL_CMD += ;
-    endif
-
     my_symlink_target_dir := $(patsubst $(PRODUCT_OUT)%,%,\
-        $(my_shared_library_path))
-    LOCAL_POST_INSTALL_CMD += \
-        mkdir -p $(my_app_lib_path) \
-        $(foreach lib, $(my_jni_filenames), ;ln -sf $(my_symlink_target_dir)/$(lib) $(my_app_lib_path)/$(lib))
-    $(LOCAL_INSTALLED_MODULE): PRIVATE_POST_INSTALL_CMD := $(LOCAL_POST_INSTALL_CMD)
-  else
-    ifdef LOCAL_POST_INSTALL_CMD
-      $(LOCAL_INSTALLED_MODULE): PRIVATE_POST_INSTALL_CMD := $(LOCAL_POST_INSTALL_CMD)
-    endif
+      $(my_shared_library_path))
+    $(foreach lib,$(my_jni_filenames),\
+      $(call symlink-file, \
+        $(my_shared_library_path)/$(lib), \
+        $(my_symlink_target_dir)/$(lib), \
+        $(my_app_lib_path)/$(lib)) \
+      $(eval $$(LOCAL_INSTALLED_MODULE) : $$(my_app_lib_path)/$$(lib)) \
+      $(eval ALL_MODULES.$$(LOCAL_MODULE).INSTALLED += $$(my_app_lib_path)/$$(lib)))
   endif
 
   # Clear jni_shared_libraries to not embed it into the apk.
