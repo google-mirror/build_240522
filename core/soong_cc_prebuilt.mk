7efb791775fcd36e9e8106274640724576e0b3a7
Vic Yang <victoryang@google.com>
2020-01-08 14:39:10 -0800

    Always check VNDK variant identicalness
    Previously, we only check VNDK core and vendor variants are identical
    when a VNDK library is not declared to have different variants AND the
    target has TARGET_VNDK_USE_CORE_VARIANT set.  Therefore, it is fairly
    easily to break a TARGET_VNDK_USE_CORE_VARIANT target as it needs to be
    tested explicitly.
    
    This change uses the new LOCAL_CHECK_SAME_VNDK_VARIANTS and expands the
    check to run regardless of TARGET_VNDK_USE_CORE_VARIANT.  Also adds
    support for VNDK-in-product.
    
    Bug: 145157349
    Test: Build success for targets with and without
          TARGET_VNDK_USE_CORE_VARIANT.
    Test: With the corresponding change in build/make, remove libbinder
          from build/soong/cc/config/vndk.go and check build fails even
          when TARGET_VNDK_USE_CORE_VARIANT is not set.
    
    Change-Id: Iec708b971072e6580f77a03e243b30b89b3b054d

--- core/soong_cc_prebuilt.mk
+++ core/soong_cc_prebuilt.mk
@@ -104,12 +104,13 @@ ifdef LOCAL_INSTALLED_MODULE
   endif
 endif
 
-ifeq ($(LOCAL_VNDK_DEPEND_ON_CORE_VARIANT),true)
+ifeq ($(LOCAL_CHECK_SAME_VNDK_VARIANTS),true)
   # Add $(LOCAL_BUILT_MODULE) as a dependency to no_vendor_variant_vndk_check so
   # that the vendor variant will be built and checked against the core variant.
   no_vendor_variant_vndk_check: $(LOCAL_BUILT_MODULE)
 
-  my_core_register_name := $(subst .vendor,,$(my_register_name))
+  my_core_register_name := \
+		$(subst .vendor,,$(subst .product,,$(my_register_name)))
   my_core_variant_files := $(call module-target-built-files,$(my_core_register_name))
   my_core_shared_lib := $(sort $(filter %.so,$(my_core_variant_files)))
   $(LOCAL_BUILT_MODULE): PRIVATE_CORE_VARIANT := $(my_core_shared_lib)
@@ -119,7 +120,7 @@ ifeq ($(LOCAL_VNDK_DEPEND_ON_CORE_VARIANT),true)
   $(LOCAL_BUILT_MODULE): $(my_core_shared_lib)
 endif
 
-ifeq ($(LOCAL_VNDK_DEPEND_ON_CORE_VARIANT),true)
+ifeq ($(LOCAL_CHECK_SAME_VNDK_VARIANTS),true)
 $(LOCAL_BUILT_MODULE): PRIVATE_TOOLS_PREFIX := $($(LOCAL_2ND_ARCH_VAR_PREFIX)$(my_prefix)TOOLS_PREFIX)
 $(LOCAL_BUILT_MODULE): $(LOCAL_PREBUILT_MODULE_FILE) $(LIBRARY_IDENTITY_CHECK_SCRIPT)
 	$(call verify-vndk-libs-identical,\
