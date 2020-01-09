c8055f26ee05c9923bd2935c32f7c9822df89ac2
Daniel Norman <danielnorman@google.com>
2020-01-08 17:01:11 -0800

    Uses a per-partition fingerprint for building images and avb_salt.
    This causes the output image files of a merged build to be identical
    to the image files of the input partial builds, for each image whose
    contents come strictly from only one partial build (such as system.img
    or vendor.img, but not vbmeta.img or super/super_empty.img).
    
    Test: python -m unittest test_common
    Test: `m dist`; `unzip out/dist/target_files.zip IMAGES/\*`;
          `zip -d out/dist/target_files.zip IMAGES/\*`
          `add_img_to_target_files -a out/dist/target_files.zip`.
          Verify that the rebuilt images are identical to the deleted ones.
    Test: Build a merged target (using merge_target_files.py). Verify that
          the partial target-files.zip IMAGES are identical to the merged
          target-files.zip IMAGES (for images that are purely from one
          build).
    Bug: 144101993
    Change-Id: I5fdf5783c1aff9c14cf5408090389b1f65b69ca6

--- tools/releasetools/test_common.py
+++ tools/releasetools/test_common.py
@@ -48,13 +48,36 @@ class BuildInfoTest(test_utils.ReleaseToolsTestCase):
 
   TEST_INFO_DICT = {
       'build.prop' : {
-          'ro.product.device' : 'product-device',
+          'ro.product.brand' : 'product-brand',
           'ro.product.name' : 'product-name',
-          'ro.build.fingerprint' : 'build-fingerprint',
+          'ro.product.device' : 'product-device',
+          'ro.build.version.release' : 'version-release',
+          'ro.build.id' : 'build-id',
+          'ro.build.version.incremental' : 'version-incremental',
+          'ro.build.type' : 'build-type',
+          'ro.build.tags' : 'build-tags',
           'ro.build.foo' : 'build-foo',
       },
+      'system.build.prop' : {
+          'ro.product.system.brand' : 'product-brand',
+          'ro.product.system.name' : 'product-name',
+          'ro.product.system.device' : 'product-device',
+          'ro.system.build.version.release' : 'version-release',
+          'ro.system.build.id' : 'build-id',
+          'ro.system.build.version.incremental' : 'version-incremental',
+          'ro.system.build.type' : 'build-type',
+          'ro.system.build.tags' : 'build-tags',
+          'ro.system.build.foo' : 'build-foo',
+      },
       'vendor.build.prop' : {
-          'ro.vendor.build.fingerprint' : 'vendor-build-fingerprint',
+          'ro.product.vendor.brand' : 'vendor-product-brand',
+          'ro.product.vendor.name' : 'vendor-product-name',
+          'ro.product.vendor.device' : 'vendor-product-device',
+          'ro.vendor.build.version.release' : 'vendor-version-release',
+          'ro.vendor.build.id' : 'vendor-build-id',
+          'ro.vendor.build.version.incremental' : 'vendor-version-incremental',
+          'ro.vendor.build.type' : 'vendor-build-type',
+          'ro.vendor.build.tags' : 'vendor-build-tags',
       },
       'property1' : 'value1',
       'property2' : 4096,
@@ -92,7 +115,11 @@ class BuildInfoTest(test_utils.ReleaseToolsTestCase):
   def test_init(self):
     target_info = common.BuildInfo(self.TEST_INFO_DICT, None)
     self.assertEqual('product-device', target_info.device)
-    self.assertEqual('build-fingerprint', target_info.fingerprint)
+    self.assertEqual(
+        target_info.GetPartitionFingerprint('system'),
+        'product-brand/product-name/product-device'
+        ':version-release/build-id/version-incremental'
+        ':build-type/build-tags')
     self.assertFalse(target_info.is_ab)
     self.assertIsNone(target_info.oem_props)
 
@@ -186,39 +213,24 @@ class BuildInfoTest(test_utils.ReleaseToolsTestCase):
     self.assertRaises(common.ExternalError, target_info.GetBuildProp,
                       'ro.build.nonexistent')
 
-  def test_GetVendorBuildProp(self):
+  def test_GetPartitionFingerprint(self):
     target_info = common.BuildInfo(self.TEST_INFO_DICT, None)
-    self.assertEqual('vendor-build-fingerprint',
-                     target_info.GetVendorBuildProp(
-                         'ro.vendor.build.fingerprint'))
-    self.assertRaises(common.ExternalError, target_info.GetVendorBuildProp,
-                      'ro.build.nonexistent')
+    self.assertEqual(
+        target_info.GetPartitionFingerprint('vendor'),
+        'vendor-product-brand/vendor-product-name/vendor-product-device'
+        ':vendor-version-release/vendor-build-id/vendor-version-incremental'
+        ':vendor-build-type/vendor-build-tags')
 
-  def test_GetVendorBuildProp_with_oem_props(self):
-    target_info = common.BuildInfo(self.TEST_INFO_DICT_USES_OEM_PROPS,
-                                   self.TEST_OEM_DICTS)
-    self.assertEqual('vendor-build-fingerprint',
-                     target_info.GetVendorBuildProp(
-                         'ro.vendor.build.fingerprint'))
-    self.assertRaises(common.ExternalError, target_info.GetVendorBuildProp,
-                      'ro.build.nonexistent')
-
-  def test_vendor_fingerprint(self):
-    target_info = common.BuildInfo(self.TEST_INFO_DICT, None)
-    self.assertEqual('vendor-build-fingerprint',
-                     target_info.vendor_fingerprint)
-
-  def test_vendor_fingerprint_blacklisted(self):
-    target_info_dict = copy.deepcopy(self.TEST_INFO_DICT_USES_OEM_PROPS)
-    del target_info_dict['vendor.build.prop']['ro.vendor.build.fingerprint']
-    target_info = common.BuildInfo(target_info_dict, self.TEST_OEM_DICTS)
-    self.assertIsNone(target_info.vendor_fingerprint)
-
-  def test_vendor_fingerprint_without_vendor_build_prop(self):
-    target_info_dict = copy.deepcopy(self.TEST_INFO_DICT_USES_OEM_PROPS)
-    del target_info_dict['vendor.build.prop']
-    target_info = common.BuildInfo(target_info_dict, self.TEST_OEM_DICTS)
-    self.assertIsNone(target_info.vendor_fingerprint)
+  def test_GetPartitionFingerprint_missing_partition_props(self):
+    info_dict = copy.deepcopy(self.TEST_INFO_DICT)
+    del info_dict['vendor.build.prop']
+    target_info = common.BuildInfo(info_dict, None)
+    # The system.build.prop fingerprint should be returned.
+    self.assertEqual(
+        target_info.GetPartitionFingerprint('vendor'),
+        'product-brand/product-name/product-device'
+        ':version-release/build-id/version-incremental'
+        ':build-type/build-tags')
 
   def test_WriteMountOemScript(self):
     target_info = common.BuildInfo(self.TEST_INFO_DICT_USES_OEM_PROPS,
