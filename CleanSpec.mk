188b907b0c798b3b74c51bec6cd6346ae3ba68a5
Dan Shi <dshi@google.com>
2020-03-28 13:25:21 -0700

    Cleanup VTS-Core and VTS10 related artifacts
    Bug: 151896491
    Test: noen
    Change-Id: I419f03b7a940e6a7201c4957f560bb0ab1f50ebe

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -713,6 +713,12 @@ $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/apex/com.android.apex.cts.sh
 $(call add-clean-step, find $(SOONG_OUT_DIR)/.intermediates -type d -name "android_*_recovery*" -print0 | xargs -0 rm -rf)
 $(call add-clean-step, find $(SOONG_OUT_DIR)/.intermediates -type d -name "android_*_vendor*" -print0 | xargs -0 rm -rf)
 
+# Cleanup VTS-Core and VTS10 related artifacts.
+$(call add-clean-step, rm -rf $(HOST_OUT)/vts-core/*)
+$(call add-clean-step, rm -rf $(HOST_OUT)/framework/vts-core-tradefed.jar)
+$(call add-clean-step, rm -rf $(HOST_OUT)/vts10/*)
+$(call add-clean-step, rm -rf $(HOST_OUT)/framework/vts10-tradefed.jar)
+
 # ************************************************
 # NEWER CLEAN STEPS MUST BE AT THE END OF THE LIST
 # ************************************************
