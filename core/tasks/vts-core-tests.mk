974d1a4aedf7340a1ea6d3d9ad1bcb7501897796
Dan Shi <dshi@google.com>
2020-04-24 20:45:16 -0700

    [DO NOT MERGE] just a test
    Change-Id: Ic54652b2fddad7c7ff316045b76cbd78f8ff4d27

--- core/tasks/vts-core-tests.mk
+++ core/tasks/vts-core-tests.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# just a test
+
 test_suite_name := vts
 test_suite_tradefed := vts-tradefed
 test_suite_readme := test/vts/tools/vts-core-tradefed/README
