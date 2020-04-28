5b97249aefbfc8b93d2b7451ce27eed681b4292b
Hongguang Chen <hgchen@google.com>
2020-04-27 18:36:36 -0700

    Add options to sign the custom images.
    The custom images are any images owned by OEMs and SoCs, oem images
    mounted on /oem is an example. The oem images can be used to customize
    devices for different carriers, like wallpaper, ringtones, and
    carrier-specific apks. OEMs can generate multiple oem images,
    like oem.img, oem-carrier1.img and oem-carrier2.img and flash different
    oem images for different carriers. The oem images are only one case,
    OEMs and SoCs can add more custom images and mount them to custom
    partitions.
    
    This change enables custom images to be vbmeta.img chained partitions.
    The following configuration in BoardConfig.mk is an exmaple. It has two
    custom partitions: oem and test. They will be signed by different keys.
    And they will be chained by vbmeta.img. The custom images here are
    prebuilts, which can be built by `make custom_images` separately.
    BOARD_AVB_<CUSTOM_PARTITION>_IMAGE_LIST should include all custom images
    to apply AVB signing. And to every custom partition, one image whose
    name is partition name must be added in its
    BOARD_AVB_<CUSTOM_PARTITION>_IMAGE_LIST.
    
    BOARD_CUSTOMIMAGES_PARTITION_LIST := oem test
    
    BOARD_AVB_OEM_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
    BOARD_AVB_OEM_ALGORITHM := SHA256_RSA4096
    BOARD_AVB_OEM_ADD_HASHTREE_FOOTER_ARGS :=
    BOARD_AVB_OEM_ROLLBACK_INDEX_LOCATION := 1
    BOARD_AVB_OEM_PARTITION_SIZE := 5242880
    BOARD_AVB_OEM_IMAGE_LIST := \
        device/xxxx/yyyy/oem/oem.img \
        device/xxxx/yyyy/oem/oem1.img
    
    BOARD_AVB_TEST_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
    BOARD_AVB_TEST_ALGORITHM := SHA256_RSA2048
    BOARD_AVB_TEST_ADD_HASHTREE_FOOTER_ARGS :=
    BOARD_AVB_TEST_ROLLBACK_INDEX_LOCATION := 2
    BOARD_AVB_TEST_PARTITION_SIZE := 10485760
    BOARD_AVB_TEST_IMAGE_LIST := \
        device/xxxx/yyyy/test/test.img \
        device/xxxx/yyyy/test/test1.img
    
    To resign the custom images in the target zip file, the
    avb_extra_custom_image_key, avb_extra_custom_image_algorithms and
    avb_extra_custom_image_extra_args options are added to the
    sign_target_files_apks tool too. The following test cases list some
    examples  about how to use them.
    
    BUG: 154171021
    Test: 1) "atest --host releasetools_test releasetools_py3_test -c"
      2) Build images by 'make dist', sign and validate target files.
       a) Test on dist w/ chained vbmeta_system and ome custom images
       ./build/make/tools/releasetools/sign_target_files_apks -d certs \
         --avb_extra_custom_image_key oem=oem_rsa4096.pem \
         --avb_extra_custom_image_algorithm oem=SHA256_RSA4096 \
         xxx-target_xxx.zip signed.zip
       ./build/make/tools/releasetools/validate_target_files.py signed.zip
       Flash image and boot up.
       Verify the oem images and vbmeta images in OUT and target zips by
       avbtool.
    
       b) Test on dist w/ chained vbmeta_system and oem and test custom images
       ./build/make/tools/releasetools/sign_target_files_apks -d certs \
         --avb_extra_custom_image_key oem=oem_rsa4096.pem \
         --avb_extra_custom_image_algorithm oem=SHA256_RSA4096 \
         --avb_extra_custom_image_key test=test_rsa4096.pem \
         --avb_extra_custom_image_algorithm test=SHA256_RSA4096 \
         xxx-target_xxx.zip signed.zip
       ./build/make/tools/releasetools/validate_target_files.py signed.zip
       Verify the oem, test images and vbmeta images in OUT and target zips
       by avbtool.
    
       4) Test on dist w/o chained partition.
       ./build/make/tools/releasetools/sign_target_files_apks -d certs \
         xxx-target_xxx.zip signed.zip
       ./build/make/tools/releasetools/validate_target_files.py signed.zip
       Flash image and boot up.
       Verify the vbmeta images in OUT and target zips by avbtool.
    
    Change-Id: Ifccfee5e8909697eef6ccda0cc352fa16a9f6db6

--- tools/releasetools/verity_utils.py
+++ tools/releasetools/verity_utils.py
@@ -19,6 +19,7 @@ from __future__ import print_function
 import logging
 import os.path
 import shlex
+import shutil
 import struct
 
 import common
@@ -695,3 +696,20 @@ class VerifiedBootVersion1HashtreeInfoGenerator(HashtreeInfoGenerator):
       raise HashtreeInfoGenerationError("Failed to reconstruct the verity tree")
 
     return self.hashtree_info
+
+
+def CreateCustomImageBuilder(info_dict, partition_name, partition_size,
+                            key_path, algorithm, extra_args):
+  builder = None
+  if info_dict.get("avb_enable") == "true":
+    builder = VerifiedBootVersion2VerityImageBuilder(
+        partition_name,
+        partition_size,
+        VerifiedBootVersion2VerityImageBuilder.AVB_HASHTREE_FOOTER,
+        info_dict.get("avb_avbtool"),
+        key_path,
+        algorithm,
+        info_dict.get("avb_salt"),
+        extra_args)
+
+  return builder
