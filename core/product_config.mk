0c65af4c4f88f2ab329780a7f7145d9474a92da9
Ulya Trafimovich <skvadrik@google.com>
2020-04-30 10:59:48 +0100

    Use two-column format for PRODUCT_BOOT_JARS components.
    The first component is the apex name, or a special name "platform"
    if the boot jar is a platform jar rather than a part of some apex.
    This is a prerequisite change for moving core-icu4j to a separate
    com.android.i18n apex.
    
    Old one-column format is still supported, but all unqualified
    components of PRODUCT_BOOT_JARS get "platform:" prepended to them
    after reading the product makefiles.
    
    Test: aosp_walleye-userdebug boots
    Bug: 138994281
    Change-Id: I0f79c7d10477880ca65354251a5d1ca0b7ce79ab

--- core/product_config.mk
+++ core/product_config.mk
@@ -231,6 +231,12 @@ PRODUCT_AAPT_CONFIG := $(subst $(space),$(comma),$(PRODUCT_AAPT_CONFIG))
 # Extra boot jars must be appended at the end after common boot jars.
 PRODUCT_BOOT_JARS += $(PRODUCT_BOOT_JARS_EXTRA)
 
+# Add 'platform:' prefix to unqualified boot jars
+PRODUCT_BOOT_JARS := $(foreach pair,$(PRODUCT_BOOT_JARS), \
+  $(eval word1 := $(call word-colon,1,$(pair))) \
+  $(eval word2 := $(call word-colon,2,$(pair))) \
+  $(if $(word2),$(pair),platform:$(word1)))
+
 # The extra system server jars must be appended at the end after common system server jars.
 PRODUCT_SYSTEM_SERVER_JARS += $(PRODUCT_SYSTEM_SERVER_JARS_EXTRA)
 
@@ -271,11 +277,9 @@ ifdef PRODUCT_DEFAULT_DEV_CERTIFICATE
 endif
 
 $(foreach pair,$(PRODUCT_UPDATABLE_BOOT_JARS), \
-  $(if $(findstring $(call word-colon,2,$(pair)),$(PRODUCT_BOOT_JARS)), \
-    $(error A jar in PRODUCT_UPDATABLE_BOOT_JARS must not be in PRODUCT_BOOT_JARS, \
-      but $(call word-colon,2,$(pair)) is) \
-  ) \
-)
+  $(eval jar := $(call word-colon,2,$(pair))) \
+  $(if $(findstring $(jar), $(PRODUCT_BOOT_JARS)), \
+    $(error A jar in PRODUCT_UPDATABLE_BOOT_JARS must not be in PRODUCT_BOOT_JARS, but $(jar) is)))
 
 ENFORCE_SYSTEM_CERTIFICATE := $(PRODUCT_ENFORCE_ARTIFACT_SYSTEM_CERTIFICATE_REQUIREMENT)
 ENFORCE_SYSTEM_CERTIFICATE_WHITELIST := $(PRODUCT_ARTIFACT_SYSTEM_CERTIFICATE_REQUIREMENT_WHITELIST)
