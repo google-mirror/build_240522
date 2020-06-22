296a3af26b591c4139031d90b12c78ba06a480ce
Jiyong Park <jiyong@google.com>
2020-06-22 20:15:55 +0900

    Some properties are set as optional
    Some properties that are designed to provide a safe default value are
    explicitly set as optional using the 'a ?= b' syntax.
    
    Bug: 117892318
    Bug: 158735147
    Test: m
    Change-Id: Ie6a50ab7e0bcb210e282bc18e8c1daf412903f90

--- target/product/full_base_telephony.mk
+++ target/product/full_base_telephony.mk
@@ -21,7 +21,7 @@
 
 PRODUCT_VENDOR_PROPERTIES := \
     keyguard.no_require_sim=true \
-    ro.com.android.dataroaming=true
+    ro.com.android.dataroaming?=true
 
 PRODUCT_COPY_FILES := \
     device/sample/etc/apns-full-conf.xml:system/etc/apns-conf.xml \
