e5c2d700eb441b79c1c3f7ccbe791f1ed5c2edff
Howard Chen <howardsoc@google.com>
2020-07-27 15:31:02 +0800

    Move HIDL interfaces not included in mainline_system.mk to system_ext
    Test: aosp_wembley-userdebug
    Bug: 161688072
    
    Change-Id: I30d58ea8fc9baca3408d745cd6232748d84f486e

--- target/product/mainline_system.mk
+++ target/product/mainline_system.mk
@@ -79,6 +79,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio.config@1.0 \
     android.hardware.radio.deprecated@1.0 \
     android.hardware.secure_element@1.0 \
+    android.hardware.soundtrigger@2.1 \
+    android.hardware.soundtrigger@2.2 \
     android.hardware.wifi@1.0 \
     libaudio-resampler \
     libaudiohal \
