fbce3ec31af9e18bfd3edcbda641752f7753fca9
JaeMan <jaeman@google.com>
2020-08-11 02:37:01 +0000

    Rename mainline_system(.*).mk to generic_system(.*).mk
    Rename mainline_system to generic_system in mk files
    to avoid confusing with mainline module.
    
    First, move mainline_system(.*).mk to generic_system(.*).mk
    and make previous mainline_system(.*).mk to symlink to
    generic_system(.*).mk.
    
    These symlinks will be kept remain during migration
    for preventing build breakage and making changes to
    mainline_system(.*).mk possile during migration.
    
    Bug: 160649682
    Test: m nothing
    Change-Id: I3609ec2d91b0f08859a36cac75c25876b1db6d6a

--- target/product/AndroidProducts.mk
+++ target/product/AndroidProducts.mk
@@ -57,10 +57,10 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/generic.mk \
     $(LOCAL_DIR)/generic_x86.mk \
     $(LOCAL_DIR)/gsi_arm64.mk \
-    $(LOCAL_DIR)/mainline_system_arm64.mk \
-    $(LOCAL_DIR)/mainline_system_x86.mk \
-    $(LOCAL_DIR)/mainline_system_x86_arm.mk \
-    $(LOCAL_DIR)/mainline_system_x86_64.mk \
+    $(LOCAL_DIR)/generic_system_arm64.mk \
+    $(LOCAL_DIR)/generic_system_x86.mk \
+    $(LOCAL_DIR)/generic_system_x86_arm.mk \
+    $(LOCAL_DIR)/generic_system_x86_64.mk \
     $(LOCAL_DIR)/sdk_arm64.mk \
     $(LOCAL_DIR)/sdk.mk \
     $(LOCAL_DIR)/sdk_phone_arm64.mk \
