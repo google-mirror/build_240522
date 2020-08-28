35d0c12513803699970ab0722393281730edf633
Howard Chen <howardsoc@google.com>
2020-08-28 12:02:23 +0800

    Add the PRODUCT_SYSTEM_FREEZE check for files installed to system
    The PRODUCT_SYSTEM_FREEZE is an optional check for a product to
    compare files installed to the system partition against a snapshot
    with a specified mode. It can check added files, deleted files or both.
    For example, adding the line below enables check for the aosp_bar-userdebug
    target
    
    vendor/foo/aosp_bar.mk
    
    PRODUCT_SYSTEM_FREEZE := vendor/foo/aosp_bar-installed-files.txt:+-
    
    Test: build aosp_wembley-userdebug
    Bug: 165516786
    Change-Id: I6c616ebe22d5fe388b07ed1198757d6330d2883a

--- tools/fileslist_util.py
+++ tools/fileslist_util.py
@@ -29,16 +29,41 @@ def PrintCanonicalList(path):
   for line in data:
     print "{0:12d}  {1}".format(line["Size"], line["Name"])
 
+def CompareFileNames(path, snapshot, mode):
+  with open(path) as jf:
+    data = json.load(jf)
+  new_files = []
+  for line in data:
+    new_files.append(str(line["Name"]))
+  with open(snapshot) as f:
+    ori_files = f.read().splitlines()
+  error = False
+  if '+' in mode:
+    added = frozenset(new_files).difference(ori_files)
+    for d in added:
+      print("  +" + d)
+      error = True
+  if '-' in mode:
+    deled = frozenset(ori_files).difference(new_files)
+    for d in deled:
+      print("  -" + d)
+      error = True
+  if error:
+    print("Fatal differences are detected. Or you may want to update the snapshot with:")
+    print(" %s -n %s > %s " % (sys.argv[0], path, snapshot))
+    sys.exit(2)
+
 def PrintUsage(name):
   print("""
-Usage: %s -[nc] json_files_list
+Usage: %s -[nc] json_files_list | -f jason_files_list snapshot_flies_list:[+-]*
  -n produces list of files only
  -c produces classic installed-files.txt
+ -f compare list of files with the snapshot and the specified mode
 """ % (name))
 
 def main(argv):
   try:
-    opts, args = getopt.getopt(argv[1:], "nc", "")
+    opts, args = getopt.getopt(argv[1:], "ncf", "")
   except getopt.GetoptError, err:
     print(err)
     PrintUsage(argv[0])
@@ -61,6 +86,16 @@ def main(argv):
     elif o == ("-c"):
       PrintCanonicalList(args[0])
       sys.exit()
+    elif o == ("-f"):
+      if len(args) < 2:
+        print("PRODUCT_SYSTEM_FREEZE is not specified")
+        return 
+      t = args[1].split(':')
+      if len(t) < 2:
+        print("No compare mode provided")
+        PrintUsage(argv[0])
+        sys.exit(2)
+      CompareFileNames(args[0], t[0], t[1])
     else:
       assert False, "Unsupported option"
 
