00bedce9e5789e1f157daeb921af3ef09ef8f256
Yifan Hong <elsk@google.com>
2020-09-24 18:18:26 -0700

    Add ramdisk/build.prop, and add contents to recovery prop
    This is a build.prop file for ramdisk. Properties uses the
    name ro.[product.]bootimage*.
    
    These ro.[product.]bootimage.* properties are also included in recovery
    properties.
    
    Test: m bootimage and inspect output
    Bug: 169169031
    Bug: 162623577
    
    Change-Id: I94b993ce3214356036d038b6db57c4e1b755c111

--- core/sysprop.mk
+++ core/sysprop.mk
@@ -429,3 +429,12 @@ $(eval $(call build-properties,\
     $(_prop_files_),\
     $(_prop_vars_),\
     $(empty)))
+
+# ----------------------------------------------------------------
+# ramdisk/prop.default
+#
+
+INSTALLED_RAMDISK_BUILD_PROP_TARGET := $(TARGET_RAMDISK_OUT)/build.prop
+$(eval $(call build-properties,\
+    bootimage,\
+    $(INSTALLED_RAMDISK_BUILD_PROP_TARGET)))
