802ab97ec856e69f9e37b7008874646a88ac3188
Fabien Sanglard <sanglardf@google.com>
2020-10-20 15:47:10 -0700

    Fix zipalign alignment error
    Problem: Zipalign operates over several false assumptions. First it
    assumes that zip entries are in the same order in the body and in
    the Central Direcotry. Second, it assumes there are not space
    between entries. This makes alignment incorrect when these asserts
    are not true.
    
    Solution: Don't align entries by tracking bias based on input zip
    entry location. Calculate the expected alignment based on the out-
    put zip file and correct with extra padding.
    
    Fixes: 162117652
    Test: Units Tests
    Change-Id: Ia179338f658cab18a377cba2c7c8e629089a2785

--- tools/zipalign/tests/src/align_test.cpp
+++ tools/zipalign/tests/src/align_test.cpp
@@ -13,3 +13,29 @@ TEST(Align, Unaligned) {
   int result = process(src, dst, 4, true, false, 4096);
   ASSERT_EQ(0, result);
 }
+
+// Align a zip featuring a hole at the beginning. The
+// hole in the archive is a delete entry in the Central
+// Directory.
+TEST(Align, Holes) {
+  const char* src = "tests/data/holes.zip";
+  const char* dst = "tests/data/holes_out.zip";
+
+  int processed = process(src, dst, 4, true, false, 4096);
+  ASSERT_EQ(0, processed);
+
+  int verified = verify(dst, 4, false, true);
+  ASSERT_EQ(0, verified);
+}
+
+// Align a zip where LFH order and CD entries differ.
+TEST(Align, DifferenteOrders) {
+  const char* src = "tests/data/diffOrders.zip";
+  const char* dst = "tests/data/diffOrders_out.zip";
+
+  int processed = process(src, dst, 4, true, false, 4096);
+  ASSERT_EQ(0, processed);
+
+  int verified = verify(dst, 4, false, true);
+  ASSERT_EQ(0, verified);
+}
