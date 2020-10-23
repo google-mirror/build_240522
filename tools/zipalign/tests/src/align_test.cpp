6c903f7fc7436e325dcb552dbbb44a846d3f76c5
Fabien Sanglard <sanglardf@google.com>
2020-10-22 17:58:12 -0700

    Refactor zipalign to allow unit tests
    Test: Self-tested
    Bug: NA
    Change-Id: Ib7496092f4482384d2b54046c867f41e7ff4dfdc

---
+++ tools/zipalign/tests/src/align_test.cpp
@@ -0,0 +1,20 @@
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+#include "ZipAlign.h"
+
+#include <stdio.h>
+#include <string>
+
+#include <android-base/file.h>
+
+using namespace android;
+
+static std::string test_data_dir = android::base::GetExecutableDirectory() + "/tests/data";
+
+TEST(Align, Unaligned) {
+  const std::string src = test_data_dir + "unaligned.zip";
+  const std::string dst = test_data_dir + "/unaligned_out.zip";
+  int result = process(src.c_str(), dsti.c_str(), 4, true, false, 4096);
+  ASSERT_EQ(0, result);
+}
