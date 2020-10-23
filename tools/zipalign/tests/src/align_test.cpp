18036f5a44e00b069df52e7c00677e46533f1842
Fabien Sanglard <sanglardf@google.com>
2020-10-22 17:58:12 -0700

    Fix zipalign tests with GetExecutableDirectory
    Test: Self-tested
    Bug: NA
    Change-Id: Idc15c0e028b74a2b38c26af0e7d4a51f32210145

--- tools/zipalign/tests/src/align_test.cpp
+++ tools/zipalign/tests/src/align_test.cpp
@@ -4,12 +4,18 @@
 #include "ZipAlign.h"
 
 #include <stdio.h>
+#include <string>
 
 using namespace android;
 
+#include <android-base/file.h>
+
+
+static std::string test_data_dir = android::base::GetExecutableDirectory() + "/tests/data";
+
 TEST(Align, Unaligned) {
-  const char* src = "tests/data/unaligned.zip";
-  const char* dst = "tests/data/unaligned_out.zip";
-  int result = process(src, dst, 4, true, false, 4096);
+  const std::string src = test_data_dir + "unaligned.zip";
+  const std::string dst = test_data_dir + "/unaligned_out.zip";
+  int result = process(src.c_str(), dsti.c_str(), 4, true, false, 4096);
   ASSERT_EQ(0, result);
 }
