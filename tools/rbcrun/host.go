c63640b83e9c3fd0770fd730062f34c9a1d38575
Sasha Smundak <asmundak@google.com>
2020-10-26 15:43:21 -0700

    Roboleaf product configuration runner
    Test: self test
    Fixes: 180529448
    Change-Id: I7d728b47d3f381b7052a0d7d51c9e698e5c2e316

---
+++ tools/rbcrun/host.go
@@ -0,0 +1,289 @@
+package rbcrun
+
+import (
+	"fmt"
+	"go.starlark.net/starlark"
+	"go.starlark.net/starlarkstruct"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
+)
+
+const callerDirKey = "callerDir"
+
+var LoadPathRoot = "."
+var shellPath string
+
+type modentry struct {
+	globals starlark.StringDict
+	err     error
+}
+
+var moduleCache = make(map[string]*modentry)
+
+var builtins starlark.StringDict
+
+func moduleName2AbsPath(moduleName string, callerDir string) (string, error) {
+	path := moduleName
+	if ix := strings.LastIndex(path, ":"); ix >= 0 {
+		path = path[0:ix] + string(os.PathSeparator) + path[ix+1:]
+	}
+	if strings.HasPrefix(path, "//") {
+		return filepath.Abs(filepath.Join(LoadPathRoot, path[2:]))
+	} else if strings.HasPrefix(moduleName, ":") {
+		return filepath.Abs(filepath.Join(callerDir, path[1:]))
+	} else {
+		return filepath.Abs(path)
+	}
+}
+
+// loader implements load statement. The format of the loaded module URI is
+//  [//path]:base[|symbol]
+// The file path is $ROOT/path/base if path is present, <caller_dir>/base otherwise.
+// The presence of `|symbol` indicates that the loader should return a single 'symbol'
+// bound to None if file is missing.
+func loader(thread *starlark.Thread, module string) (starlark.StringDict, error) {
+	pipePos := strings.LastIndex(module, "|")
+	mustLoad := pipePos < 0
+	var defaultSymbol string
+	if !mustLoad {
+		defaultSymbol = module[pipePos+1:]
+		module = module[:pipePos]
+	}
+	modulePath, err := moduleName2AbsPath(module, thread.Local(callerDirKey).(string))
+	if err != nil {
+		return nil, err
+	}
+	e, ok := moduleCache[modulePath]
+	if e == nil {
+		if ok {
+			return nil, fmt.Errorf("cycle in load graph")
+		}
+
+		// Add a placeholder to indicate "load in progress".
+		moduleCache[modulePath] = nil
+
+		// Decide if we should load.
+		if !mustLoad {
+			if _, err := os.Stat(modulePath); err == nil {
+				mustLoad = true
+			}
+		}
+
+		// Load or return default
+		if mustLoad {
+			childThread := &starlark.Thread{Name: "exec " + module, Load: thread.Load}
+			// Cheating for the sake of testing:
+			// propagate starlarktest's Reporter key, otherwise testing
+			// the load function may cause panic in starlarktest code.
+			const testReporterKey = "Reporter"
+			if v := thread.Local(testReporterKey); v != nil {
+				childThread.SetLocal(testReporterKey, v)
+			}
+
+			childThread.SetLocal(callerDirKey, filepath.Dir(modulePath))
+			globals, err := starlark.ExecFile(childThread, modulePath, nil, builtins)
+			e = &modentry{globals, err}
+		} else {
+			e = &modentry{starlark.StringDict{defaultSymbol: starlark.None}, nil}
+		}
+
+		// Update the cache.
+		moduleCache[modulePath] = e
+	}
+	return e.globals, e.err
+}
+
+// loadGenerated("cmd", [args]) runs command which generates Starlark script on stdout
+// and then executes this script.
+func loadGenerated(_ *starlark.Thread, b *starlark.Builtin, args starlark.Tuple,
+	kwargs []starlark.Tuple) (starlark.Value, error) {
+	var command string
+	var stringList *starlark.List
+	if err := starlark.UnpackArgs(b.Name(), args, kwargs, "c", &command, "a", &stringList); err != nil {
+		return starlark.None, err
+	}
+
+	cmdArgs := make([]string, stringList.Len())
+	for i := 0; i < stringList.Len(); i++ {
+		v := stringList.Index(i)
+		if strVal, ok := v.(starlark.String); ok {
+			cmdArgs[i] = string(strVal)
+		} else {
+			return starlark.None, fmt.Errorf("command argument list can contain only strings, got %s", v.Type())
+		}
+	}
+	cmd := exec.Command(command, cmdArgs...)
+	bytes, err := cmd.Output()
+	if err != nil {
+		return starlark.None, fmt.Errorf("'%s' failed: %s", cmd, err)
+	}
+	loadGenThread := &starlark.Thread{
+		Name: "loadGenerated " + command,
+		Load: loader,
+	}
+	_, err = starlark.ExecFile(loadGenThread, fmt.Sprintf("$(%s)", cmd),
+		string(bytes), builtins)
+	if err != nil {
+		return starlark.None, err
+	}
+	return starlark.None, nil
+}
+
+// fileExists returns True if file with given name exists.
+func fileExists(_ *starlark.Thread, b *starlark.Builtin, args starlark.Tuple,
+	kwargs []starlark.Tuple) (starlark.Value, error) {
+	var path string
+	if err := starlark.UnpackPositionalArgs(b.Name(), args, kwargs, 1, &path); err != nil {
+		return starlark.None, err
+	}
+	if stat, err := os.Stat(path); err != nil || stat.IsDir() {
+		return starlark.False, nil
+	}
+	return starlark.True, nil
+}
+
+// regexMatch(pattern, s) returns True if s matches pattern (a regex)
+func regexMatch(_ *starlark.Thread, b *starlark.Builtin, args starlark.Tuple,
+	kwargs []starlark.Tuple) (starlark.Value, error) {
+	var pattern, s string
+	if err := starlark.UnpackPositionalArgs(b.Name(), args, kwargs, 2, &pattern, &s); err != nil {
+		return starlark.None, err
+	}
+	match, err := regexp.MatchString(pattern, s)
+	if err != nil {
+		return starlark.None, err
+	}
+	if match {
+		return starlark.True, nil
+	}
+	return starlark.False, nil
+}
+
+// wildcard(pattern, top=None) expands shell's glob pattern. If 'top' is present,
+// the 'top/pattern' is globbed and then 'top/' prefix is removed.
+func wildcard(_ *starlark.Thread, b *starlark.Builtin, args starlark.Tuple,
+	kwargs []starlark.Tuple) (starlark.Value, error) {
+	var pattern string
+	var top string
+
+	if err := starlark.UnpackPositionalArgs(b.Name(), args, kwargs, 1, &pattern, &top); err != nil {
+		return starlark.None, err
+	}
+
+	var files []string
+	var err error
+	if top == "" {
+		if files, err = filepath.Glob(pattern); err != nil {
+			return starlark.None, err
+		}
+	} else {
+		prefix := top + string(filepath.Separator)
+		if files, err = filepath.Glob(prefix + pattern); err != nil {
+			return starlark.None, err
+		}
+		for i := range files {
+			files[i] = strings.TrimPrefix(files[i], prefix)
+		}
+	}
+	return makeStringList(files), nil
+}
+
+// shell(command) runs OS shell with given command and returns back
+// its output the same way as Make's $(shell ) function. The end-of-lines
+// ("\n" or "\r\n") are replaced with " " in the result, and the trailing
+// end-of-line is removed.
+func shell(_ *starlark.Thread, b *starlark.Builtin, args starlark.Tuple,
+	kwargs []starlark.Tuple) (starlark.Value, error) {
+	var command string
+	if err := starlark.UnpackPositionalArgs(b.Name(), args, kwargs, 1, &command); err != nil {
+		return starlark.None, err
+	}
+	if shellPath == "" {
+		return starlark.None,
+			fmt.Errorf("cannot run shell, SHELL environment variable is not set (running on Windows?)")
+	}
+	cmd := exec.Command(shellPath, "-c", command)
+	// We ignore command's status
+	bytes, _ := cmd.Output()
+	output := string(bytes)
+	if strings.HasSuffix(output, "\n") {
+		output = strings.TrimSuffix(output, "\n")
+	} else {
+		output = strings.TrimSuffix(output, "\r\n")
+	}
+
+	return starlark.String(
+		strings.ReplaceAll(
+			strings.ReplaceAll(output, "\r\n", " "),
+			"\n", " ")), nil
+}
+
+func makeStringList(items []string) *starlark.List {
+	elems := make([]starlark.Value, len(items))
+	for i, item := range items {
+		elems[i] = starlark.String(item)
+	}
+	return starlark.NewList(elems)
+}
+
+// propsetFromEnv constructs a propset from the array of KEY=value strings
+func propsetFromEnv(env []string) *PropSet {
+	ps := &PropSet{properties: make(map[string]starlark.Value, len(env))}
+	for _, x := range env {
+		kv := strings.SplitN(x, "=", 2)
+		ps.properties[kv[0]] = starlark.String(kv[1])
+	}
+	return ps
+}
+
+func setup(env []string) {
+	builtins = starlark.StringDict{
+		"loadGenerated":    starlark.NewBuiltin("loadGenerated", loadGenerated),
+		"module":           starlark.NewBuiltin("module", starlarkstruct.MakeModule),
+		"propset":          starlark.NewBuiltin("propset", MakePropset),
+		"psetdefault":      starlark.NewBuiltin("psetdefault", propsetSetDefault),
+		"stack":            starlark.NewBuiltin("stack", MakeStack),
+		"rblf_cli":         propsetFromEnv(env),
+		"rblf_env":         propsetFromEnv(os.Environ()),
+		"rblf_file_exists": starlark.NewBuiltin("rblf_file_exists", fileExists),
+		"rblf_regex":       starlark.NewBuiltin("rblf_match", regexMatch),
+		"rblf_shell":       starlark.NewBuiltin("rblf_shell", shell),
+		"rblf_wildcard":    starlark.NewBuiltin("rblf_wildcard", wildcard),
+	}
+
+	// TODO(asmundak): on Windows, it's rather COMSPEC than SHELL, but
+	// check the other differences before supporting it on Windows
+	const shellKey = "SHELL="
+	for _, e := range os.Environ() {
+		if strings.HasPrefix(e, shellKey) {
+			shellPath = e[len(shellKey):]
+		}
+	}
+}
+
+// Parses, resolves, and executes a Starlark file.
+// filename and src parameters are as for starlark.ExecFile:
+// * filename is the name of the file to execute,
+//   and the name that appears in error messages;
+// * src is an optional source of bytes to use instead of filename
+//   (it can be a string, or a byte array, or an io.Reader instance)
+// * commandVars is an array of "VAR=value" items. They are accessible from
+//   the starlark script as members of the `rblf_cli` propset.
+func Run(filename string, src interface{}, commandVars []string) error {
+	setup(commandVars)
+
+	mainThread := &starlark.Thread{
+		Name:  "main",
+		Print: func(_ *starlark.Thread, msg string) { fmt.Println(msg) },
+		Load:  loader,
+	}
+	absPath, err := filepath.Abs(filename)
+	if err == nil {
+		mainThread.SetLocal(callerDirKey, filepath.Dir(absPath))
+		_, err = starlark.ExecFile(mainThread, absPath, src, builtins)
+	}
+	return err
+}
---
+++ tools/rbcrun/stack.go
@@ -0,0 +1,145 @@
+package rbcrun
+
+import (
+	"fmt"
+	"go.starlark.net/starlark"
+	"strings"
+)
+
+// Stack is an iterable.
+// Unlike  the available iterables (lists, etc.) stack can be updated
+// during the iteration (and this is the reason to have it).
+
+var stackMethods = map[string]*starlark.Builtin{
+	"pop":  starlark.NewBuiltin("write", stackPop),
+	"push": starlark.NewBuiltin("unread", stackPush),
+}
+
+type Stack struct {
+	items     []starlark.Value
+	itercount uint32
+	frozen    bool
+}
+
+func (st *Stack) Attr(name string) (starlark.Value, error) {
+	if v, found := stackMethods[name]; found {
+		return v.BindReceiver(st), nil
+	}
+	return nil, nil
+}
+
+func (st *Stack) AttrNames() []string {
+	var names []string
+	for k := range stackMethods {
+		names = append(names, k)
+	}
+	return names
+}
+
+func (st *Stack) String() string {
+	buf := new(strings.Builder)
+	buf.WriteString("[")
+	const maxStack = 1000
+	for i, item := range st.items {
+		if i > 0 {
+			buf.WriteString(", ")
+		}
+		if i > maxStack {
+			buf.WriteString("...")
+			break
+		}
+		buf.WriteString(item.String())
+	}
+	buf.WriteString("]")
+	return buf.String()
+}
+
+func (st *Stack) Type() string {
+	return "stack"
+}
+
+func (st *Stack) Freeze() {
+	if st.frozen {
+		return
+	}
+	st.frozen = true
+	for _, item := range st.items {
+		item.Freeze()
+	}
+}
+
+func (st *Stack) Truth() starlark.Bool {
+	return len(st.items) > 0
+}
+
+func (st *Stack) Hash() (uint32, error) {
+	return 0, fmt.Errorf("unhashable type: stack")
+}
+
+func (st *Stack) Iterate() starlark.Iterator {
+	if !st.frozen {
+		st.itercount++
+	}
+	return &stackIterator{st}
+}
+
+func (st *Stack) pop() (starlark.Value, bool) {
+	n := len(st.items) - 1
+	if n < 0 {
+		return starlark.None, false
+	}
+	v := st.items[n]
+	st.items = st.items[:n]
+	return v, true
+}
+
+// Pushes an item.
+func stackPush(_ *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var v starlark.Value
+	if err := starlark.UnpackPositionalArgs(fn.Name(), args, kwargs, 1, &v); err != nil {
+		return nil, err
+	}
+	recv := fn.Receiver().(*Stack)
+	if recv.frozen {
+		return nil, fmt.Errorf("%s: stack is frozen", fn.Name())
+	}
+	recv.items = append(recv.items, v)
+	return starlark.None, nil
+}
+
+// Pops the top item
+func stackPop(_ *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	if err := starlark.UnpackPositionalArgs(fn.Name(), args, kwargs, 0); err != nil {
+		return nil, err
+	}
+	st := fn.Receiver().(*Stack)
+	if v, ok := st.pop(); ok {
+		return v, nil
+	}
+	return nil, fmt.Errorf("stack is empty")
+}
+
+
+func MakeStack(_ *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	q := &Stack{}
+	if err := starlark.UnpackPositionalArgs(fn.Name(), args, kwargs, 0); err != nil {
+		return nil, nil
+	}
+	return q, nil
+}
+
+type stackIterator struct {
+	stack *Stack
+}
+
+func (ist *stackIterator) Next(p *starlark.Value) bool {
+	var ok bool
+	*p, ok = ist.stack.pop()
+	return ok
+}
+
+func (ist *stackIterator) Done() {
+	if !ist.stack.frozen {
+		ist.stack.itercount--
+	}
+}
