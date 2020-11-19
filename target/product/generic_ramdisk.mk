9d92463fbcb7332427bef8a73452db23dd876d4d
Yifan Hong <elsk@google.com>
2020-11-19 15:55:43 -0800

    Set allow list for generic ramdisk & remove e2fsck from GKI
    Devices that uses generic ramdisk must inherit from generic_ramdisk.mk.
    This makefile ensures that only a set of files can be installed to the
    ramdisk. Other files must be installed to the vendor-ramdisk.
    
    Let aosp_arm64 use this makefile. As a result, e2fsck is removed from
    the generic ramdisk.
    
    Bug: 173742069
    Test: manual
    Change-Id: I814735156756c73a54d54c0b5e17d75cbac128b1

---
+++ target/product/generic_ramdisk.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This makefile installs contents of the generic ramdisk.
+# Inherit from this makefile to declare that this product uses generic ramdisk.
+# This makefile checks that other makefiles must not install things to the
+# ramdisk.
+
+PRODUCT_NAME := generic_ramdisk
+PRODUCT_BRAND := generic
+
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init_first_stage \
+
+# Debug ramdisk
+PRODUCT_PACKAGES += \
+    userdebug_plat_sepolicy.cil \
+
+_my_paths := \
+    $(TARGET_COPY_OUT_RAMDISK) \
+    $(TARGET_COPY_OUT_DEBUG_RAMDISK) \
+
+_my_relaxed_allowed_list := \
+    system/usr/share/zoneinfo/tz_version \
+    system/usr/share/zoneinfo/tzdata \
+
+$(call require-artifacts-in-path, $(_my_paths), , $(_my_relaxed_allowed_list))
