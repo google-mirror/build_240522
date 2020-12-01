672c0aea54873045db7c7665d06df5a4c145b5ef
Roman Kiryanov <rkir@google.com>
2020-11-30 18:17:22 -0800

    Move network related packages to generic_system.mk
    These packages are build for the system partition
    but not listed in generic_system.mk, this break
    the build if package partition requirement is
    enforsed. Since network packages are required
    for all devices we want to list these packages
    in generic_system.mk.
    
    Bug: 167689596
    Test: presubmit
    Signed-off-by: Roman Kiryanov <rkir@google.com>
    Change-Id: I1176eb8d6ef5fabe38e5dfb0ac5ccdfe2aba7dc6

--- target/product/generic_system.mk
+++ target/product/generic_system.mk
@@ -47,6 +47,13 @@ PRODUCT_PACKAGES += \
 # Wrapped net utils for /vendor access.
 PRODUCT_PACKAGES += netutils-wrapper-1.0
 
+# The network stack is required on all devices
+PRODUCT_PACKAGES += \
+    PlatformCaptivePortalLogin \
+    PlatformNetworkPermissionConfig \
+    CellBroadcastServiceModulePlatform \
+    com.android.tethering.inprocess
+
 # Charger images
 PRODUCT_PACKAGES += charger_res_images
 
