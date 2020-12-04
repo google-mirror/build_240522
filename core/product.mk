80f3af1c183ca46e0fa385a9f86c61fba74421b9
Yifan Hong <elsk@google.com>
2020-12-03 17:47:44 -0800

    require-artifacts-in-path support relaxed allowed list
    The relaxed allowed list does not get redundant allowed entries check.
    
    Test: pass
    Change-Id: Ifa12999d99362dd4eb5d138588543b6f54538fd8

--- core/product.mk
+++ core/product.mk
@@ -456,6 +456,7 @@ define require-artifacts-in-path
   $(eval current_mk := $(strip $(word 1,$(_include_stack)))) \
   $(eval PRODUCTS.$(current_mk).ARTIFACT_PATH_REQUIREMENTS := $(strip $(1))) \
   $(eval PRODUCTS.$(current_mk).ARTIFACT_PATH_ALLOWED_LIST := $(strip $(2))) \
+  $(eval PRODUCTS.$(current_mk).ARTIFACT_PATH_RELAXED_ALLOWED_LIST := $(strip $(3))) \
   $(eval ARTIFACT_PATH_REQUIREMENT_PRODUCTS := \
     $(sort $(ARTIFACT_PATH_REQUIREMENT_PRODUCTS) $(current_mk)))
 endef
