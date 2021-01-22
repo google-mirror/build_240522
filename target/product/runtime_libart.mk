b709e24ad28a1489b184f37015c3d90a25f73ab3
Yan Wang <yawanng@google.com>
2021-01-22 05:41:21 +0000

    Revert "Do not set ro.iorapd.enable=true"
    This reverts commit 4a4a4df0afa2bc94bf234905bd3810705e4916c3.
    
    Reason for revert: By default, this flag is unset. Iorap is disabled by default and rely on this flag to wake up in Rasta experiment. 
    
    Change-Id: I9630712b74cd411a5678632fb1ff695a84ed1048

--- target/product/runtime_libart.mk
+++ target/product/runtime_libart.mk
@@ -97,6 +97,10 @@ PRODUCT_SYSTEM_PROPERTIES += \
     dalvik.vm.minidebuginfo=true \
     dalvik.vm.dex2oat-minidebuginfo=true
 
+# Enable iorapd by default
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.iorapd.enable=true
+
 PRODUCT_USES_DEFAULT_ART_CONFIG := true
 
 # Disable iorapd by default
