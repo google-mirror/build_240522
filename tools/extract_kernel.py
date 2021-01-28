de3a31a6f73d3005af0ee4faca24dc01b137ec4f
cfig <yuyezhong@gmail.com>
2021-01-28 08:20:01 +0800

    extract_kernel: make it py2/py3 compatible
    use py3 for AOSP build, while the script itself can be
    executed under py2/py3 environment directly.
    
    TEST: extract config/version/compiler from Pixel 4 kernel
    
    Change-Id: I00db6dabb1ff93f0758017a666c476816caefae7

--- tools/extract_kernel.py
+++ tools/extract_kernel.py
@@ -39,12 +39,12 @@ COMPRESSION_ALGO = (
 # "Linux version " UTS_RELEASE " (" LINUX_COMPILE_BY "@"
 # LINUX_COMPILE_HOST ") (" LINUX_COMPILER ") " UTS_VERSION "\n";
 LINUX_BANNER_PREFIX = b'Linux version '
-LINUX_BANNER_REGEX = LINUX_BANNER_PREFIX + \
+LINUX_BANNER_REGEX = LINUX_BANNER_PREFIX.decode() + \
     r'(?P<release>(?P<version>[0-9]+[.][0-9]+[.][0-9]+).*) \(.*@.*\) \((?P<compiler>.*)\) .*\n'
 
 
 def get_from_release(input_bytes, start_idx, key):
-  null_idx = input_bytes.find('\x00', start_idx)
+  null_idx = input_bytes.find(b'\x00', start_idx)
   if null_idx < 0:
     return None
   try:
@@ -140,7 +140,7 @@ def try_decompress(cmd, search_bytes, input_bytes):
   while True:
     idx = input_bytes.find(search_bytes, idx)
     if idx < 0:
-      raise StopIteration()
+      return
 
     yield try_decompress_bytes(cmd, input_bytes[idx:])
     idx += 1
@@ -176,7 +176,10 @@ def dump_to_file(f, dump_fn, input_bytes, desc):
   if f is not None:
     o = decompress_dump(dump_fn, input_bytes)
     if o:
-      f.write(o)
+      if isinstance(o, str):
+        f.write(o.encode())
+      else:
+        f.write(o)
     else:
       sys.stderr.write(
           "Cannot extract kernel {}".format(desc))
