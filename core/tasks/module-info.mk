8eaba9af09397425c33caf2e616cd9bbd1395d26
Hadrien Zalek <hzalek@google.com>
2021-02-04 14:12:49 -0800

    [POC] Export the module test executable location
    Bug: 184900078
    Test: m atest; atest-dev HelloWorldTests --build_system_mode -m
    Change-Id: I08cab8d9347238796a9a5b98f15921d06f05b881

--- core/tasks/module-info.mk
+++ core/tasks/module-info.mk
@@ -20,6 +20,7 @@ $(MODULE_INFO_JSON):
 			'"srcjars": [$(foreach w,$(sort $(ALL_MODULES.$(m).SRCJARS)),"$(w)", )], ' \
 			'"classes_jar": [$(foreach w,$(sort $(ALL_MODULES.$(m).CLASSES_JAR)),"$(w)", )], ' \
 			'"test_mainline_modules": [$(foreach w,$(sort $(ALL_MODULES.$(m).TEST_MAINLINE_MODULES)),"$(w)", )], ' \
+			'"test_executable": [$(foreach w,$(sort $(ALL_MODULES.$(m).TEST_EXECUTABLE)),"$(w)", )], ' \
 			'"is_unit_test": "$(ALL_MODULES.$(m).IS_UNIT_TEST)", ' \
 			'},\n' \
 	 ) | sed -e 's/, *\]/]/g' -e 's/, *\}/ }/g' -e '$$s/,$$//' >> $@
