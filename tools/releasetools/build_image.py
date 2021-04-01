c21074944fc08dbfb1115331fc506c7b75ebd6e9
David Anderson <dvander@google.com>
2021-03-31 20:01:41 -0700

    Relax inode usage estimate.
    The current inode usage estimate applies a factor (1.04x) to account for
    differences between the directory and file count and what mkfs.ext4
    produces. Bump this to 1.06x to allow more leniency.
    
    This increases bramble partition sizes by 32KB total.
    
    Bug: 182365132
    Test: manual test
    Change-Id: I9ade82cb89422e31ae8c5df9fff31157e88278c5

--- tools/releasetools/build_image.py
+++ tools/releasetools/build_image.py
@@ -75,7 +75,7 @@ def GetInodeUsage(path):
   output = common.RunAndCheckOutput(cmd, verbose=False)
   # increase by > 4% as number of files and directories is not whole picture.
   inodes = output.count('\n')
-  spare_inodes = inodes * 4 // 100
+  spare_inodes = inodes * 6 // 100
   min_spare_inodes = 12
   if spare_inodes < min_spare_inodes:
     spare_inodes = min_spare_inodes
