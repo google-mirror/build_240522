b442b82b546bd57eaff75515c3e854a3a86a8865
Ulya Trafimovich <skvadrik@google.com>
2021-04-08 11:47:00 +0100

    Don't expect all Java modules to have a manifest.
    Some libraries that go through manifest_check do not have a manifest or
    APK, so there is nothing to check LOCAL_USES_LIBRARIES and
    LOCAL_OPTIONAL_USES_LIBRARIES against. Handle it as if the manifest had
    zero <uses-library> tags: don't fail the build unless the module has
    non-empty LOCAL_USES_LIBRARIES or LOCAL_OPTIONAL_USES_LIBRARIES.
    
    Bug: 132357300
    Test: lunch cf_x86_64_phone-userdebug && m
    Change-Id: I4b1317cfbd93cb6129caba51b56081307a564442

--- core/dex_preopt_odex_install.mk
+++ core/dex_preopt_odex_install.mk
@@ -208,6 +208,18 @@ add_json_class_loader_context = \
 # Verify <uses-library> coherence between the build system and the manifest.
 ################################################################################
 
+# Some libraries do not have a manifest, so there is nothing to check against.
+# Handle it as if the manifest had zero <uses-library> tags: it is ok unless the
+# module has non-empty LOCAL_USES_LIBRARIES or LOCAL_OPTIONAL_USES_LIBRARIES.
+ifndef my_manifest_or_apk
+  ifneq (,$(strip $(LOCAL_USES_LIBRARIES)$(LOCAL_OPTIONAL_USES_LIBRARIES)))
+    $(error $(LOCAL_MODULE) has non-empty LOCAL_USES_LIBRARIES or \
+      LOCAL_OPTIONAL_USES_LIBRARIES, but no manifest)
+  else
+    LOCAL_ENFORCE_USES_LIBRARIES := false
+  endif
+endif
+
 # Verify LOCAL_USES_LIBRARIES/LOCAL_OPTIONAL_USES_LIBRARIES
 # If LOCAL_ENFORCE_USES_LIBRARIES is not set, default to true if either of LOCAL_USES_LIBRARIES or
 # LOCAL_OPTIONAL_USES_LIBRARIES are specified.
