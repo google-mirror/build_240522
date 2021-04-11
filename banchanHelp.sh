5ee0753a3518118932c4c94596ed330b6617e346
Martin Stjernholm <mast@google.com>
2021-04-12 00:01:10 +0100

    Add new user setup command banchan for module building.
    It currently uses TARGET_BUILD_APPS just like tapas, but the use case is
    different and it may diverge more in the future.
    
    Test: banchan com.android.art
    Test: banchan help
    Test: banchan
    Test: hmm
    Bug: 179779520
    Change-Id: Iae718e65a2a7212c741c397e03c6f9a6d5ee8951

---
+++ banchanHelp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# locate some directories
+cd "$(dirname $0)"
+SCRIPT_DIR="${PWD}"
+cd ../..
+TOP="${PWD}"
+
+message='usage: banchan <module> ... [arm|x86|arm64|x86_64] [eng|userdebug|user]
+
+banchan selects individual modules to be built by the Android build system. Like
+"tapas", "banchan" does not request the building of images for a device but
+instead configures it for an unbundled build of the given modules, suitable for
+installing on any api-compatible device.
+
+The module names should match apex{} modules in Android.bp files, typically
+starting with "com.android.".
+
+The usage of the other arguments matches that of the rest of the platform
+build system and can be found by running `m help`'
+
+echo "$message"
