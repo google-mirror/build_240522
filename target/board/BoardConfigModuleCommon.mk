6762cd8d34b7bb465cee3703b340c6c009750ad4
Anton Hansson <hansson@google.com>
2021-04-19 10:18:43 +0100

    Set VNDK version to current for module builds
    This setting changes which libraries are included in the media.swcodec
    apex, and is a step on the way of eliminating the differences when
    building between module_arm64 and aosp_arm64.
    
    Bug: 185769808
    Bug: 185789027
    Test: compare media.swcodec apex build with module_arm64 and aosp_arm64
    Change-Id: Iafb0194827f984a7ba108f519016d24b2980e2e1

--- target/board/BoardConfigModuleCommon.mk
+++ target/board/BoardConfigModuleCommon.mk
@@ -4,3 +4,6 @@
 
 # Required for all module devices.
 TARGET_USES_64_BIT_BINDER := true
+
+# Necessary to make modules able to use the VNDK (which media.swcodec does).
+BOARD_VNDK_VERSION := current
