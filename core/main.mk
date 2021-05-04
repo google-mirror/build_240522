9e0b42fc957859891a1fe22f6253b6f18d12e9c0
Sasha Smundak <asmundak@google.com>
2021-05-04 09:58:38 -0700

    Integrate Starlark-based configuration runner
    If RBC_PRODUCT_CONFIG variable is set, obtain product configuration
    variables by converting product configuration makefiles to Starlark
    files and then executing them.
    Also, introduce RBC_NO_PRODUCT_GRAPH variable to suppress product graph
    generation. We cannot generate product graph with Starlark, so this
    option allows to verify that the rest of the contents of the generated
    Ninja files remains the same when Starlark-based converter is used.
    
    Bug: 181797530
    Test: Manual
    Change-Id: Ic6173a9640f32766b71c02a2b1833ce7a278e4cc

--- core/main.mk
+++ core/main.mk
@@ -1206,6 +1206,15 @@ define auto-included-modules
 
 endef
 
+# TODO(asmundak):
+# `product-installed-files` and `host-installed-files` macros are called only once.
+# When it happens, per-file configuration attributes (those named
+# PRODUCT.<product-makefile>.<attribute>) have been already moved to the global
+# <attribute> variable, so there is no need to use
+# $(call get-product-var,$(1), FOO) because its value has been already
+# hoisted to FOO. Fix the macro instead of having Starlark configuration
+# separately.
+ifndef RBC_PRODUCT_CONFIG
 # Lists most of the files a particular product installs, including:
 # - PRODUCT_PACKAGES, and their LOCAL_REQUIRED_MODULES
 # - PRODUCT_COPY_FILES
@@ -1256,6 +1265,43 @@ define host-installed-files
   $(filter $(HOST_OUT)/%,$(call module-installed-files, $(_hif_modules))) \
   $(filter $(HOST_CROSS_OUT)/%,$(call module-installed-files, $(_hcif_modules)))
 endef
+else
+define product-installed-files
+  $(eval _pif_modules := $(PRODUCT_PACKAGES) \
+    $(if $(filter eng,$(tags_to_install)),$(PRODUCT_PACKAGES_ENG)) \
+    $(if $(filter debug,$(tags_to_install)),$(PRODUCT_PACKAGES_DEBUG)) \
+    $(if $(filter tests,$(tags_to_install)),$(PRODUCT_PACKAGES_TESTS)) \
+    $(if $(filter asan,$(tags_to_install)),$(PRODUCT_PACKAGES_DEBUG_ASAN)) \
+    $(if $(filter java_coverage,$(tags_to_install)),$(PRODUCT_PACKAGES_DEBUG_JAVA_COVERAGE)) \
+    $(call auto-included-modules) \
+  ) \
+  $(eval ### Filter out the overridden packages and executables before doing expansion) \
+  $(eval _pif_overrides := $(call module-overrides,$(_pif_modules))) \
+  $(eval _pif_modules := $(filter-out $(_pif_overrides), $(_pif_modules))) \
+  $(eval ### Resolve the :32 :64 module name) \
+  $(eval _pif_modules := $(sort $(call resolve-bitness-for-modules,TARGET,$(_pif_modules)))) \
+  $(call expand-required-modules,_pif_modules,$(_pif_modules),$(_pif_overrides)) \
+  $(filter-out $(HOST_OUT_ROOT)/%,$(call module-installed-files, $(_pif_modules))) \
+  $(call resolve-product-relative-paths,\
+    $(foreach cf,$(PRODUCT_COPY_FILES),$(call word-colon,2,$(cf))))
+endef
+
+# Similar to product-installed-files above, but handles PRODUCT_HOST_PACKAGES instead
+# This does support the :32 / :64 syntax, but does not support module overrides.
+define host-installed-files
+  $(eval _hif_modules := $(PRODUCT_HOST_PACKAGES)) \
+  $(eval ### Split host vs host cross modules) \
+  $(eval _hcif_modules := $(filter host_cross_%,$(_hif_modules))) \
+  $(eval _hif_modules := $(filter-out host_cross_%,$(_hif_modules))) \
+  $(eval ### Resolve the :32 :64 module name) \
+  $(eval _hif_modules := $(sort $(call resolve-bitness-for-modules,HOST,$(_hif_modules)))) \
+  $(eval _hcif_modules := $(sort $(call resolve-bitness-for-modules,HOST_CROSS,$(_hcif_modules)))) \
+  $(call expand-required-host-modules,_hif_modules,$(_hif_modules),HOST) \
+  $(call expand-required-host-modules,_hcif_modules,$(_hcif_modules),HOST_CROSS) \
+  $(filter $(HOST_OUT)/%,$(call module-installed-files, $(_hif_modules))) \
+  $(filter $(HOST_CROSS_OUT)/%,$(call module-installed-files, $(_hcif_modules)))
+endef
+endif
 
 # Fails the build if the given list is non-empty, and prints it entries (stripping PRODUCT_OUT).
 # $(1): list of files to print
