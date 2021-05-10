d5d4f7bff7303e2d2a670983b60eaad5b11805c0
Bowgo Tsai <bowgotsai@google.com>
2021-05-10 12:58:02 +0800

    Fix GKI 2.0 signing in the release process
    In the release signing process, it uses --signing_helper_with_files
    for avbtool. This can be done by passing
    --gki_signing_signature_args="--signing_helper_with_files=/path/to/helper.sh"
    for mkbootimg.
    
    However, this only works when there is a default value of
    "gki_signing_signature_args" in the misc_info.txt, to be appended
    with more extra args. Adding a default value "--prop foo:bar" to make
    it work.
    
    Bug: 178559811
    Bug: 177862434
    Test: `make bootimage`
          `unpack_bootimg --boot_img $OUT/boot.img --out unpack`
          `avbtool info_image --image ./unpack/boot_signature`
    Change-Id: I8bd8ad3acf324931b47d45fd30bc590206b1927e

--- target/board/generic_arm64/BoardConfig.mk
+++ target/board/generic_arm64/BoardConfig.mk
@@ -80,6 +80,10 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 # Enable GKI 2.0 signing.
 BOARD_GKI_SIGNING_KEY_PATH := build/make/target/product/gsi/testkey_rsa2048.pem
 BOARD_GKI_SIGNING_ALGORITHM := SHA256_RSA2048
+# The following is needed to allow release signing process appends more extra
+# args, e.g., passing --signing_helper_with_files from mkbootimg to avbtool.
+# See b/178559811 for more details.
+BOARD_GKI_SIGNING_SIGNATURE_ARGS := --prop foo:bar
 
 BOARD_KERNEL_BINARIES := \
     kernel-4.19-gz \
