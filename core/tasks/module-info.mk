0fbe636deae7665655f49e2391a268c23433b1d8
Jingwen Chen <jingwen@google.com>
2021-08-17 05:33:14 +0000

    Enable bazel query for Make (and Soong) modules.
    Steps:
    
    1) RECORD_ALL_DEPS=true SOONG_COLLECT_JAVA_DEPS=true SOONG_COLLECT_CC_DEPS=true m out/target/product/generic/module-info/BUILD
    
    2) bazel query 'deps(//out/target/product/generic/module-info:bionic-unit-tests)' # => https://gist.github.com/jin/005686b1f748c3c2b38539bb6026dd87
    
    3) bazel query --output=build //out/target/product/generic/module-info:sync_clock_jni
    
    ```
    module_info(
      name = "sync_clock_jni",
      installed =
      ["//out/target/product/generic/module-info:out/target/product/generic/system/lib/sync_clock_jni.so"],
      module_name = "sync_clock_jni",
      module_class = ["SHARED_LIBRARIES"],
      module_path = ["external/walt/android/WALT/app/src/main/jni"],
      dependencies = [],
    )
    ```
    
    Test: Run commands
    Change-Id: I425f1e4d08a1aae6f0e778c415834d6e956fdf1c

--- core/tasks/module-info.mk
+++ core/tasks/module-info.mk
@@ -1,6 +1,7 @@
 # Print a list of the modules that could be built
 
 MODULE_INFO_JSON := $(PRODUCT_OUT)/module-info.json
+MODULE_INFO_BUILD := $(PRODUCT_OUT)/module-info/BUILD
 
 $(MODULE_INFO_JSON):
 	@echo Generating $@
@@ -25,6 +26,20 @@ $(MODULE_INFO_JSON):
 	 ) | sed -e 's/, *\]/]/g' -e 's/, *\}/ }/g' -e '$$s/,$$//' >> $@
 	$(hide) echo '}' >> $@
 
+# RECORD_ALL_DEPS=true SOONG_COLLECT_JAVA_DEPS=true SOONG_COLLECT_CC_DEPS=true m out/target/product/generic/module-info/BUILD
+$(MODULE_INFO_BUILD):
+	@echo Generating $@
+	$(hide) echo -ne 'load("//build/make/core/tasks:module-info.bzl", "module_info")\n\n' > $@
+	$(hide) echo -ne $(foreach m, $(sort $(ALL_MODULES)), \
+		'module_info(\n' \
+			'    name = "$(m)",\n' \
+			'    module_name = "$(ALL_MODULES.$(m).MODULE_NAME)",\n' \
+			'    installed = [$(foreach w,$(sort $(ALL_MODULES.$(m).INSTALLED)),"$(w)", )],\n' \
+			'    module_class = [$(foreach w,$(sort $(ALL_MODULES.$(m).CLASS)),"$(w)", )],\n' \
+			'    module_path = [$(foreach w,$(sort $(ALL_MODULES.$(m).PATH)),"$(w)", )],\n' \
+			'    dependencies = [$(foreach w,$(sort $(ALL_DEPS.$(m).ALL_DEPS)),\n        "$(w)",)    ],\n' \
+			')\n\n' \
+	) | sed -e 's/^\s//g' >> $@
 
 droidcore-unbundled: $(MODULE_INFO_JSON)
 
