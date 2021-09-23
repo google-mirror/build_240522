6231d2ffc2c4cb179fd525a1f3f9bdd347e93af0
Bowgo Tsai <bowgotsai@google.com>
2021-09-23 15:46:33 +0800

    Renames boot-debug-*.img in GSI targets
    Those boot-debug-*.img is used with `repack_bootimg` for a
    vendor_boot-debug.img in VTS setup. It is not for GKI boot.img
    release.
    
      https://source.android.com/compatibility/vts/vts-on-gsi#repacking
    
    Renames boot-debug-*.img to boot-with-debug-ramdisk-*.img to
    avoid confusion with the official GKI boot.img release.
    
    Bug: 200878300
    Test: `lunch gsi_arm64-user` then `make bootimage_debug`
    Change-Id: Ia1f6ba847d5b7409fb7a8534432484d2aa972494
    Merged-In: Ia1f6ba847d5b7409fb7a8534432484d2aa972494
    (cherry picked from commit a2a5db44663a8dff93f0456034e17848155613f8)

--- core/product.mk
+++ core/product.mk
@@ -275,7 +275,14 @@ _product_single_value_vars += \
     PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION \
     PRODUCT_USES_DEFAULT_ART_CONFIG \
 
+<<<<<<< HEAD   (04f125 Fix `m bootimage_test_harness` failure)
 _product_list_vars += PRODUCT_SYSTEM_SERVER_COMPILER_FILTER
+=======
+# The file name for the boot image with a debug ramdisk.
+_product_single_value_vars += PRODUCT_DEBUG_RAMDISK_BOOT_IMAGE_NAME
+
+_product_single_value_vars += PRODUCT_SYSTEM_SERVER_COMPILER_FILTER
+>>>>>>> CHANGE (a2a5db Renames boot-debug-*.img in GSI targets)
 # Per-module sanitizer configs
 _product_list_vars += PRODUCT_SANITIZER_MODULE_CONFIGS
 _product_single_value_vars += PRODUCT_SYSTEM_BASE_FS_PATH
--- target/product/gsi_release.mk
+++ target/product/gsi_release.mk
@@ -54,5 +54,28 @@ PRODUCT_PACKAGES += \
     gsi_skip_mount.cfg \
     init.gsi.rc
 
+<<<<<<< HEAD   (04f125 Fix `m bootimage_test_harness` failure)
 # Support addtional P and Q VNDK packages
 PRODUCT_EXTRA_VNDK_VERSIONS := 28 29
+=======
+# Support additional P, Q and R VNDK packages
+PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30
+
+# Do not build non-GSI partition images.
+PRODUCT_BUILD_CACHE_IMAGE := false
+PRODUCT_BUILD_USERDATA_IMAGE := false
+PRODUCT_BUILD_VENDOR_IMAGE := false
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_BUILD_SUPER_EMPTY_IMAGE := false
+
+# Always build modules from source
+MODULE_BUILD_FROM_SOURCE := true
+
+# Additional settings used in all GSI builds
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.crypto.metadata_init_delete_all_keys.enabled=false \
+
+# Renames boot-debug-*.img to boot-with-debug-ramdisk-*.img to avoid confusion
+# with GKI boot images.
+PRODUCT_DEBUG_RAMDISK_BOOT_IMAGE_NAME := boot-with-debug-ramdisk
+>>>>>>> CHANGE (a2a5db Renames boot-debug-*.img in GSI targets)
