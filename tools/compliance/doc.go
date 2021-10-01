be10f781f7840353f4d194165306cf1d4e004c0b
Bob Badour <bbadour@google.com>
2021-10-01 15:21:58 -0700

    compliance package for license metadata
    package to read, consume, and analyze license metadata and dependency
    graph.
    
    Includes the below command-line tools:
    
    dumpgraph outputs edges of the graph as "target dependency annotations"
    
    dumpresolutions outputs the resulting set of resolutions after the
    bottom-up resolve.
    
    listshare outputs csv of projects to share to meet restricted and
    reciprocal license requirements with one project per line. The first
    field is the path to the project, and subsequent fields identify the
    license resolutions as colon-separated target:annotations tuples.
    
    checkshare outputs error messages to stderr for any targets where
    policy dictates both sharing and not sharing the source-code, and PASS
    or FAIL to stdout. exit status indicates success 0 or conflict found 1
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    Test: m systemlicense
    Test: m listshare; out/soong/host/linux-x86/bin/listshare ...
    Test: m checkshare; out/soong/host/linux-x86/bin/checkshare ...
    Test: m dumpgraph; out/soong/host/linux-x86/dumpgraph ...
    Test: m dumpresolutions; out/soong/host/linux-x86/dumpresolutions ...
    
    where ... is the path to the .meta_lic file for the system image. In my
    case if
    
    $ export PRODUCT=$(realpath $ANDROID_PRODUCT_OUT --relative-to=$PWD)
    
    ... can be expressed as:
    
    ${PRODUCT}/gen/META/lic_intermediates/${PRODUCT}/system.img.meta_lic
    
    Change-Id: I4ff3f98848f7e6d03a35734300d763ef5f245d53

---
+++ tools/compliance/doc.go
@@ -0,0 +1,57 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/*
+Package compliance provides an approved means for reading, consuming, and
+analyzing license metadata graphs.
+
+Assuming the license metadata and dependencies are fully and accurately
+recorded in the build system, any discrepancy between the official policy for
+open source license compliance and this code is a bug in this code.
+
+A few principal types to understand are LicenseGraph, LicenseCondition, and
+ResolutionSet.
+
+LicenseGraph
+------------
+A LicenseGraph is an immutable graph of the targets and dependencies reachable
+from a specific set of root targets. In general, the root targets will be the
+artifacts in a release or distribution. While conceptually immutable, parts of
+the graph may be loaded or evaluated lazily.
+
+LicenseCondition
+----------------
+A LicenseCondition is an immutable tuple pairing a condition name with an
+originating target. e.g. Per current policy, a static library licensed under an
+MIT license would pair a "notice" condition with the static library target, and
+a dynamic license licensed under GPL would pair a "restricted" condition with
+the dynamic library target.
+
+ResolutionSet
+-------------
+A ResolutionSet is an immutable set of targets and the license conditions which
+apply to each of those targets in a given context. Remember: Each license
+condition pairs a condition name with an originating target so each resolution
+in a ResolutionSet has a target it applies to and a target it originates from,
+which may be the same target.
+
+A ResolutionSet may at times require careful consideration. Consider a reciprocal
+library. The reciprocal condition originating at the library will apply to every
+target that links the library, and the ResolutionSet will attach conditions to
+each of those targets. However, no matter how many targets the condition applies
+to, only the source-code for the originating library needs to be shared to
+satisfy the policy.
+
+*/
+package compliance
