2924d74d656de98d590f37137c275a224d94ef21
Bob Badour <bbadour@google.com>
2021-10-01 15:21:58 -0700

    compliance package for license metadata
    package to read, consume, and analyze license metadata and dependency
    graph.
    
    Includes the below command-line tools:
    
    dumpgraph outputs edges of the graph as "target dependency annotations"
    
    dumpresolutions outputs the resulting set of resolutions after the
    bottom-up resolve.
    
    listshare outputs csv of projects to share to meet restricted and
    reciprocal license requirements with one project per line. The first
    field is the path to the project, and subsequent fields identify the
    license resolutions as colon-separated target:annotations tuples.
    
    checkshare outputs error messages to stderr for any targets where
    policy dictates both sharing and not sharing the source-code, and PASS
    or FAIL to stdout. exit status indicates success 0 or conflict found 1
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    Test: m systemlicense
    Test: m listshare; out/soong/host/linux-x86/bin/listshare ...
    Test: m checkshare; out/soong/host/linux-x86/bin/checkshare ...
    Test: m dumpgraph; out/soong/host/linux-x86/dumpgraph ...
    Test: m dumpresolutions; out/soong/host/linux-x86/dumpresolutions ...
    
    where ... is the path to the .meta_lic file for the system image. In my
    case if
    
    $ export PRODUCT=$(realpath $ANDROID_PRODUCT_OUT --relative-to=$PWD)
    
    ... can be expressed as:
    
    ${PRODUCT}/gen/META/lic_intermediates/${PRODUCT}/system.img.meta_lic
    
    Change-Id: I4ff3f98848f7e6d03a35734300d763ef5f245d53

---
+++ tools/compliance/cmd/dumpgraph.go
@@ -0,0 +1,106 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"compliance"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage: %s file.meta_lic {file.meta_lic...}
+
+Outputs space-separated Target Dependency Annotations tuples for each edge in
+the license graph.
+
+Options:
+`, filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
+}
+
+type byTargetDep []compliance.TargetEdge
+
+func (l byTargetDep) Len() int           { return len(l) }
+func (l byTargetDep) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l byTargetDep) Less(i, j int) bool {
+	if l[i].Target().Name() == l[j].Target().Name() {
+		if l[i].Dependency().Name() == l[j].Dependency().Name() {
+			a1 := l[i].Annotations().ListAnnotations()
+			a2 := l[j].Annotations().ListAnnotations()
+			sort.Strings(a1)
+			sort.Strings(a2)
+			for k := 0; k < len(a1) && k < len(a2); k++ {
+				if a1[k] < a2[k] {
+					return true
+				}
+				if a1[k] > a2[k] {
+					return false
+				}
+			}
+			return len(a1) < len(a2)
+		}
+		return l[i].Dependency().Name() < l[j].Dependency().Name()
+	}
+	return l[i].Target().Name() < l[j].Target().Name()
+}
+
+func main() {
+	flag.Parse()
+
+	// FIXME: remove debug output
+	fmt.Fprintf(os.Stderr, "Environment:\n")
+	for _, ev := range os.Environ() {
+		fmt.Fprintf(os.Stderr, "%s\n", ev)
+	}
+	fmt.Fprintf(os.Stderr, "\n")
+
+	// Must specify at least one root target.
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	files := append([]string{}, flag.Args()...)
+	licenseGraph, err := compliance.ReadLicenseGraph(files)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to read license metadata file(s) %q: %v\n", files, err)
+		os.Exit(1)
+	}
+	if licenseGraph == nil {
+		fmt.Fprintf(os.Stderr, "No licenses\n")
+		os.Exit(1)
+	}
+
+	edges := licenseGraph.Edges()
+	sort.Sort(byTargetDep(edges))
+	for _, e := range edges {
+		fmt.Fprintf(os.Stdout, "%s %s", e.Target().Name(), e.Dependency().Name())
+		annotations := e.Annotations().ListAnnotations()
+		if len(annotations) > 0 {
+			sort.Strings(annotations)
+			fmt.Fprintf(os.Stdout, " %s", strings.Join(annotations, ":"))
+		}
+		fmt.Fprintln(os.Stdout)
+	}
+
+	os.Exit(0)
+}
