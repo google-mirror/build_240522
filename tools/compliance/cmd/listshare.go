82c601a875a7c4719adfb014c6d28ab36d1212eb
Bob Badour <bbadour@google.com>
2021-10-01 15:21:58 -0700

    compliance package for license metadata
    package to read, consume, and analyze license metadata and dependency
    graph.
    
    Includes the below command-line tools:
    
    dumpgraph outputs edges of the graph as "target dependency annotations"
    
    dumpresolutions outputs the resulting set of resolutions after the
    bottom-up resolve.
    
    listshare outputs csv of projects to share to meet restricted and
    reciprocal license requirements with one project per line. The first
    field is the path to the project, and subsequent fields identify the
    license resolutions as colon-separated target:annotations tuples.
    
    checkshare outputs error messages to stderr for any targets where
    policy dictates both sharing and not sharing the source-code, and PASS
    or FAIL to stdout. exit status indicates success 0 or conflict found 1
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    Test: m systemlicense
    Test: m listshare; out/soong/host/linux-x86/bin/listshare ...
    Test: m checkshare; out/soong/host/linux-x86/bin/checkshare ...
    Test: m dumpgraph; out/soong/host/linux-x86/dumpgraph ...
    Test: m dumpresolutions; out/soong/host/linux-x86/dumpresolutions ...
    
    where ... is the path to the .meta_lic file for the system image. In my
    case if
    
    $ export PRODUCT=$(realpath $ANDROID_PRODUCT_OUT --relative-to=$PWD)
    
    ... can be expressed as:
    
    ${PRODUCT}/gen/META/lic_intermediates/${PRODUCT}/system.img.meta_lic
    
    Change-Id: I4ff3f98848f7e6d03a35734300d763ef5f245d53

---
+++ tools/compliance/cmd/listshare.go
@@ -0,0 +1,120 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"compliance"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage: %s file.meta_lic {file.meta_lic...}
+
+Outputs a csv file with 1 project per line in the first field followed
+by target:condition pairs describing why the project must be shared.
+
+Each target is the path to a generated license metadata file for a
+Soong module or Make target, and the license condition is either
+restricted (e.g. GPL) or reciprocal (e.g. MPL).
+
+%s always output all of the projects. When the -full_walk flag
+is given, %s also includes a complete list of triggering targets
+and conditions. This takes longer.
+
+Options:
+`, filepath.Base(os.Args[0]), filepath.Base(os.Args[0]), filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
+}
+
+type byTarget []compliance.LicenseCondition
+
+func (l byTarget) Len() int           { return len(l) }
+func (l byTarget) Swap(i, j int)      { l[i], l[j] = l[j], l[i] }
+func (l byTarget) Less(i, j int) bool {
+	return l[i].Origin().Name() < l[j].Origin().Name() || (
+		l[i].Origin().Name() == l[j].Origin().Name() && l[i].Name() < l[j].Name())
+}
+
+func main() {
+	flag.Parse()
+
+	// FIXME: remove debug output
+	fmt.Fprintf(os.Stderr, "Environment:\n")
+	for _, ev := range os.Environ() {
+		fmt.Fprintf(os.Stderr, "%s\n", ev)
+	}
+	fmt.Fprintf(os.Stderr, "\n")
+
+	// Must specify at least one root target.
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	files := append([]string{}, flag.Args()...)
+	licenseGraph, err := compliance.ReadLicenseGraph(files)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to read license metadata file(s) %q: %v\n", files, err)
+		os.Exit(1)
+	}
+	if licenseGraph == nil {
+		fmt.Fprintf(os.Stderr, "No licenses\n")
+		os.Exit(1)
+	}
+
+	// shareSource is the set of all source-sharing resolutions.
+	shareSource := compliance.ResolveSourceSharing(licenseGraph)
+
+	// group the resolutions by project
+	presolution := make(map[string]compliance.LicenseConditionSet)
+	for _, t := range shareSource.AppliesTo() {
+		conditions, _ := shareSource.Conditions(t)
+		for _, c := range conditions.Conditions() {
+			for _, p := range t.Projects() {
+				if _, ok := presolution[p]; !ok {
+					presolution[p] = compliance.NewLicenseConditionSet(c)
+					continue
+				}
+				presolution[p].Add(c)
+			}
+		}
+	}
+
+	// sort the projects to make the results more usable
+	projects := make([]string, 0, len(presolution))
+	for p := range presolution {
+		projects = append(projects, p)
+	}
+	sort.Strings(projects)
+
+	// output the sorted projects and the license conditions sharing the project resolves.
+	for _, p := range projects {
+		fmt.Fprintf(os.Stdout, "%s", p)
+		cs := presolution[p]
+		conditions := cs.Conditions()
+		sort.Sort(byTarget(conditions))
+		for _, c := range conditions {
+			fmt.Fprintf(os.Stdout, ",%s:%s", c.Origin().Name(), c.Name())
+		}
+		fmt.Fprintf(os.Stdout, "\n")
+	}
+	os.Exit(0)
+}
