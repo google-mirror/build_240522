d0a892fe6b1264e427ee83f25f8d82d7f4496d31
Bob Badour <bbadour@google.com>
2021-10-01 15:21:58 -0700

    compliance package for license metadata
    package to read, consume, and analyze license metadata and dependency
    graph.
    
    Includes the below command-line tools:
    
    dumpgraph outputs edges of the graph as "target dependency annotations"
    
    dumpresolutions outputs the resulting set of resolutions after the
    bottom-up resolve.
    
    listshare outputs csv of projects to share to meet restricted and
    reciprocal license requirements with one project per line. The first
    field is the path to the project, and subsequent fields identify the
    license resolutions as colon-separated target:annotations tuples.
    
    checkshare outputs error messages to stderr for any targets where
    policy dictates both sharing and not sharing the source-code, and PASS
    or FAIL to stdout. exit status indicates success 0 or conflict found 1
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m all
    Test: m systemlicense
    Test: m listshare; out/soong/host/linux-x86/bin/listshare ...
    Test: m checkshare; out/soong/host/linux-x86/bin/checkshare ...
    Test: m dumpgraph; out/soong/host/linux-x86/dumpgraph ...
    Test: m dumpresolutions; out/soong/host/linux-x86/dumpresolutions ...
    
    where ... is the path to the .meta_lic file for the system image. In my
    case if
    
    $ export PRODUCT=$(realpath $ANDROID_PRODUCT_OUT --relative-to=$PWD)
    
    ... can be expressed as:
    
    ${PRODUCT}/gen/META/lic_intermediates/${PRODUCT}/system.img.meta_lic
    
    Change-Id: I4ff3f98848f7e6d03a35734300d763ef5f245d53

---
+++ tools/compliance/cmd/checkshare_test.go
@@ -0,0 +1,176 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	tests := []struct {
+		condition string
+		name      string
+		roots     []string
+	}{
+		{
+			condition: "firstparty",
+			name:      "apex",
+			roots:     []string{"highest.apex.meta_lic"},
+		},
+		{
+			condition: "firstparty",
+			name:      "container",
+			roots:     []string{"container.zip.meta_lic"},
+		},
+		{
+			condition: "firstparty",
+			name:      "application",
+			roots:     []string{"application.meta_lic"},
+		},
+		{
+			condition: "firstparty",
+			name:      "binary",
+			roots:     []string{"bin/bin1.meta_lic"},
+		},
+		{
+			condition: "firstparty",
+			name:      "library",
+			roots:     []string{"lib/libd.so.meta_lic"},
+		},
+		{
+			condition: "notice",
+			name:      "apex",
+			roots:     []string{"highest.apex.meta_lic"},
+		},
+		{
+			condition: "notice",
+			name:      "container",
+			roots:     []string{"container.zip.meta_lic"},
+		},
+		{
+			condition: "notice",
+			name:      "application",
+			roots:     []string{"application.meta_lic"},
+		},
+		{
+			condition: "notice",
+			name:      "binary",
+			roots:     []string{"bin/bin1.meta_lic"},
+		},
+		{
+			condition: "notice",
+			name:      "library",
+			roots:     []string{"lib/libd.so.meta_lic"},
+		},
+		{
+			condition: "reciprocal",
+			name:      "apex",
+			roots:     []string{"highest.apex.meta_lic"},
+		},
+		{
+			condition: "reciprocal",
+			name:      "container",
+			roots:     []string{"container.zip.meta_lic"},
+		},
+		{
+			condition: "reciprocal",
+			name:      "application",
+			roots:     []string{"application.meta_lic"},
+		},
+		{
+			condition: "reciprocal",
+			name:      "binary",
+			roots:     []string{"bin/bin1.meta_lic"},
+		},
+		{
+			condition: "reciprocal",
+			name:      "library",
+			roots:     []string{"lib/libd.so.meta_lic"},
+		},
+		{
+			condition: "restricted",
+			name:      "apex",
+			roots:     []string{"highest.apex.meta_lic"},
+		},
+		{
+			condition: "restricted",
+			name:      "container",
+			roots:     []string{"container.zip.meta_lic"},
+		},
+		{
+			condition: "restricted",
+			name:      "application",
+			roots:     []string{"application.meta_lic"},
+		},
+		{
+			condition: "restricted",
+			name:      "binary",
+			roots:     []string{"bin/bin1.meta_lic"},
+		},
+		{
+			condition: "restricted",
+			name:      "library",
+			roots:     []string{"lib/libd.so.meta_lic"},
+		},
+		{
+			condition: "proprietary",
+			name:      "apex",
+			roots:     []string{"highest.apex.meta_lic"},
+		},
+		{
+			condition: "proprietary",
+			name:      "container",
+			roots:     []string{"container.zip.meta_lic"},
+		},
+		{
+			condition: "proprietary",
+			name:      "application",
+			roots:     []string{"application.meta_lic"},
+		},
+		{
+			condition: "proprietary",
+			name:      "binary",
+			roots:     []string{"bin/bin1.meta_lic"},
+		},
+		{
+			condition: "proprietary",
+			name:      "library",
+			roots:     []string{"lib/libd.so.meta_lic"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.condition+" "+tt.name, func(t *testing.T) {
+			stdout := &bytes.Buffer{}
+			stderr := &bytes.Buffer{}
+
+			rootFiles := make([]string, 0, len(tt.roots))
+			for _, r := range tt.roots {
+				rootFiles = append(rootFiles, "testdata/"+tt.condition+"/"+r)
+			}
+			err := checkShare(stdout, stderr, rootFiles...)
+			fmt.Fprintf(os.Stderr, "\nstderr:\n%s\n", stderr)
+			fmt.Fprintf(os.Stderr, "\nstdout:\n%s\n", stdout)
+			if err == nil {
+				fmt.Fprintf(os.Stderr, "%s %s: passed\n", tt.condition, tt.name)
+				return
+			}
+			fmt.Fprintf(os.Stderr, "%s %s: failed, err=%q\n", tt.condition, tt.name, err.Error())
+		})
+	}
+	t.Errorf("completed %d tests", len(tests))
+}
---
+++ tools/compliance/cmd/listshare.go
@@ -0,0 +1,133 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"compliance"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
+	"sort"
+)
+
+func init() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, `Usage: %s file.meta_lic {file.meta_lic...}
+
+Outputs a csv file with 1 project per line in the first field followed
+by target:condition pairs describing why the project must be shared.
+
+Each target is the path to a generated license metadata file for a
+Soong module or Make target, and the license condition is either
+restricted (e.g. GPL) or reciprocal (e.g. MPL).
+
+Options:
+`, filepath.Base(os.Args[0]))
+		flag.PrintDefaults()
+	}
+}
+
+// byTarget orders license conditions by originating target then condition name.
+type byTarget []compliance.LicenseCondition
+
+func (l byTarget) Len() int      { return len(l) }
+func (l byTarget) Swap(i, j int) { l[i], l[j] = l[j], l[i] }
+func (l byTarget) Less(i, j int) bool {
+	return l[i].Origin().Name() < l[j].Origin().Name() || (l[i].Origin().Name() == l[j].Origin().Name() && l[i].Name() < l[j].Name())
+}
+
+func main() {
+	flag.Parse()
+
+	// FIXME: remove debug output
+	fmt.Fprintf(os.Stderr, "Environment:\n")
+	for _, ev := range os.Environ() {
+		fmt.Fprintf(os.Stderr, "%s\n", ev)
+	}
+	fmt.Fprintf(os.Stderr, "\n")
+
+	// Must specify at least one root target.
+	if flag.NArg() == 0 {
+		flag.Usage()
+		os.Exit(2)
+	}
+
+	err := listShare(os.Stdout, os.Stderr, flag.Args()...)
+	if err != nil {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func listShare(stdout, stderr io.Writer, files ...string) error {
+	// Must be at least one root file.
+	if len(files) < 1 {
+		return fmt.Errorf("no root license metadata files requested")
+	}
+
+	// Read the license graph from the license metadata files (*.meta_lic).
+	licenseGraph, err := compliance.ReadLicenseGraph(files)
+	if err != nil {
+		fmt.Fprintf(stderr, "Unable to read license metadata file(s) %q: %v\n", files, err)
+		return err
+	}
+	if licenseGraph == nil {
+		fmt.Fprintf(stderr, "No licenses\n")
+		return fmt.Errorf("no licenses")
+	}
+
+	// shareSource contains all source-sharing resolutions.
+	shareSource := compliance.ResolveSourceSharing(licenseGraph)
+
+	// Group the resolutions by project.
+	presolution := make(map[string]compliance.LicenseConditionSet)
+	for _, target := range shareSource.AppliesTo() {
+		for _, c := range shareSource.Conditions(target).AsList() {
+			for _, p := range target.Projects() {
+				if _, ok := presolution[p]; !ok {
+					presolution[p] = compliance.NewLicenseConditionSet(c)
+					continue
+				}
+				presolution[p].Add(c)
+			}
+		}
+	}
+
+	// Sort the projects for repeatability/stability.
+	projects := make([]string, 0, len(presolution))
+	for p := range presolution {
+		projects = append(projects, p)
+	}
+	sort.Strings(projects)
+
+	// Output the sorted projects and the source-sharing license conditions that each project resolves.
+	for _, p := range projects {
+		fmt.Fprintf(stdout, "%s", p)
+
+		// Sort the conditions for repeatability/stability.
+		conditions := presolution[p].AsList()
+		sort.Sort(byTarget(conditions))
+
+		// Output the sorted origin:condition pairs.
+		for _, c := range conditions {
+			fmt.Fprintf(stdout, ",%s:%s", c.Origin().Name(), c.Name())
+		}
+		fmt.Fprintf(stdout, "\n")
+	}
+
+	return nil
+}
