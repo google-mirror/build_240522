56d313b3a6540d9e7e7905fbc513e81c0fbdf6d2
Alan Stokes <alanstokes@google.com>
2021-10-19 09:52:30 +0100

    CompOS support.
    Add isolated_compilation.mk to allow a product to opt in to using
    isolated compilation in a protected VM.
    
    Bug: 199147668
    Test: builds, with and without CompOS enabled
    Change-Id: Ieb8efc1dbe052be4442a1314bcd23760af3cda6f

---
+++ target/product/isolated_compilation.mk
@@ -0,0 +1,30 @@
+#
+# Copyright 2021 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Inherit this in order to support Isolated Compilation in a protected VM.
+
+PRODUCT_PACKAGES += com.android.compos
+
+PRODUCT_APEX_SYSTEM_SERVER_JARS += \
+    com.android.compos:service-compos
+
+PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
+    system/apex/com.android.compos.apex \
+    system/apex/com.android.virt.apex \
+    system/framework/oat/%@service-compos.jar@classes.odex \
+    system/framework/oat/%@service-compos.jar@classes.vdex \
+    system/lib64/%.dylib.so \
+    system/lib64/libgfxstream_backend.so \
