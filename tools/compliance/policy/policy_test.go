0e0eb105f4037bd2969ee4fbd10a29b9f108f0e1
Bob Badour <bbadour@google.com>
2021-10-25 16:51:48 -0700

    compliance package policy and resolves
    package to read, consume, and analyze license metadata and dependency
    graph.
    
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Change-Id: Ic08406fa2250a08ad26f2167d934f841c95d9148

---
+++ tools/compliance/policy/policy_test.go
@@ -0,0 +1,442 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package compliance
+
+import (
+	"bytes"
+	"fmt"
+	"sort"
+	"strings"
+	"testing"
+)
+
+func TestPolicy_edgeConditions(t *testing.T) {
+	tests := []struct {
+		name                     string
+		edge                     annotated
+		treatAsAggregate         bool
+		otherCondition           string
+		expectedDepConditions    []string
+		expectedTargetConditions []string
+	}{
+		{
+			name:                     "firstparty",
+			edge:                     annotated{"apacheBin.meta_lic", "apacheLib.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"apacheLib.meta_lic:notice"},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "notice",
+			edge:                     annotated{"mitBin.meta_lic", "mitLib.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"mitLib.meta_lic:notice"},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "fponlgpl",
+			edge:                     annotated{"apacheBin.meta_lic", "lgplLib.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"lgplLib.meta_lic:restricted"},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "fponlgpldynamic",
+			edge:                     annotated{"apacheBin.meta_lic", "lgplLib.meta_lic", []string{"dynamic"}},
+			expectedDepConditions:    []string{},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "fpongpl",
+			edge:                     annotated{"apacheBin.meta_lic", "gplLib.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"gplLib.meta_lic:restricted"},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "fpongpldynamic",
+			edge:                     annotated{"apacheBin.meta_lic", "gplLib.meta_lic", []string{"dynamic"}},
+			expectedDepConditions:    []string{"gplLib.meta_lic:restricted"},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "independentmodule",
+			edge:                     annotated{"apacheBin.meta_lic", "gplWithClasspathException.meta_lic", []string{"dynamic"}},
+			expectedDepConditions:    []string{},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "independentmodulestatic",
+			edge:                     annotated{"apacheBin.meta_lic", "gplWithClasspathException.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"gplWithClasspathException.meta_lic:restricted"},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "dependentmodule",
+			edge:                     annotated{"dependentModule.meta_lic", "gplWithClasspathException.meta_lic", []string{"dynamic"}},
+			expectedDepConditions:    []string{"gplWithClasspathException.meta_lic:restricted"},
+			expectedTargetConditions: []string{},
+		},
+
+		{
+			name:                     "lgplonfp",
+			edge:                     annotated{"lgplBin.meta_lic", "apacheLib.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"apacheLib.meta_lic:notice"},
+			expectedTargetConditions: []string{"lgplBin.meta_lic:restricted"},
+		},
+		{
+			name:                     "lgplonfpdynamic",
+			edge:                     annotated{"lgplBin.meta_lic", "apacheLib.meta_lic", []string{"dynamic"}},
+			expectedDepConditions:    []string{},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "gplonfp",
+			edge:                     annotated{"gplBin.meta_lic", "apacheLib.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"apacheLib.meta_lic:notice"},
+			expectedTargetConditions: []string{"gplBin.meta_lic:restricted"},
+		},
+		{
+			name:                     "gplcontainer",
+			edge:                     annotated{"gplContainer.meta_lic", "apacheLib.meta_lic", []string{"static"}},
+			treatAsAggregate:         true,
+			expectedDepConditions:    []string{"apacheLib.meta_lic:notice"},
+			expectedTargetConditions: []string{"gplContainer.meta_lic:restricted"},
+		},
+		{
+			name:                     "gploncontainer",
+			edge:                     annotated{"apacheContainer.meta_lic", "apacheLib.meta_lic", []string{"static"}},
+			treatAsAggregate:         true,
+			otherCondition:           "gplLib.meta_lic:restricted",
+			expectedDepConditions:    []string{"apacheLib.meta_lic:notice", "gplLib.meta_lic:restricted"},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "gplonbin",
+			edge:                     annotated{"apacheBin.meta_lic", "apacheLib.meta_lic", []string{"static"}},
+			treatAsAggregate:         false,
+			otherCondition:           "gplLib.meta_lic:restricted",
+			expectedDepConditions:    []string{"apacheLib.meta_lic:notice", "gplLib.meta_lic:restricted"},
+			expectedTargetConditions: []string{"gplLib.meta_lic:restricted"},
+		},
+		{
+			name:                     "gplonfpdynamic",
+			edge:                     annotated{"gplBin.meta_lic", "apacheLib.meta_lic", []string{"dynamic"}},
+			expectedDepConditions:    []string{},
+			expectedTargetConditions: []string{"gplBin.meta_lic:restricted"},
+		},
+		{
+			name:                     "independentmodulereverse",
+			edge:                     annotated{"gplWithClasspathException.meta_lic", "apacheBin.meta_lic", []string{"dynamic"}},
+			expectedDepConditions:    []string{},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "independentmodulereversestatic",
+			edge:                     annotated{"gplWithClasspathException.meta_lic", "apacheBin.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"apacheBin.meta_lic:notice"},
+			expectedTargetConditions: []string{"gplWithClasspathException.meta_lic:restricted"},
+		},
+		{
+			name:                     "dependentmodulereverse",
+			edge:                     annotated{"gplWithClasspathException.meta_lic", "dependentModule.meta_lic", []string{"dynamic"}},
+			expectedDepConditions:    []string{},
+			expectedTargetConditions: []string{"gplWithClasspathException.meta_lic:restricted"},
+		},
+		{
+			name:                     "ponr",
+			edge:                     annotated{"proprietary.meta_lic", "gplLib.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"gplLib.meta_lic:restricted"},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "ronp",
+			edge:                     annotated{"gplBin.meta_lic", "proprietary.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"proprietary.meta_lic:proprietary"},
+			expectedTargetConditions: []string{"gplBin.meta_lic:restricted"},
+		},
+		{
+			name:                     "noticeonb_e_o",
+			edge:                     annotated{"mitBin.meta_lic", "by_exception.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"by_exception.meta_lic:by_exception_only"},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "b_e_oonnotice",
+			edge:                     annotated{"by_exception.meta_lic", "mitLib.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"mitLib.meta_lic:notice"},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "noticeonrecip",
+			edge:                     annotated{"mitBin.meta_lic", "mplLib.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"mplLib.meta_lic:reciprocal"},
+			expectedTargetConditions: []string{},
+		},
+		{
+			name:                     "reciponnotice",
+			edge:                     annotated{"mplBin.meta_lic", "mitLib.meta_lic", []string{"static"}},
+			expectedDepConditions:    []string{"mitLib.meta_lic:notice"},
+			expectedTargetConditions: []string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			fs := make(testFS)
+			stderr := &bytes.Buffer{}
+			target := meta[tt.edge.target] + fmt.Sprintf("deps: {\n  file: \"%s\"\n", tt.edge.dep)
+			for _, ann := range tt.edge.annotations {
+				target += fmt.Sprintf("  annotations: \"%s\"\n", ann)
+			}
+			fs[tt.edge.target] = []byte(target + "}\n")
+			fs[tt.edge.dep] = []byte(meta[tt.edge.dep])
+			licenseGraph, err := ReadLicenseGraph(&fs, stderr, []string{tt.edge.target})
+			if err != nil {
+				t.Errorf("unexpected error reading graph: %w", err)
+				return
+			}
+			lg := licenseGraph.(*licenseGraphImp)
+			// simulate a condition inherited from another edge/dependency.
+			otherTarget := ""
+			otherCondition := ""
+			if len(tt.otherCondition) > 0 {
+				fields := strings.Split(tt.otherCondition, ":")
+				otherTarget = fields[0]
+				otherCondition = fields[1]
+				// other target must exist in graph
+				lg.targets[otherTarget] = &targetNode{name: otherTarget}
+				lg.targets[otherTarget].LicenseConditions = append(lg.targets[otherTarget].LicenseConditions, otherCondition)
+			}
+			if tt.expectedDepConditions != nil {
+				depConditions := lg.TargetNode(tt.edge.dep).LicenseConditions().(*licenseConditionSetImp)
+				if otherTarget != "" {
+					depConditions.addAll(otherTarget, otherCondition)
+				}
+				cs := depConditionsApplicableToTarget(lg.Edges()[0].(targetEdgeImp), depConditions, tt.treatAsAggregate)
+				actual := make([]string, 0, cs.Count())
+				for _, lc := range cs.AsList() {
+					actual = append(actual, lc.(licenseConditionImp).asString(":"))
+				}
+				sort.Strings(actual)
+				sort.Strings(tt.expectedDepConditions)
+				if len(actual) != len(tt.expectedDepConditions) {
+					t.Errorf("unexpected number of dependency conditions: got %v with %d conditions, want %v with %d conditions",
+						actual, len(actual), tt.expectedDepConditions, len(tt.expectedDepConditions))
+				} else {
+					for i := 0; i < len(actual); i++ {
+						if actual[i] != tt.expectedDepConditions[i] {
+							t.Errorf("unexpected dependency condition at element %d: got %q, want %q",
+								i, actual[i], tt.expectedDepConditions[i])
+						}
+					}
+				}
+			}
+			if tt.expectedTargetConditions != nil {
+				targetConditions := lg.TargetNode(tt.edge.target).LicenseConditions().(*licenseConditionSetImp)
+				if otherTarget != "" {
+					targetConditions.addAll(otherTarget, otherCondition)
+				}
+				cs := targetConditionsApplicableToDep(lg.Edges()[0].(targetEdgeImp), targetConditions, tt.treatAsAggregate)
+				actual := make([]string, 0, cs.Count())
+				for _, lc := range cs.AsList() {
+					actual = append(actual, lc.(licenseConditionImp).asString(":"))
+				}
+				sort.Strings(actual)
+				sort.Strings(tt.expectedTargetConditions)
+				if len(actual) != len(tt.expectedTargetConditions) {
+					t.Errorf("unexpected number of target conditions: got %v with %d conditions, want %v with %d conditions",
+						actual, len(actual), tt.expectedTargetConditions, len(tt.expectedTargetConditions))
+				} else {
+					for i := 0; i < len(actual); i++ {
+						if actual[i] != tt.expectedTargetConditions[i] {
+							t.Errorf("unexpected target condition at element %d: got %q, want %q",
+								i, actual[i], tt.expectedTargetConditions[i])
+						}
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestPolicy_selfConditions(t *testing.T) {
+	tests := []struct {
+		name                   string
+		conditionType          string
+		conditions             []string
+		expectedSelfConditions []string
+	}{
+		{
+			name:                   "noticenotice",
+			conditionType:          "notice",
+			conditions:             []string{"notice"},
+			expectedSelfConditions: []string{"notice"},
+		},
+		{
+			name:                   "noticerestricted",
+			conditionType:          "restricted",
+			conditions:             []string{"notice"},
+			expectedSelfConditions: []string{},
+		},
+		{
+			name:                   "noticeproprietary",
+			conditionType:          "proprietary",
+			conditions:             []string{"notice"},
+			expectedSelfConditions: []string{},
+		},
+		{
+			name:                   "restrictednotice",
+			conditionType:          "notice",
+			conditions:             []string{"restricted"},
+			expectedSelfConditions: []string{"restricted"},
+		},
+		{
+			name:                   "restrictedrestricted",
+			conditionType:          "restricted",
+			conditions:             []string{"restricted"},
+			expectedSelfConditions: []string{"restricted"},
+		},
+		{
+			name:                   "restrictedproprietary",
+			conditionType:          "proprietary",
+			conditions:             []string{"restricted"},
+			expectedSelfConditions: []string{},
+		},
+		{
+			name:                   "proprietarynotice",
+			conditionType:          "notice",
+			conditions:             []string{"proprietary"},
+			expectedSelfConditions: []string{"proprietary"},
+		},
+		{
+			name:                   "proprietaryrestricted",
+			conditionType:          "restricted",
+			conditions:             []string{"proprietary"},
+			expectedSelfConditions: []string{},
+		},
+		{
+			name:                   "proprietaryproprietary",
+			conditionType:          "proprietary",
+			conditions:             []string{"proprietary"},
+			expectedSelfConditions: []string{"proprietary"},
+		},
+		{
+			name:                   "manynotnotice",
+			conditionType:          "notice",
+			conditions:             []string{"permissive", "reciprocal", "by_exception_only"},
+			expectedSelfConditions: []string{"permissive", "reciprocal", "by_exception_only"},
+		},
+		{
+			name:                   "manynotrestricted",
+			conditionType:          "restricted",
+			conditions:             []string{"permissive", "reciprocal", "by_exception_only"},
+			expectedSelfConditions: []string{},
+		},
+		{
+			name:                   "manynotproprietary",
+			conditionType:          "proprietary",
+			conditions:             []string{"permissive", "reciprocal", "by_exception_only"},
+			expectedSelfConditions: []string{},
+		},
+		{
+			name:                   "manynotice",
+			conditionType:          "notice",
+			conditions:             []string{"notice", "reciprocal", "by_exception_only"},
+			expectedSelfConditions: []string{"notice", "reciprocal", "by_exception_only"},
+		},
+		{
+			name:                   "manyrestricted",
+			conditionType:          "restricted",
+			conditions:             []string{"permissive", "restricted", "by_exception_only"},
+			expectedSelfConditions: []string{"restricted"},
+		},
+		{
+			name:                   "manyproprietary",
+			conditionType:          "proprietary",
+			conditions:             []string{"permissive", "proprietary", "by_exception_only"},
+			expectedSelfConditions: []string{"proprietary"},
+		},
+		{
+			name:                   "manyall",
+			conditionType:          "all",
+			conditions:             []string{"notice", "reciprocal", "by_exception_only"},
+			expectedSelfConditions: []string{"notice", "reciprocal", "by_exception_only"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			lg := newLicenseGraphImp()
+			_ = newTestNode(lg, "target")
+			lg.targets["target"].LicenseConditions = append(lg.targets["target"].LicenseConditions, tt.conditions...)
+			selfConditions := selfConditionsApplicableForConditionName(
+				tt.conditionType, lg.TargetNode("target").LicenseConditions(), false)
+			actual := make([]string, 0, len(selfConditions.conditions))
+			for name, origins := range selfConditions.conditions {
+				actual = append(actual, name)
+				for _, payload := range origins {
+					if false != payload {
+						t.Errorf("unexpected payload: got %v, want false", payload)
+					}
+				}
+			}
+			sort.Strings(actual)
+			sort.Strings(tt.expectedSelfConditions)
+			if len(actual) != len(tt.expectedSelfConditions) {
+				t.Errorf("unexpected number of self conditions: got %v with %d conditions, want %v with %d conditions",
+					actual, len(actual), tt.expectedSelfConditions, len(tt.expectedSelfConditions))
+			} else {
+				for i := 0; i < len(actual); i++ {
+					if actual[i] != tt.expectedSelfConditions[i] {
+						t.Errorf("unexpected self condition at element %d: got %q, want %q",
+							i, actual[i], tt.expectedSelfConditions[i])
+					}
+				}
+			}
+			selfConditions = selfConditionsApplicableForConditionName(
+				tt.conditionType, lg.TargetNode("target").LicenseConditions(), true)
+			actual = actual[:0]
+			for name, origins := range selfConditions.conditions {
+				actual = append(actual, name)
+				for _, payload := range origins {
+					if true != payload {
+						t.Errorf("unexpected payload: got %v, want true", payload)
+					}
+				}
+			}
+			sort.Strings(actual)
+			sort.Strings(tt.expectedSelfConditions)
+			if len(actual) != len(tt.expectedSelfConditions) {
+				t.Errorf("unexpected number of self conditions: got %v with %d conditions, want %v with %d conditions",
+					actual, len(actual), tt.expectedSelfConditions, len(tt.expectedSelfConditions))
+			} else {
+				for i := 0; i < len(actual); i++ {
+					if actual[i] != tt.expectedSelfConditions[i] {
+						t.Errorf("unexpected self condition at element %d: got %q, want %q",
+							i, actual[i], tt.expectedSelfConditions[i])
+					}
+				}
+			}
+		})
+	}
+}
+
+func TestPolicy_unknownSelfConditionType(t *testing.T) {
+	defer func() {
+		if r := recover(); r == nil {
+			t.Errorf("expected error: want panic, got no panic")
+		}
+	}()
+
+	lg := newLicenseGraphImp()
+	_ = newTestNode(lg, "target")
+	lg.targets["target"].LicenseConditions = append(lg.targets["target"].LicenseConditions, "notice", "restricted")
+	_ = selfConditionsApplicableForConditionName("unknown", lg.TargetNode("target").LicenseConditions(), false)
+}
