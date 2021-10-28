bb434929aef341e783bc50de9a145f3f8f7e4dc4
Jiakai Zhang <jiakaiz@google.com>
2021-10-28 14:37:20 +0000

    Add a variable to specify standalone system server jars.
    This is the list of jars that system_server loads dynamically using
    separate classloaders. We will rely on this variable to decide which
    jars to preopt in the build system and on the device for system_server.
    
    The list is supposed to be in sync with the code in SystemServer.java.
    There will be a follow-up CL to add a comment in SystemServer.java to
    remind developers to keep them in sync.
    
    Bug: 203198541
    Test: m nothing
    Change-Id: I305a73218ef2d2c61ac3795d21026b2afe7007fd

--- target/product/default_art_config.mk
+++ target/product/default_art_config.mk
@@ -62,12 +62,25 @@ PRODUCT_APEX_BOOT_JARS := \
     com.android.tethering:framework-tethering \
     com.android.wifi:framework-wifi
 
-# APEX system server jars. Keep the list sorted by module names and then library names.
+# List of system_server classpath jars delivered via apex.
+# Keep the list sorted by module names and then library names.
 PRODUCT_APEX_SYSTEM_SERVER_JARS := \
     com.android.appsearch:service-appsearch \
     com.android.art:service-art \
     com.android.media:service-media-s \
     com.android.permission:service-permission \
+
+# List of jars on the platform that system_server loads dynamically using separate classloaders. The
+# order does not matter.
+PRODUCT_STANDALONE_SYSTEM_SERVER_JARS := \
+
+# List of jars delivered via apex that system_server loads dynamically using separate classloaders.
+# The order does not matter.
+PRODUCT_APEX_STANDALONE_SYSTEM_SERVER_JARS := \
+    com.android.os.statsd:service-statsd \
+    com.android.scheduling:service-scheduling \
+    com.android.tethering:service-connectivity \
+    com.android.wifi:service-wifi \
 
 # Minimal configuration for running dex2oat (default argument values).
 # PRODUCT_USES_DEFAULT_ART_CONFIG must be true to enable boot image compilation.
