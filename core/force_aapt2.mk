bfe0ecd92729fa1467d540a4119db8dfb5219f8d
Spandan Das <spandandas@google.com>
2021-11-02 03:54:05 +0000

    Move manifest generation logic into static_java_library.mk
    Previously a manifest file would be generated for static_java_library.mk
    and package_internal.mk. Doing this for pacakge_internal.mk would cause
    cryptic errors like b/188612215#2
    
    With this CL, a manifest file would only be generated for java_library
    with need_compile_res == true. The advantages are
    1. No redundant generation for java_library with need_compile_res == false
    (i.e. modules that do not require aapt2)
    2. Force devs to provide manifest for android_app, and not silently
    generating one for them
    
    Bug: 188652897
    Test: TH
    Test: m nothing in tv-dev, car-dev
    
    Change-Id: I69a23e373f1b4ac1569ae5b1d36067bfc0dadc05

--- core/force_aapt2.mk
+++ core/force_aapt2.mk
@@ -46,8 +46,11 @@ endif
 
 ifeq (,$(strip $(LOCAL_MANIFEST_FILE)$(LOCAL_FULL_MANIFEST_FILE)))
   ifeq (,$(wildcard $(LOCAL_PATH)/AndroidManifest.xml))
-    # work around missing manifests by creating a default one
-    LOCAL_FULL_MANIFEST_FILE := $(call local-intermediates-dir,COMMON)/DefaultManifest.xml
-    $(call create-default-manifest-file,$(LOCAL_FULL_MANIFEST_FILE),$(call module-min-sdk-version))
+    # aapt2 does not support apps with missing manifests
+    ifeq (APPS,$(LOCAL_MODULE_CLASS))
+      $(call pretty-error,App missing manifest file which is required by aapt2. \
+Provide a manifest file by either setting LOCAL_MANIFEST_FILE in Android.mk or \
+manifest in Android.bp or via a AndroidManifest.xml in this directory)
+    endif
   endif
 endif
