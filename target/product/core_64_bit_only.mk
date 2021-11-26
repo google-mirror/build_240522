ca2d27a8d1c108b8bd208dd4bd11072c0bf18656
Kevin Brodsky <kevin.brodsky@arm.com>
2021-11-26 15:40:08 +0000

    Use dex2oat64 on 64-bit-only builds
    dexopt defaults to dex2oat32 unless instructed otherwise. Since
    dex2oat32 doesn't exist on a 64-bit-only build, make sure dex2oat64
    is selected instead.
    
    Test: boot aosp_cf_x86_64_only_phone and check that installd doesn't
      try to use dex2oat32
    Change-Id: Ia67e746894684a52a4e5b765bfde0f6dd0efbf6e
    Merged-In: Ia67e746894684a52a4e5b765bfde0f6dd0efbf6e
    (cherry picked from commit 1917e4e047641c8166b77e898568429a773ec364)
    Bug: 296185206

--- target/product/core_64_bit_only.mk
+++ target/product/core_64_bit_only.mk
@@ -24,7 +24,14 @@ PRODUCT_COPY_FILES += system/core/rootdir/init.zygote64.rc:system/etc/init/hw/in
 
 # Set the zygote property to select the 64-bit script.
 # This line must be parsed before the one in core_minimal.mk
+<<<<<<< HEAD   (f66221 Merge "Disable OMX for 64b_only devices." into android11-gsi)
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += ro.zygote=zygote64
+=======
+PRODUCT_VENDOR_PROPERTIES += ro.zygote=zygote64
+# A 64-bit-only platform does not have dex2oat32, so make sure dex2oat64 is
+# used for dexopt.
+PRODUCT_VENDOR_PROPERTIES += dalvik.vm.dex2oat64.enabled=true
+>>>>>>> CHANGE (1917e4 Use dex2oat64 on 64-bit-only builds)
 
 TARGET_SUPPORTS_32_BIT_APPS := false
 TARGET_SUPPORTS_64_BIT_APPS := true
