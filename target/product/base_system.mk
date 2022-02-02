5a3b8768f869e60ccb8807514d212a9796423ab9
Max Bires <jbires@google.com>
2022-02-01 21:46:04 -0800

    Adding ro.remote_provisioning.rkp_only
    This property should default to false, optionally being set to true in
    the individual <<device>>.mk files.
    
    Test: property is default false
    Change-Id: I271f30e74d6d7061136ceaa9e09b001b72e989ec

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -363,6 +363,7 @@ PRODUCT_VENDOR_PROPERTIES += ro.zygote?=zygote32
 
 PRODUCT_SYSTEM_PROPERTIES += debug.atrace.tags.enableflags=0
 PRODUCT_SYSTEM_PROPERTIES += persist.traced.enable=1
+PRODUCT_SYSTEM_PROPERTIES += ro.remote_provisioning.rkp_only=0
 
 # Packages included only for eng or userdebug builds, previously debug tagged
 PRODUCT_PACKAGES_DEBUG := \
