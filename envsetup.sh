1e3bd5f2226ed22968258e976b5d61ef03aee9d6
Will Burr <wgb@google.com>
2022-02-07 12:12:01 +0000

    envsetup.sh: Update text in lunch menu for clarity
    Updates the lunch menu text to make it clearer that the choices in lunch
    menu are not fully comprehensive. As the list comes from common lunch
    choices the text now refers to 'common combinations' and informs that
    you can 'specify your own'. This should help developers who are unsure
    why they cannot find their desired lunch target in the list.
    
    Change-Id: Ib9d55ee46b7037c30a8f4b43fdd552ea66e887e8
    Test: source build/envsetup.sh && lunch

--- envsetup.sh
+++ envsetup.sh
@@ -625,7 +625,7 @@ function print_lunch_menu()
         return
     fi
 
-    echo "Lunch menu... pick a combo:"
+    echo "Lunch menu .. Here are the common combinations:"
 
     local i=1
     local choice
@@ -651,7 +651,8 @@ function lunch()
         answer=$1
     else
         print_lunch_menu
-        echo -n "Which would you like? [aosp_arm-eng] "
+        echo "Which would you like? [aosp_arm-eng]"
+        echo -n "Pick from common choices above (e.g. 13) or specify your own (e.g. aosp_barbet-eng): "
         read answer
     fi
 
