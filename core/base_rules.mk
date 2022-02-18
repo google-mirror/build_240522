252de3007a8be2552f314ee5c605800b214343da
yangbill <yangbill@google.com>
2022-02-18 15:27:53 +0800

    Add support_varients to moudle-info.
    This CL export the information for the module if it is defined to run oh
    host or device.
    
    Bug:
    Test: m out/target/product/vsoc_x86_64/module-info.json
    Change-Id: I10ae6ea21791d0d100686dcabecbce543a9adc3f

--- core/base_rules.mk
+++ core/base_rules.mk
@@ -876,6 +876,16 @@ endif  # LOCAL_UNINSTALLABLE_MODULE
 endif  # LOCAL_COMPATIBILITY_SUITE
 
 ###########################################################
+## Define Support varient.
+###########################################################
+my_support_varient :=
+ifdef LOCAL_IS_HOST_MODULE
+  my_support_varient := HOST
+else
+  my_support_varient := DEVICE
+endif
+
+###########################################################
 ## Add test module to ALL_DISABLED_PRESUBMIT_TESTS if LOCAL_PRESUBMIT_DISABLED is set to true.
 ###########################################################
 ifeq ($(LOCAL_PRESUBMIT_DISABLED),true)
@@ -1000,6 +1010,10 @@ ifdef LOCAL_TEST_DATA_BINS
   ALL_MODULES.$(my_register_name).TEST_DATA_BINS := \
     $(ALL_MODULES.$(my_register_name).TEST_DATA_BINS) $(LOCAL_TEST_DATA_BINS)
 endif
+
+ALL_MODULES.$(my_register_name).SUPPORT_VARIENTS := \
+  $(ALL_MODULES.$(my_register_name).SUPPORT_VARIENTS) \
+  $(filter-out $(ALL_MODULES.$(my_register_name).SUPPORT_VARIENTS),$(my_support_varient))
 
 ##########################################################################
 ## When compiling against the VNDK, add the .vendor or .product suffix to
