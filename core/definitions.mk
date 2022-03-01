17d6a4473c0c871682dcf74cc726443d76931d97
Bob Badour <bbadour@google.com>
2022-02-28 20:05:44 -0800

    Phony target to report all library names.
    Bug: 68860345
    Bug: 151177513
    Bug: 151953481
    
    Test: m nothing reportallnoticelibrarynames
    
    Change-Id: I0f791e2d92b8135d6af3a6c93f0db348fc69f5a1

--- core/definitions.mk
+++ core/definitions.mk
@@ -874,6 +874,32 @@ reportmissinglicenses:
 
 endef
 
+
+###########################################################
+# Returns the unique list of built license metadata files.
+###########################################################
+define all-license-metadata
+$(sort \
+  $(foreach t,$(ALL_NON_MODULES),$(if $(filter 0p,$(ALL_TARGETS.$(t).META_LIC)),, $(ALL_TARGETS.$(t).META_LIC))) \
+  $(foreach m,$(ALL_MODULES), $(ALL_MODULES.$(m).META_LIC)) \
+)
+endef
+
+###########################################################
+# Declares the rule to report all library names used in any notice files.
+###########################################################
+define report-all-notice-library-names-rule
+.PHONY: reportallnoticelibrarynames
+reportallnoticelibrarynames: PRIVATE_LIST_FILE := $(call license-metadata-dir)/filelist
+reportallnoticelibrarynames: | $(COMPLIANCENOTICE_SHIPPEDLIBS)
+reportallnoticelibrarynames: $(call all-license-metadata)
+	@echo Reporting notice library names for $$(words $(call all-license-metadata)) license metadata files under $$(dir $$(PRIVATE_LIST_FILE))
+	$(hide) rm -f $$(PRIVATE_LIST_FILE)
+	$(hide) mkdir -p $$(dir $$(PRIVATE_LIST_FILE))
+	$(hide) find $$(dir $$(PRIVATE_LIST_FILE)) -name '*meta_lic' -type f >$$(PRIVATE_LIST_FILE)
+	$(COMPLIANCENOTICE_SHIPPEDLIBS) @$$(PRIVATE_LIST_FILE)
+endef
+
 ###########################################################
 ## Declares a license metadata build rule for ALL_MODULES
 ###########################################################
@@ -888,7 +914,8 @@ $(strip \
   ) \
   $(foreach t,$(sort $(ALL_NON_MODULES)),$(eval $(call non-module-license-metadata-rule,$(t)))) \
   $(foreach m,$(sort $(ALL_MODULES)),$(eval $(call license-metadata-rule,$(m)))) \
-  $(eval $(call report-missing-licenses-rule)))
+  $(eval $(call report-missing-licenses-rule)) \
+  $(eval $(call report-all-notice-library-names-rule)))
 endef
 
 ###########################################################
