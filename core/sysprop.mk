78b248088417a9ba6781869f10a1ca05b697d4ce
Yi-Yo Chiang <yochiang@google.com>
2022-03-15 17:40:23 +0800

    Rename init_boot properties to ro.init_boot.*
    Right now they're named ro.bootimage.* which is really confusing.
    Change it so that if generic ramdisk is in boot.img, then name the
    properties as ro.bootimage.*, otherwise if generic ramdisk is in
    init_boot.img, then name the prop as ro.init_boot.*.
    
    Bug: 211823573
    Test: m dist
    Change-Id: Icff3c7be04b9a3c6490fbf49ba4f05565b029890

--- core/sysprop.mk
+++ core/sysprop.mk
@@ -503,6 +503,7 @@ $(eval $(call build-properties,\
 
 RAMDISK_BUILD_PROP_REL_PATH := system/etc/ramdisk/build.prop
 INSTALLED_RAMDISK_BUILD_PROP_TARGET := $(TARGET_RAMDISK_OUT)/$(RAMDISK_BUILD_PROP_REL_PATH)
+RAMDISK_PARTITION_NAME := $(if $(BUILDING_INIT_BOOT_IMAGE),init_boot,bootimage)
 $(eval $(call build-properties,\
     bootimage,\
     $(INSTALLED_RAMDISK_BUILD_PROP_TARGET),\
