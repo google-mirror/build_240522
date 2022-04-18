1aed58e3594e178f3df7a65018da73d28074dab4
Bob Badour <bbadour@google.com>
2022-04-18 17:28:11 +0000

    Revert "Revert "Create lists of projects to share per dist target""
    This reverts commit 59e49510215480438e57c4b3076e9fd4a310bf23.
    
    Put all `cd` shell commands inside parantheses.
    
    Reason for revert: Attempt to roll forward again.
    
    Test: m droid dist reportmissinglicenses
    
    Change-Id: Ibf5a9869d03726e279fb23bf68ca66547b972138

--- core/java_common.mk
+++ core/java_common.mk
@@ -123,7 +123,7 @@ ifneq ($(LOCAL_JAVA_RESOURCE_DIRS),)
     $(foreach dir,$(LOCAL_JAVA_RESOURCE_DIRS), \
 	$(subst $(space),:,$(strip \
 		$(LOCAL_PATH)/$(dir): \
-	    $(patsubst ./%,%,$(sort $(shell cd $(LOCAL_PATH)/$(dir) && \
+	    $(patsubst ./%,%,$(sort $(shell (cd $(LOCAL_PATH)/$(dir) && \
 		find . \
 		    -type d -a -name ".svn" -prune -o \
 		    -type f \
@@ -134,7 +134,7 @@ ifneq ($(LOCAL_JAVA_RESOURCE_DIRS),)
 			-a \! -name ".DS_Store" \
 			-a \! -name "*~" \
 			-print \
-		    ))) \
+		    )))) \
 	)) \
     )
   java_resource_file_groups := $(filter-out %:,$(java_resource_file_groups))
