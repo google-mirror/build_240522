cad522dc6f4490b698afdd0b795d6e10c8444dd1
Jiakai Zhang <jiakaiz@google.com>
2022-05-16 15:09:55 +0100

    Add a link to the g3doc about debugging `dex_preopt_check` error.
    Bug: 232542678
    Test: m nothing
    Change-Id: I2e9868a6a470e41e8c3743c045e0cf4cf7367efa

--- core/tasks/dex_preopt_check.mk
+++ core/tasks/dex_preopt_check.mk
@@ -12,7 +12,8 @@ ifneq (true,$(DISABLE_DEXPREOPT_CHECK))
   ifneq (,$(filter services,$(PRODUCT_PACKAGES)))
     $(call maybe-print-list-and-error,\
       $(filter-out $(ALL_DEFAULT_INSTALLED_MODULES),$(DEXPREOPT_SYSTEMSERVER_ARTIFACTS)),\
-      Missing compilation artifacts. Dexpreopting is not working for some system server jars \
+      Missing compilation artifacts. Dexpreopting is not working for some system server jars. See \
+      go/android-art/art/aot/dexpreopt.md#debugging-dex-preopt-check-error \
     )
   endif
 endif
