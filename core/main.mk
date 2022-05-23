b11c8b4ce619e6d0cbdce265a3ae92604ed26e17
Yi-Yo Chiang <yochiang@google.com>
2022-05-23 16:30:59 +0800

    Add a .PHONY target that builds lsdump_paths.txt
    This file is used to expose a MAKE variable to
    development/vndk/tools/header-checker/utils/create_reference_dumps.py,
    so it doesn't have any "real" dependency. This means the
    lsdump_paths.txt file won't be regenerated reliably if there is any
    local edit.
    
    $ m findlsdump
    $ echo "" > $ANDROID_PRODUCT_OUT/lsdump_paths.txt
    $ development/vndk/tools/header-checker/utils/create_reference_dumps.py
    
    Would fail because the contents of lsdump_paths.txt is corrupted.
    
    This change adds a phony target that unconditionally builds
    lsdump_paths.txt, so the above sequence of commands wouldn't error.
    
    Bug: 233562033
    Test: Manual test the above commands
    Change-Id: I1f000c9a988d4685383f2707774af13de4c23759

--- core/main.mk
+++ core/main.mk
@@ -1966,12 +1966,14 @@ LSDUMP_PATHS_FILE := $(PRODUCT_OUT)/lsdump_paths.txt
 
 .PHONY: findlsdumps
 # LSDUMP_PATHS is a list of tag:path.
-findlsdumps: $(LSDUMP_PATHS_FILE) $(foreach p,$(LSDUMP_PATHS),$(call word-colon,2,$(p)))
+findlsdumps: lsdump_paths
+findlsdumps: $(foreach p,$(LSDUMP_PATHS),$(call word-colon,2,$(p)))
 
-$(LSDUMP_PATHS_FILE): PRIVATE_LSDUMP_PATHS := $(LSDUMP_PATHS)
-$(LSDUMP_PATHS_FILE):
-	@echo "Generate $@"
-	@rm -rf $@ && echo -e "$(subst :,:$(space),$(subst $(space),\n,$(PRIVATE_LSDUMP_PATHS)))" > $@
+.PHONY: lsdump_paths
+lsdump_paths:
+	@echo "Generate $(LSDUMP_PATHS_FILE)"
+	@rm -rf "$(LSDUMP_PATHS_FILE)"
+	@echo -e "$(subst :,:$(space),$(subst $(space),\n,$(LSDUMP_PATHS)))" > "$(LSDUMP_PATHS_FILE)"
 
 .PHONY: check-elf-files
 check-elf-files:
