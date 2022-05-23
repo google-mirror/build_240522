4842a339c2d3ae990fefe65baf859a62b051bd45
Spandan Das <spandandas@google.com>
2022-05-23 22:28:08 +0000

    Generate api_levels.json for ndkstubgen
    ndkstubgen.py has an implicit dep on a file called api_levels.json.
    Soong creates this file in api_levels.go using a hardcoded map and
    PLATFORM_VERSION_ALL_CODENAMES (which comes from product config). Port
    this solution to orchestrator prototype for now
    
    Test: python -m cc.test_stub_generator
    Test: build/build/orchestrator/core/orchestrator.py ;
    master/prebuilts/build-tools/linux-x86/bin/ninja -f out/multitree.ninja
    out/intermediates/api_surfaces/api_a/1/libhello1/arm/stub.c
    
    Change-Id: Ibb3270fdcd4d9009513d4b856f6e338eff151ae7

--- orchestrator/core/cc/stub_generator.py
+++ orchestrator/core/cc/stub_generator.py
@@ -44,7 +44,7 @@ class StubGenerator:
 
     def __init__(self):
         self._stubgen_rule = None
-        self._version_map_file = None # TODO: Create ninja rule to build api_levels.json
+        self._version_map_file = None
 
     def add_stub_gen_rule(self, ninja: Ninja):
         """This adds a ninja rule to run ndkstubgen
@@ -61,11 +61,22 @@ class StubGenerator:
         ninja.add_rule(rule)
         self._stubgen_rule = rule
 
+    def add_version_map_file(self, ninja: Ninja, api_surfaces_work_dir: str):
+        """This adds a ninja build statement to generate api_levels.json
+        This file is an implicit dependency of ndkstubgen"""
+        if self._version_map_file:
+            return
+        version_map_file = os.path.join(api_surfaces_work_dir, "api_levels.json")
+        ninja.add_write_file(version_map_file, json.dumps(_get_api_levels()))
+        self._version_map_file = version_map_file
+
     def add_stub_gen_action(self, ninja: Ninja, stub_input: GenCcStubsInput,
                             work_dir: str) -> GenCcStubsOutput:
         """This adds a ninja build action to generate stubs using `genCcStubsRule`"""
         if self._stubgen_rule is None:
             raise Exception("Cannot create ninja build statement to generate C stubs without creating a ninja rule")
+        if self._version_map_file is None:
+            raise Exception("Cannot create ninja build statement to generate C stubs without a ninja statement to build api_levels.json")
 
         outputs = GenCcStubsOutput(stub_src=os.path.join(work_dir, stub_input.arch, "stub.c"),
                                    version_script=os.path.join(work_dir, stub_input.arch, "stub.map"),
@@ -86,3 +97,36 @@ class StubGenerator:
         # Add the build action to the ninja file
         ninja.add_build_action(gen_stub_build_action)
         return outputs
+
+# ndkstubgen uses a map for converting Android version codes to a numeric code
+# e.g. "R" --> 30
+# The map contains active_codenames as well, which get mapped to a preview level
+# (9000+)
+# TODO: Soong does it in android/api_levels.go, figure out the solution for
+# multi-tree
+PLATFORM_VERSION_ALL_CODENAMES = ["Tiramisu", "UpsideDownCake"]
+PREVIEW_API_LEVEL_BASE = 9000
+def _get_api_levels() -> dict:
+    api_levels = {
+            "G": 9,
+            "I": 14,
+            "J": 16,
+            "J-MR1": 17,
+            "J-MR2": 18,
+            "K":     19,
+            "L":     21,
+            "L-MR1": 22,
+            "M":     23,
+            "N":     24,
+            "N-MR1": 25,
+            "O":     26,
+            "O-MR1": 27,
+            "P":     28,
+            "Q":     29,
+            "R":     30,
+            "S":     31,
+            "S-V2":  32,
+    }
+    for index, codename in enumerate(PLATFORM_VERSION_ALL_CODENAMES):
+        api_levels[codename] = PREVIEW_API_LEVEL_BASE + index
+    return api_levels
