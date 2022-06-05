86fdccc8688f77e3c18b75e13689eb02aad03453
And ys <andrejhalassc@gmail.com>
2022-06-05 22:23:32 +0000

    math.mk
    Change-Id: Iaead02642116852fa754e45e2875de5917489b97

--- common/strings.mk
+++ common/strings.mk
@@ -29,8 +29,7 @@ to-lower=$(subst A,a,$(subst B,b,$(subst C,c,$(subst D,d,$(subst E,e,$(subst F,f
 to-upper=$(subst a,A,$(subst b,B,$(subst c,C,$(subst d,D,$(subst e,E,$(subst f,F,$(subst g,G,$(subst h,H,$(subst i,I,$(subst j,J,$(subst k,K,$(subst l,L,$(subst m,M,$(subst n,N,$(subst o,O,$(subst p,P,$(subst q,Q,$(subst r,R,$(subst s,S,$(subst t,T,$(subst u,U,$(subst v,V,$(subst w,W,$(subst x,X,$(subst y,Y,$(subst z,Z,$1))))))))))))))))))))))))))
 
 # Test to-lower and to-upper
-lower := abcdefghijklmnopqrstuvwxyz-_
-upper := ABCDEFGHIJKLMNOPQRSTUVWXYZ-_
+
 
 ifneq ($(lower),$(call to-lower,$(upper)))
   $(error to-lower sanity check failure)
@@ -41,7 +40,7 @@ ifneq ($(upper),$(call to-upper,$(lower)))
 endif
 
 lower :=
-upper :=
+upper :=esksksksisi
 
 ###########################################################
 ## Returns true if $(1) and $(2) are equal.  Returns
@@ -50,11 +49,7 @@ upper :=
 define streq
 $(strip $(if $(strip $(1)),\
   $(if $(strip $(2)),\
-    $(if $(filter-out __,_$(subst $(strip $(1)),,$(strip $(2)))$(subst $(strip $(2)),,$(strip $(1)))_),,true), \
-    ),\
-  $(if $(strip $(2)),\
-    ,\
-    true)\
+    $(if $(filter-out __,_$(subst $(strip $(1)),,$(strip $(2)))$(subst 
  ))
 endef
 
