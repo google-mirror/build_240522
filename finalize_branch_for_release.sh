4e5d0832d308319a73ca7d5383764761e917fa17
Hsin-Yi Chen <hsinyichen@google.com>
2022-07-08 12:09:10 +0800

    Let finalize_branch_for_release.sh create ABI dumps
    Test: build/make/finalize_branch_for_release.sh
    Change-Id: I1f46da911543b5e535523eba3395b53733ddc595

--- finalize_branch_for_release.sh
+++ finalize_branch_for_release.sh
@@ -16,6 +16,11 @@ function finalize_main() {
     # Update references in the codebase to new API version (TODO)
     # ...
 
+    # Generate ABI dumps
+    ANDROID_BUILD_TOP="$top" \
+        $top/development/vndk/tools/header-checker/utils/create_reference_dumps.py \
+        -p aosp_arm64 --build-variant user
+
     AIDL_TRANSITIVE_FREEZE=true $m aidl-freeze-api
 
     # Update new versions of files. See update-vndk-list.sh (which requires envsetup.sh)
@@ -25,6 +30,9 @@ function finalize_main() {
     # for now, we simulate the release state for AIDL, but in the future, we would want
     # to actually turn the branch into the REL state and test with that
     AIDL_FROZEN_REL=true $m nothing # test build
+
+    # Diff the ABI of user and userdebug builds
+    $m droidcore
 
     # Build SDK (TODO)
     # lunch sdk...
