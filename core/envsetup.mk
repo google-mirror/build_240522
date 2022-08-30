b413314b7746f71f7a525c0e0305c4514ef9c04c
Ryan Prichard <rprichard@google.com>
2022-08-30 16:25:19 -0700

    Disable 32-bit host glibc by clearing HOST_2ND_ARCH
    Bug: http://b/244367576
    Test: build everything, treehugger
    Change-Id: Ie29f3f5c30aa913be547fc1fb14966b48d2fe460

--- core/envsetup.mk
+++ core/envsetup.mk
@@ -180,7 +180,7 @@ endif
 # HOST_ARCH
 ifneq (,$(findstring x86_64,$(UNAME)))
   HOST_ARCH := x86_64
-  HOST_2ND_ARCH := x86
+  HOST_2ND_ARCH :=
   HOST_IS_64_BIT := true
 else
 ifneq (,$(findstring i686,$(UNAME))$(findstring x86,$(UNAME)))
