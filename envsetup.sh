7baf408c29c32ca1bb2e1dda6a85eafd2f7abcda
Chris Parsons <cparsons@google.com>
2022-08-30 11:55:38 -0400

    Remove reference to USE_BAZEL_ANALYSIS
    Test: N/A
    Bug: 244309479
    Change-Id: I40567bf37c292bc4c5b9186581ff510691673a8e

--- envsetup.sh
+++ envsetup.sh
@@ -1839,7 +1839,7 @@ function _trigger_build()
 function b()
 (
     # Generate BUILD, bzl files into the synthetic Bazel workspace (out/soong/workspace).
-    _trigger_build "all-modules" bp2build USE_BAZEL_ANALYSIS= || return 1
+    _trigger_build "all-modules" bp2build || return 1
     # Then, run Bazel using the synthetic workspace as the --package_path.
     if [[ -z "$@" ]]; then
         # If there are no args, show help.
