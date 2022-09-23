fca57648615241fde2f9cb1e195a3c27fc9eb541
Justin Yun <justinyun@google.com>
2022-09-23 10:47:21 +0900

    Split system and vendor unmounted notice deps
    Some unmounted notices need to be included in system only but not in
    vendor image. Split the unmounted notice deps to avoid duplication.
    
    Bug: 243753534
    Test: check notice.xml
    Change-Id: I23071542ac7823caf9697463928dcd1781f9414a

--- core/os_licensing.mk
+++ core/os_licensing.mk
@@ -30,7 +30,7 @@ vendorlicense: $(call corresponding-license-metadata, $(VENDOR_NOTICE_DEPS)) rep
 
 ifneq (,$(VENDOR_NOTICE_DEPS))
 
-VENDOR_NOTICE_DEPS += $(UNMOUNTED_NOTICE_DEPS)
+VENDOR_NOTICE_DEPS += $(UNMOUNTED_NOTICE_VENDOR_DEPS)
 
 $(eval $(call text-notice-rule,$(target_vendor_notice_file_txt),"Vendor image", \
          "Notices for files contained in all filesystem images except system/system_ext/product/odm/vendor_dlkm/odm_dlkm in this directory:", \
