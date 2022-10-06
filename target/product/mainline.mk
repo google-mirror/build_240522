0ea187c10f0e51f28bae1aa3c70b995c583aade5
Android Culprit Assistant <boq-android-culprit-assistant@system.gserviceaccount.com>
2022-10-06 05:20:18 +0000

    Revert "Add more product defaults to mainline.mk"
    Revert "Move PhotoTable to /product"
    
    Revert "Remove duplicated product configuration"
    
    This is an Android Culprit Assistant revert, created as part of a culprit search session (go/aca-get/be470d75-d853-4553-9594-5618ff40d3ae). It will not be committed, and can be ignored. If you need to commit this revert, please don't use this - create a new revert of the original topic.
    
    Change-Id: I658bc669ff9ce575ac289950eb6d04e5b3ee2a6b

--- target/product/mainline.mk
+++ target/product/mainline.mk
@@ -14,24 +14,10 @@
 # limitations under the License.
 #
 
-# This makefile is intended to serve as a base for completely AOSP based
-# mainline devices, It contain the mainline system partition and sensible
-# defaults for the product and vendor partition.
+# Makefile including the mainline system image, and the relevant AOSP portions
+# for the other partitions.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/mainline_system.mk)
-
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_product.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_product.mk)
-
-$(call inherit-product, frameworks/base/data/sounds/AllAudio.mk)
-
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=Ring_Synth_04.ogg \
-    ro.com.android.dataroaming=true \
-
-PRODUCT_PACKAGES += \
-    PhotoTable \
-    WallpaperPicker \
-
-PRODUCT_COPY_FILES += device/sample/etc/apns-full-conf.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/apns-conf.xml
