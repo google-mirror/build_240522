598ca72e916ac9c13ffa070debd3e09f8c9c5fe6
Fabien Sanglard <sanglardf@google.com>
2022-10-10 22:19:47 +0000

    Zipalign: Don't align directory entries
    Directories are entries with uncompressed size zero and ending with
    character '/' or '\' are allowed in apks since b/204425803. These
    entries should not be considered for alignment since they are not
    mmap by the framework.
    
    Test: align_test.cpp
    Bug: 250872480
    Change-Id: I964aad118a82839f9ed230acc4c2c76f51888c67

--- tools/zipalign/tests/src/align_test.cpp
+++ tools/zipalign/tests/src/align_test.cpp
@@ -12,6 +12,46 @@
 using namespace android;
 using namespace base;
 
+// This load the whole file to memory so be careful!
+static bool sameContent(const std::string& path1, const std::string& path2) {
+    FILE* f1 = fopen(path1.c_str(), "r");
+    if (f1 == nullptr) {
+        printf("Unable to open %s\n", path1.c_str());
+        return false;
+    }
+
+    FILE* f2 = fopen(path2.c_str(), "r");
+    if (f2 == nullptr) {
+        printf("Unable to open %s\n", path2.c_str());
+        return false;
+    }
+
+    fseek(f1, 0, SEEK_END); // seek to end of file
+    int f1Size = ftell(f1);
+
+    fseek(f2, 0, SEEK_END); // seek to end of file
+    int f2Size = ftell(f2);
+
+    if (f1Size != f2Size) {
+        printf("File '%s' and '%s' are not the same\n", path1.c_str(), path2.c_str());
+        return false;
+    }
+
+    char* f1Content = (char*)malloc(f1Size);
+    fread(f1Content, f1Size, 1, f1);
+    fclose(f1);
+
+    char* f2Content = (char*)malloc(f2Size);
+    fread(f2Content, f1Size, 1, f2);
+    fclose(f2);
+
+    bool diffCount = strcmp(f1Content, f2Content);
+
+    delete f1Content;
+    delete f2Content;
+    return diffCount == 0;
+}
+
 static std::string GetTestPath(const std::string& filename) {
   static std::string test_data_dir = android::base::GetExecutableDirectory() + "/tests/data/";
   return test_data_dir + filename;
@@ -83,6 +123,24 @@ TEST(Align, DifferenteOrders) {
 
   int processed = process(src.c_str(), dst.c_str(), 4, true, false, 4096);
   ASSERT_EQ(0, processed);
+
+  int verified = verify(dst.c_str(), 4, false, true);
+  ASSERT_EQ(0, verified);
+}
+
+TEST(Align, DirectoryEntryDoNotRequireAlignment) {
+  const std::string src = GetTestPath("archiveWithOneDirectoryEntry.zip");
+  int verified = verify(src.c_str(), 4, false, true);
+  ASSERT_EQ(0, verified);
+}
+
+TEST(Align, DirectoryEntry) {
+  const std::string src = GetTestPath("archiveWithOneDirectoryEntry.zip");
+  const std::string dst = GetTempPath("archiveWithOneDirectoryEntry_out.zip");
+
+  int processed = process(src.c_str(), dst.c_str(), 4, true, false, 4096);
+  ASSERT_EQ(0, processed);
+  ASSERT_EQ(true, sameContent(src, dst));
 
   int verified = verify(dst.c_str(), 4, false, true);
   ASSERT_EQ(0, verified);
