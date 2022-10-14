8b0fdf1b5a9851b8515008247a34bb28d873daa9
Devin Moore <devinmoore@google.com>
2022-10-14 15:20:20 +0000

    Don't install omx service by default on new devices
    On devices launching after Android U, we no longer install the HIDL OMX
    service. It is deprecated.
    
    Test: Build and run Cuttlefish with SHIPING_API_LEVEL 33 and 34
    Bug: 218588089
    Change-Id: I9a6dbffd381bdad5428e9c365bd50445a9b14a67

--- target/product/base_vendor.mk
+++ target/product/base_vendor.mk
@@ -75,7 +75,8 @@ PRODUCT_PACKAGES += \
 
 # OMX not supported for 64bit_only builds
 ifneq ($(TARGET_SUPPORTS_OMX_SERVICE),false)
-    PRODUCT_PACKAGES += \
+	# Only supported when SHIPPING_API_LEVEL is less than or equal to 33
+    PRODUCT_PACKAGES_SHIPPING_API_LEVEL_33 += \
         android.hardware.media.omx@1.0-service \
 
 endif
