b284b8817d43bb571ebeb35a458f4482f87f29b8
Sasha Smundak <asmundak@google.com>
2022-10-19 23:43:41 -0700

    Redirect bazel_notice_gen to the prebuild one.
    Fixes: 240630051
    Test: treehugger
    Change-Id: Ie38d1992ade153d361c94e2c7fc7e5e109ae3f26

--- tools/compliance/cmd/bazel_notice_gen/bazel_notice_gen_test.go
+++ tools/compliance/cmd/bazel_notice_gen/bazel_notice_gen_test.go
@@ -25,24 +25,42 @@ func Test_doit(t *testing.T) {
       }
     ],
     "copyright_notice": "Copyright (C) The Android Open Source Project",
-    "package_name": null,
+    "package_name": "Discombobulator",
     "package_url": null,
     "package_version": null,
-    "license_text": "build/soong/licenses/LICENSE"
+    "license_text": "../testdata/notice/NOTICE_LICENSE"
+ },
+ {
+    "rule": "@//external/scudo:external_scudo_license",
+    "license_kinds": [
+      {
+        "target": "@//build/soong/licenses:SPDX-license-identifier-Apache-2.0",
+        "name": "SPDX-license-identifier-Apache-2.0",
+        "conditions": ["notice"]
+      }
+    ],
+    "copyright_notice": "",
+    "package_name": "Scudo Standalone",
+    "package_url": null,
+    "package_version": null,
+    "license_text": "external/scudo/LICENSE.TXT"
   }
 ]
 `,
 			want: `<!DOCTYPE html>
 <html>
   <head>
-    <style type="text/css"
+    <style type="text/css">
       body { padding: 2px; margin: 0; }
-      ul { list-style-type: none; margin: 0; padding: 0; }
-      li { padding-left: 1em; }
-      .file-list { margin-left: 1em; }
-    </style>
-  </head>
+      .license { background-color: seashell; margin: 1em;}
+      pre { padding: 1em; }</style></head>
   <body>
+    The following software has been included in this product and contains the license and notice as shown below.<p>
+    <strong>Discombobulator</strong><br>Copyright Notice: Copyright (C) The Android Open Source Project<br><a href=#0e6553ab7221430a352fb7706ebc2aad>License</a><hr>
+    <strong>Scudo Standalone</strong><hr>
+    <div id="0e6553ab7221430a352fb7706ebc2aad" class="license"><pre>%%%Notice License%%%
+
+    </pre></div>  
   </body>
 </html>
 `,
