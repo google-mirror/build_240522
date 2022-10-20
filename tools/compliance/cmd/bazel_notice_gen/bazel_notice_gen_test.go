3365537d0e40e28cd8d34d8c111f242257dde379
Sasha Smundak <asmundak@google.com>
2022-10-19 19:54:30 -0700

    Implement license notice generator for Bazel.
    Bug: 190817312
    Test: treehugger
    Change-Id: I036c3eaa24f27c22ae9d7604a2fd99613ad89b31

---
+++ tools/compliance/cmd/bazel_notice_gen/bazel_notice_gen_test.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"bytes"
+	"testing"
+)
+
+func Test_doit(t *testing.T) {
+	tests := []struct {
+		name string
+		in   string
+		want string
+	}{
+		{
+			name: "One",
+			in: `
+[
+  {
+    "rule": "@//build/soong/licenses:Android-Apache-2.0",
+    "license_kinds": [
+      {
+        "target": "@//build/soong/licenses:SPDX-license-identifier-Apache-2.0",
+        "name": "SPDX-license-identifier-Apache-2.0",
+        "conditions": ["notice"]
+      }
+    ],
+    "copyright_notice": "Copyright (C) The Android Open Source Project",
+    "package_name": "Discombobulator",
+    "package_url": null,
+    "package_version": null,
+    "license_text": "../testdata/notice/NOTICE_LICENSE",
+	"licensees": [
+        "@//bionic/libc:libc_bionic_ndk",
+		"@//system/logging/liblog:liblog"
+    ]
+ },
+ {
+    "rule": "@//external/scudo:external_scudo_license",
+    "license_kinds": [
+      {
+        "target": "@//build/soong/licenses:SPDX-license-identifier-Apache-2.0",
+        "name": "SPDX-license-identifier-Apache-2.0",
+        "conditions": ["notice"]
+      }
+    ],
+    "copyright_notice": "",
+    "package_name": "Scudo Standalone",
+    "package_url": null,
+    "package_version": null
+  }
+]
+`,
+			want: `<!DOCTYPE html>
+<html>
+  <head>
+    <style type="text/css">
+      body { padding: 2px; margin: 0; }
+      .license { background-color: seashell; margin: 1em;}
+      pre { padding: 1em; }</style></head>
+  <body>
+    The following software has been included in this product and contains the license and notice as shown below.<p>
+    <strong>Discombobulator</strong><br>Copyright Notice: Copyright (C) The Android Open Source Project<br><a href=#0e6553ab7221430a352fb7706ebc2aad>License</a><br>
+    Used by: @//bionic/libc:libc_bionic_ndk @//system/logging/liblog:liblog<hr>
+    <strong>Scudo Standalone</strong><br>
+    Used by:<hr>
+    <div id="0e6553ab7221430a352fb7706ebc2aad" class="license"><pre>%%%Notice License%%%
+
+    </pre></div>
+  </body>
+</html>
+`,
+		}, // TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			buf := bytes.Buffer{}
+			newGenerator(tt.in).generate(&buf)
+			got := buf.String()
+			if got != tt.want {
+				t.Errorf("doit() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
