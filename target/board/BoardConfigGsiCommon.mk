b93b7104a2d2bf7cb0462799fe4ab7e98fa22165
SzuWei Lin <szuweilin@google.com>
2022-11-04 19:30:58 +0800

    Support system_dlkm in GSI
    From Android 13, all devices must include a system_dlkm partition.
    The patch enables system_dlkm on all GSI targets to support the devices
    with system_dlkm partition, and be compatible with old devices
    without a system_dlkm partition.
    
    With patch, there is no /system/system_dlkm, and add a symlink
    /system/lib/modules -> /system_dlkm/lib/modules
    
    Bug: 256984997
    Test: make gsi_arm64-user and aosp_arm64-user; check system folder
    Change-Id: I2453ec43d584423cc4d5f0e23405f0ae9b6c5c90

--- target/board/BoardConfigGsiCommon.mk
+++ target/board/BoardConfigGsiCommon.mk
@@ -2,7 +2,6 @@
 #
 # Common compile-time definitions for GSI
 # Builds upon the mainline config.
-#
 
 include build/make/target/board/BoardConfigMainlineCommon.mk
 
@@ -16,6 +15,12 @@ GSI_FILE_SYSTEM_TYPE ?= ext4
 BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := $(GSI_FILE_SYSTEM_TYPE)
 TARGET_USERIMAGES_SPARSE_EXT_DISABLED := true
 TARGET_USERIMAGES_SPARSE_EROFS_DISABLED := true
+
+# Enable system_dlkm image which creates a symlink im GSI to support 
+# the devices with system_dlkm partition
+BOARD_USES_SYSTEM_DLKMIMAGE := true
+BOARD_SYSTEM_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
+TARGET_COPY_OUT_SYSTEM_DLKM := system_dlkm
 
 # GSI also includes make_f2fs to support userdata parition in f2fs
 # for some devices
