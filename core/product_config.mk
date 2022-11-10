9e933b89089bf6890528b0930b3cb98b296e0b01
Spandan Das <spandandas@google.com>
2022-11-10 03:02:13 +0000

    New product config flag to gate blueprint modules
    Example usage:
    PRODUCT_INCLUDE_TAGS += use_myspecial_sdk
    
    This also populates the allowlist with go/nogo mainline tags. Usage of
    `PRODUCT_INCLUDE_TAGS` outside this allowlist will raise an error
    in product config
    
    Test: TH
    
    Change-Id: Ica82a8f65cbfda600d72fc54fb873c1eaa1666a7

--- core/product_config.mk
+++ core/product_config.mk
@@ -274,6 +274,14 @@ endif
 current_product_makefile :=
 
 #############################################################################
+# Check product include tag allowlist
+PRODUCT_INCLUDE_TAGS_ALLOWLIST := com.google.mainline_go com.google.mainline
+.KATI_READONLY := PRODUCT_INCLUDE_TAGS_ALLOWLIST
+$(foreach include_tag,$(PRODUCT_INCLUDE_TAGS), \
+	$(if $(filter $(include_tag),$(PRODUCT_INCLUDE_TAGS_ALLOWLIST)),,\
+	$(call pretty-error, $(include_tag) is not in PRODUCT_INCLUDE_TAGS_ALLOWLIST: $(PRODUCT_INCLUDE_TAGS_ALLOWLIST))))
+#############################################################################
+
 # Quick check and assign default values
 
 TARGET_DEVICE := $(PRODUCT_DEVICE)
