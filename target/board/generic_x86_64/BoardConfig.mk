b9365c52c63d2db2225122eef7e1bf7a9b961897
Ryo Hashimoto <hashimoto@google.com>
2022-11-16 17:10:30 +0900

    Add arch variants for Intel Atom CPUs
    Bug: 259201836
    Test: Build aosp_x86_64-eng with modified
    build/make/target/board/generic_x86_64/BoardConfig.mk
    
    Change-Id: Ide469d4aaa722602dbcc797cf6518fd20d24764c

--- target/board/generic_x86_64/BoardConfig.mk
+++ target/board/generic_x86_64/BoardConfig.mk
@@ -16,11 +16,11 @@
 # x86_64 emulator specific definitions
 TARGET_CPU_ABI := x86_64
 TARGET_ARCH := x86_64
-TARGET_ARCH_VARIANT := x86_64
+TARGET_ARCH_VARIANT := goldmont
 
 TARGET_2ND_CPU_ABI := x86
 TARGET_2ND_ARCH := x86
-TARGET_2ND_ARCH_VARIANT := x86_64
+TARGET_2ND_ARCH_VARIANT := goldmont
 
 # Include 64-bit mediaserver to support 64-bit only devices
 TARGET_DYNAMIC_64_32_MEDIASERVER := true
