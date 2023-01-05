6b588de5edba187e1f2c02d3f8f589548ba0272b
Wei Li <weiwli@google.com>
2023-01-04 17:13:47 -0800

    SBOM POC: generate sbom.spdx in product out directory.
    Test: CI
    
    Change-Id: I6464fa2dc416dea62c5cf688c923d6b496f044f5

--- core/main.mk
+++ core/main.mk
@@ -2013,6 +2013,26 @@ endif
 # missing dependency errors.
 $(call build-license-metadata)
 
+
+# Generate SBOM in SPDX format
+installed_files := $(sort $(filter $(PRODUCT_OUT)/%,$(modules_to_install)))
+# Remove the sbom-metadata.csv whenever makefile is evaluated
+# $(shell rm $(PRODUCT_OUT)/sbom-metadata.csv)
+$(PRODUCT_OUT)/sbom-metadata.csv: $(installed_files)
+	rm -rf $@
+	@echo installed_file$(comma)module_path >> $@
+	$(foreach f,$(installed_files),\
+	  $(foreach p,$(strip $(sort $(ALL_MODULES.$(ALL_INSTALLED_FILES.$f).PATH))),\
+	    @echo $(patsubst $(PRODUCT_OUT)/%,%,$f)$(comma)$p >> $@ $(newline)\
+	  )\
+	)
+
+.PHONY: sbom
+sbom: $(PRODUCT_OUT)/sbom.spdx
+$(PRODUCT_OUT)/sbom.spdx: $(PRODUCT_OUT)/sbom-metadata.csv $(GEN_SBOM)
+	rm -rf $@
+	$(GEN_SBOM) --output_file $@ --metadata $(PRODUCT_OUT)/sbom-metadata.csv --product_out_dir=$(PRODUCT_OUT) --build_version $(BUILD_FINGERPRINT_FROM_FILE)  --product_mfr=$(PRODUCT_MANUFACTURER)
+
 $(call dist-write-file,$(KATI_PACKAGE_MK_DIR)/dist.mk)
 
 $(info [$(call inc_and_print,subdir_makefiles_inc)/$(subdir_makefiles_total)] writing build rules ...)
