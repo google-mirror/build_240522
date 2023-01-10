19338f1151a6428fc179dd39be4c30bcd8ec7d33
Devin Moore <devinmoore@google.com>
2023-01-10 22:20:07 +0000

    Replace the HIDL cas service from default vendor images
    This has been converted to AIDL.
    
    Test: none
    Bug: 227673974
    Change-Id: I7eaef8da34d29c448358749d331758cbed7851df

--- target/product/base_vendor.mk
+++ target/product/base_vendor.mk
@@ -46,7 +46,7 @@ PRODUCT_HOST_PACKAGES += \
 
 # Base modules and settings for the vendor partition.
 PRODUCT_PACKAGES += \
-    android.hardware.cas@1.2-service \
+    android.hardware.cas-service.example \
     boringssl_self_test_vendor \
     dumpsys_vendor \
     fs_config_files_nonsystem \
@@ -78,6 +78,7 @@ PRODUCT_PACKAGES += \
 ifneq ($(TARGET_SUPPORTS_OMX_SERVICE),false)
     PRODUCT_PACKAGES_SHIPPING_API_LEVEL_33 += \
         android.hardware.media.omx@1.0-service \
+        android.hardware.cas@1.2-service \
 
 endif
 
