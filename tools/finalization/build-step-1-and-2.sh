b298531078a56632be7388c84d02ce3e139a06ba
Alex Buynytskyy <alexbuy@google.com>
2023-02-19 18:40:44 +0000

    Refresh the latest script versions + preparation for local mode.
    Bug: 260755259
    Test: local run
    Change-Id: I45d3651bd4b837e1338535f98d7256ca9b47e089

--- tools/finalization/build-step-1-and-2.sh
+++ tools/finalization/build-step-1-and-2.sh
@@ -2,14 +2,20 @@
 
 set -ex
 
-function finalize_main_step1() {
+function finalize_main_step12() {
     local top="$(dirname "$0")"/../..
 
     # default target to modify tree and build SDK
     local m="$top/build/soong/soong_ui.bash --make-mode TARGET_PRODUCT=aosp_arm64 TARGET_BUILD_VARIANT=userdebug"
 
-    # Build finalization artifacts.
+    # SDK codename -> int
     source $top/build/make/tools/finalization/finalize-aidl-vndk-sdk-resources.sh
+
+    # Platform/Mainline SDKs build and move to prebuilts
+    source $top/build/make/tools/finalization/localonly-finalize-mainline-sdk.sh
+
+    # REL
+    source $top/build/make/tools/finalization/finalize-sdk-rel.sh
 
     # This command tests:
     #   The release state for AIDL.
@@ -18,5 +24,5 @@ function finalize_main_step1() {
     AIDL_FROZEN_REL=true $m
 }
 
-finalize_main_step1
+finalize_main_step12
 
