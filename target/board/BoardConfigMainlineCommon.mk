71f015f9d8b5559dfdbee0765561f30b5fc79ae0
Roman Kiryanov <rkir@google.com>
2023-04-10 10:53:42 -0700

    Remove emulator dependencies on non emulator targets (2)
    Bug: 111538404
    Test: presubmit
    Change-Id: Idd55d7f10bc178cfe716d173dbe59d2bed5fd963
    Signed-off-by: Roman Kiryanov <rkir@google.com>

--- target/board/BoardConfigMainlineCommon.mk
+++ target/board/BoardConfigMainlineCommon.mk
@@ -14,6 +14,7 @@ TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_COPY_OUT_SYSTEM_EXT := system_ext
 TARGET_COPY_OUT_VENDOR := vendor
 TARGET_COPY_OUT_PRODUCT := product
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
 
 # Creates metadata partition mount point under root for
 # the devices with metadata parition
