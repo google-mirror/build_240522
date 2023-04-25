347ba769216d5a7abbd5931ab9d4ccd2fd710392
Julien Desprez <jdesprez@google.com>
2023-04-25 13:39:51 -0700

    Remove jars from suites that aren't used
    Test: presubmit
    Bug: 279638202
    Change-Id: I01ea97bee5c9502eebda7e09eb617854e1aaf1a8

--- core/tasks/tools/compatibility.mk
+++ core/tasks/tools/compatibility.mk
@@ -30,7 +30,10 @@ test_suite_subdir := android-$(test_suite_name)
 out_dir := $(HOST_OUT)/$(test_suite_name)/$(test_suite_subdir)
 test_artifacts := $(COMPATIBILITY.$(test_suite_name).FILES)
 test_tools := $(HOST_OUT_JAVA_LIBRARIES)/tradefed.jar \
+<<<<<<< PATCH SET (7dd662 Remove jars from suites that aren't used)
+=======
   $(HOST_OUT_JAVA_LIBRARIES)/tradefed-test-framework.jar \
+>>>>>>> BASE      (aea934 Merge "Remove tradefed-no-fwk from suites")
   $(HOST_OUT_JAVA_LIBRARIES)/loganalysis.jar \
   $(HOST_OUT_JAVA_LIBRARIES)/compatibility-host-util.jar \
   $(HOST_OUT_JAVA_LIBRARIES)/compatibility-tradefed.jar \
