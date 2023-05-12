ef3ab4d44ddf9d1e3a32779e1309218ca8095e61
Dennis Shen <dzshen@google.com>
2023-05-12 00:21:55 +0000

    Add first iteration of cpp codegen to aconfig
    The general idea to reuse java codegen's very neat tiny template idea.
    Extract java code gen's context generation part of code to its own file:
    codegen_context.rs which is reponsible for generating generic code
    generation context to be interfaced with tiny template.
    
    For generated cpp code, it is in the form of a collection of classes
    inside a namespace. The reason we choose a collection of classes rather
    than a collection of static functions is because gmock test technology
    only supports mocking virtual method.
    
    BUG: b/279483801
    TETS: atest aconfig.test
    
    Change-Id: I9ba00667437ff7c3e147ff2828171fc95528bebf

---
+++ tools/aconfig/src/codegen_context.rs
@@ -0,0 +1,58 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use serde::Serialize;
+
+use crate::aconfig::{FlagState, Permission};
+use crate::cache::{Cache, Item};
+
+pub fn generate_codegen_context(cache: &Cache) -> Context {
+    let class_elements: Vec<ClassElement> = cache.iter().map(create_class_element).collect();
+    let readwrite = class_elements.iter().any(|item| item.readwrite);
+    let namespace =
+        cache.iter().find(|item| !item.namespace.is_empty()).unwrap().namespace.to_lowercase();
+    Context { namespace: namespace, readwrite, class_elements }
+}
+
+#[derive(Serialize)]
+pub struct Context {
+    pub namespace: String,
+    pub readwrite: bool,
+    pub class_elements: Vec<ClassElement>,
+}
+
+#[derive(Serialize)]
+pub struct ClassElement {
+    pub method_name: String,
+    pub readwrite: bool,
+    pub default_value: String,
+    pub feature_name: String,
+    pub flag_name: String,
+}
+
+fn create_class_element(item: &Item) -> ClassElement {
+    ClassElement {
+        method_name: item.name.clone(),
+        readwrite: item.permission == Permission::ReadWrite,
+        default_value: if item.state == FlagState::Enabled {
+            "true".to_string()
+        } else {
+            "false".to_string()
+        },
+        feature_name: item.name.clone(),
+        flag_name: item.name.clone(),
+    }
+}
