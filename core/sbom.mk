eb6d12791023ea217ea75d655b7703fb97e94a3d
Wei Li <weiwli@google.com>
2023-05-30 15:03:03 -0700

    Include static libraries information in Android SBOM.
    Bug: 280852724
    Test: CIs
    Test: lunch aosp_cf_x86_64_phone-userdebug && m sbom
    Change-Id: Ie2365d79ba24910b7ace132b578589be10a17d78

--- core/sbom.mk
+++ core/sbom.mk
@@ -6,6 +6,8 @@ ifdef my_register_name
   ifneq (, $(strip $(ALL_MODULES.$(my_register_name).INSTALLED)))
     $(foreach installed_file,$(ALL_MODULES.$(my_register_name).INSTALLED),\
       $(eval ALL_INSTALLED_FILES.$(installed_file) := $(my_register_name))\
+      $(eval ALL_INSTALLED_FILES.$(installed_file).STATIC_LIBRARIES := $(strip $(sort $(LOCAL_STATIC_LIBRARIES))))\
+      $(eval ALL_INSTALLED_FILES.$(installed_file).WHOLE_STATIC_LIBRARIES := $(strip $(sort $(LOCAL_WHOLE_STATIC_LIBRARIES))))\
     )
   endif
 endif
