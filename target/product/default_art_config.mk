a64a3b18e7aa3a8a5dbd847da7716605c1646372
Kangping Dong <wgtdkp@google.com>
2023-06-19 07:29:46 +0000

    add framework-threadnetwork to bootclasspath
    Instructions: go/mainline-java-lib#classpath-configuration
    
    Ignore-AOSP-First: Internal side branch
    Bug: 262683651
    (cherry picked from https://googleplex-android-review.googlesource.com/q/commit:989f19261f67a3161048bae336df7f6382f14cb0)
    Merged-In: I6d7e2d7bb502ead3bab155a77a65cb0353d8b7d5
    Change-Id: I6d7e2d7bb502ead3bab155a77a65cb0353d8b7d5

--- target/product/default_art_config.mk
+++ target/product/default_art_config.mk
@@ -77,6 +77,7 @@ PRODUCT_APEX_BOOT_JARS := \
     com.android.tethering:framework-connectivity \
     com.android.tethering:framework-connectivity-t \
     com.android.tethering:framework-tethering \
+    com.android.threadnetwork:framework-threadnetwork \
     com.android.uwb:framework-uwb \
     com.android.virt:framework-virtualization \
     com.android.wifi:framework-wifi \
