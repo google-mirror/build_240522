2017418370233b6ca6982d1d666613449b9bbfe1
Dennis Shen <dzshen@google.com>
2023-06-29 00:45:42 +0000

    cpp codegen redesign, unit test support
    cpp codegen iteration 2, based on discussions with three internal teams
    that use c++. Refer to the design doc "aconfig c++ codegen" for detailed
    design. At a high level, we generate two sets of code artifacts with the
    same signatured api: one for production that without any local flag
    override capability, one for unit test that allows local flag overrides.
    It supports static methods style interface as well as injection pattern.
    
    Refer to the test points in the codegen_cpp.rs for examples of generated
    code.
    
    for production target codegen: aconfig create-cpp-lib --cache <cache> --out <out dir>
    for test target codegen: aconfig create-cpp-lib --cache <cache> --out
    <out dir> --mode test
    
    Bug: b/279483801
    Test: atest aconfig.test
    Change-Id: I92fefb9623d5435525339a74f57bbd36d0afef08

--- tools/aconfig/src/main.rs
+++ tools/aconfig/src/main.rs
@@ -17,7 +17,9 @@
 //! `aconfig` is a build time tool to manage build time configurations, such as feature flags.
 
 use anyhow::{anyhow, bail, ensure, Result};
-use clap::{builder::ArgAction, builder::EnumValueParser, Arg, ArgMatches, Command};
+use clap::{
+    builder::ArgAction, builder::EnumValueParser, Arg, ArgMatches, Command,
+};
 use core::any::Any;
 use std::fs;
 use std::io;
@@ -60,7 +62,13 @@ fn cli() -> Command {
         .subcommand(
             Command::new("create-cpp-lib")
                 .arg(Arg::new("cache").long("cache").required(true))
-                .arg(Arg::new("out").long("out").required(true)),
+                .arg(Arg::new("out").long("out").required(true))
+                .arg(
+                    Arg::new("mode")
+                        .long("mode")
+                        .value_parser(EnumValueParser::<commands::CodegenMode>::new())
+                        .default_value("production"),
+                ),
         )
         .subcommand(
             Command::new("create-rust-lib")
@@ -163,9 +171,12 @@ fn main() -> Result<()> {
         }
         Some(("create-cpp-lib", sub_matches)) => {
             let cache = open_single_file(sub_matches, "cache")?;
-            let generated_file = commands::create_cpp_lib(cache)?;
+            let mode = get_required_arg::<CodegenMode>(sub_matches, "mode")?;
+            let generated_files = commands::create_cpp_lib(cache, *mode)?;
             let dir = PathBuf::from(get_required_arg::<String>(sub_matches, "out")?);
-            write_output_file_realtive_to_dir(&dir, &generated_file)?;
+            generated_files
+                .iter()
+                .try_for_each(|file| write_output_file_realtive_to_dir(&dir, file))?;
         }
         Some(("create-rust-lib", sub_matches)) => {
             let cache = open_single_file(sub_matches, "cache")?;
