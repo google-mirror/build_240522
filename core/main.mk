618c89fb64a4a6e2c70ac9555205b77587beb8fa
Frank Feng <frankfeng@google.com>
2023-07-14 00:19:12 +0000

    Experimental
    Change-Id: I3ad5985ee0f87631941842cc414b7fed6d1375f9

--- core/main.mk
+++ core/main.mk
@@ -1387,6 +1387,7 @@ modules_to_install := $(sort \
 # copying them to their requested locations. Assign the eval result to an unused
 # var to prevent Make from trying to make a sense of it.
 _unused := $(call copy-many-files, $(sort $(ALL_COMPATIBILITY_DIST_FILES)))
+# ^ Error on line 1388
 
 # Don't include any GNU General Public License shared objects or static
 # libraries in SDK images.  GPL executables (not static/dynamic libraries)
