bbdc8ecc6bf021ae38338a54bc0a0cf2d7c2a1c8
Dennis Shen <dzshen@google.com>
2023-07-17 14:57:23 +0000

    aconfig: Rust codegen 2nd iteration
    Introduce rust codegen 2nd iteration with unit test support. The design
    is described in detail in the design doc (go/aconfig_rust_codegen). The general idea is to generate
    different code with the same signature for production and test build
    targets, just like java/c/cpp codegen.
    
    We will have a FlagProvider struct that has flag methods implementation.
    This flag provider instance can then be used in injection pattern. In
    additon, we also generate top level functions that wraps around flag
    provider call so it can be used in static function style.
    
    Things to be decided later:
    
    should we just generate one set of code, and use cfg! as compile
    time marco to compile the right code for the right targets.
    
    Bug: b/279483360
    Test: atest aconfig.test
    Change-Id: Ic75cedbd0d27b5242014c3ac7fc80692d2ab4589

--- tools/aconfig/src/commands.rs
+++ tools/aconfig/src/commands.rs
@@ -151,12 +151,12 @@ pub fn create_cpp_lib(mut input: Input, codegen_mode: CodegenMode) -> Result<Vec
     generate_cpp_code(package, parsed_flags.parsed_flag.iter(), codegen_mode)
 }
 
-pub fn create_rust_lib(mut input: Input) -> Result<OutputFile> {
+pub fn create_rust_lib(mut input: Input, codegen_mode: CodegenMode) -> Result<OutputFile> {
     let parsed_flags = input.try_parse_flags()?;
     let Some(package) = find_unique_package(&parsed_flags) else {
         bail!("no parsed flags, or the parsed flags use different packages");
     };
-    generate_rust_code(package, parsed_flags.parsed_flag.iter())
+    generate_rust_code(package, parsed_flags.parsed_flag.iter(), codegen_mode)
 }
 
 pub fn create_device_config_defaults(mut input: Input) -> Result<Vec<u8>> {
