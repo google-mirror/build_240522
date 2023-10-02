63854978fb00799b213a4b12af58f4648f0ac2b1
Cole Faust <colefaust@google.com>
2023-10-02 14:47:05 -0700

    Convert gsi_skip_mount.cfg to soong
    The LOCAL_POST_INSTALL_CMD is problematic for m installclean-less
    builds.
    
    Bug: 205632228
    Test: presubmits
    Change-Id: Id4eb2fd5fbd4f080aec89fa90583ac2ec97c5e1c

--- target/product/gsi_release.mk
+++ target/product/gsi_release.mk
@@ -36,7 +36,8 @@ PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
     system/lib/vndk-29 \
     system/lib/vndk-sp-29 \
     system/lib64/vndk-29 \
-    system/lib64/vndk-sp-29
+    system/lib64/vndk-sp-29 \
+    system/etc/init/config
 
 # GSI should always support up-to-date platform features.
 # Keep this value at the latest API level to ensure latest build system
