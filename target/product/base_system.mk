39e67e5636139bdc2b80ea9d31998c2f256fe773
Jared Duke <jdduke@google.com>
2023-10-19 23:13:14 +0000

    Remove viewcompiler binary inclusion
    Compiled views did not ship and is being removed. Remove the associated
    binary reference from default inclusion.
    
    Bug: 158121974
    Test: m
    Change-Id: I5e64259d258b15ee999522a7b62becc1cd3a6ed1

--- target/product/base_system.mk
+++ target/product/base_system.mk
@@ -284,7 +284,6 @@ PRODUCT_PACKAGES += \
     uncrypt \
     usbd \
     vdc \
-    viewcompiler \
     voip-common \
     vold \
     watchdogd \
@@ -380,7 +379,6 @@ PRODUCT_HOST_PACKAGES += \
     unwind_info \
     unwind_reg_info \
     unwind_symbols \
-    viewcompiler \
     tzdata_host \
     tzdata_host_tzdata_apex \
     tzlookup.xml_host_tzdata_apex \
