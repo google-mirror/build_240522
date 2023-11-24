9a0023c1657b46e0c8ac8cb58e214efb2619c9a1
Aleksei Vetrov <vvvvvv@google.com>
2023-11-24 19:34:34 +0000

    Add new ReleaseNdkAbiMonitored soong variable
    Pass new trunk stable build flag RELEASE_NDK_ABI_MONITORED to soong
    config. This flag is used to enable ABI monitoring of NDK libraries.
    
    Test: TH
    Bug: 156513478
    Change-Id: I63d9215caa39e9f5d6961ddf3a4b289dfd002360

--- core/soong_config.mk
+++ core/soong_config.mk
@@ -329,6 +329,8 @@ $(call add_json_str, ReleaseAconfigFlagDefaultPermission,    $(RELEASE_ACONFIG_F
 
 $(call add_json_bool, ReleaseDefaultModuleBuildFromSource,   $(RELEASE_DEFAULT_MODULE_BUILD_FROM_SOURCE))
 
+$(call add_json_bool, ReleaseNdkAbiMonitored,   $(RELEASE_NDK_ABI_MONITORED))
+
 $(call add_json_bool, KeepVndk, $(filter true,$(KEEP_VNDK)))
 
 $(call add_json_bool, CheckVendorSeappViolations, $(filter true,$(CHECK_VENDOR_SEAPP_VIOLATIONS)))
