96333734c2ec9f29c86ef2d6b85305024ab18ec9
Dennis Shen <dzshen@google.com>
2023-12-01 21:04:29 +0000

    aconfig: add capability to create storage files part I: package.map
    The package map file is a hashtable in file to store the following
    mapping:
    
    package name -> package id and flag value file start offset for this
    package
    
    So this is hashtable is used to get the starting region in the value
    file that stores flag values for this package. We also store the package
    id so that it is more efficient in the flags.map file which is the
    second hashtable in file to be created.
    
    1, [main.rs, commands.rs] add a new aconfig command called create-storage which takes a number
    of aconfig cache files that belong to a specific container and produce
    storage files.
    
    2, [create_storage.rs] a module that executes storage creation process.
    It first sort flags by package for the designated container. And then
    pass the sorted flag information to each file creator.
    
    3, [package_table.rs] package.map file creator, the PackageTabel struct
    is the top level struct that represents the package hash table. The
    "as_bytes" method is implemented to serialize the hash table in a fixed
    format.
    
    To ensure cross platform compatibility, we use fixed format
    serialization rather in memory representation serialization. All u32 are
    serialized with big endian encoding, and storage client when
    deserializing would also use big endian format. Rust strings are by default UTF8
    when serialized, so it is already cross platform compatible.
    
    Note 1: unit tests for create_stroage and package_table module will be
    added in next cl. Wanting to have this cl reviewed and agreed on first.
    Also, try to keep cl in a reasonable size.
    
    Note 2, flag.map and flag.val file creation will be added in further cls
    using a similar pattern.
    
    Change-Id: Ia7e9f68237ea903f295ac7891c923f6a39f3422d

---
+++ tools/aconfig/src/create_storage.rs
@@ -0,0 +1,118 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use anyhow::Result;
+use std::collections::{hash_map::DefaultHasher, HashMap, HashSet};
+use std::hash::{Hash, Hasher};
+use std::path::PathBuf;
+
+use crate::commands::OutputFile;
+use crate::package_table::PackageTable;
+use crate::protos::{ProtoParsedFlag, ProtoParsedFlags};
+
+pub const FILE_VERSION: u32 = 1;
+
+pub const HASH_PRIMES: [u32; 29] = [
+    7, 13, 29, 53, 97, 193, 389, 769, 1543, 3079, 6151, 12289, 24593, 49157, 98317, 196613, 393241,
+    786433, 1572869, 3145739, 6291469, 12582917, 25165843, 50331653, 100663319, 201326611,
+    402653189, 805306457, 1610612741,
+];
+
+/// Get the right hash table size given number of entries in the table. Use a
+/// load factor of 0.5 for performance.
+pub fn get_table_size(entries: u32) -> Option<u32> {
+    HASH_PRIMES.iter().find(|&&num| num >= 2 * entries).copied()
+}
+
+/// Get the corresponding bucket index given the key and number of buckets
+pub fn get_bucket_index<T: Hash>(val: &T, num_buckets: u32) -> u64 {
+    let mut s = DefaultHasher::new();
+    val.hash(&mut s);
+    s.finish() % num_buckets as u64
+}
+
+pub struct FlagPackage<'a> {
+    pub package_name: &'a str,
+    pub package_id: u32,
+    pub flag_names: HashSet<&'a str>,
+    pub boolean_flags: Vec<&'a ProtoParsedFlag>,
+    pub boolean_offset: u32,
+}
+
+impl<'a> FlagPackage<'a> {
+    fn new(package_name: &'a str, package_id: u32) -> Self {
+        FlagPackage {
+            package_name,
+            package_id,
+            flag_names: HashSet::new(),
+            boolean_flags: vec![],
+            boolean_offset: 0,
+        }
+    }
+
+    fn insert(&mut self, pf: &'a ProtoParsedFlag) {
+        if self.flag_names.insert(pf.name()) {
+            self.boolean_flags.push(pf);
+        }
+    }
+}
+
+pub fn group_flags_by_package<'a, I>(parsed_flags_vec_iter: I) -> Vec<FlagPackage<'a>>
+where
+    I: Iterator<Item = &'a ProtoParsedFlags>,
+{
+    // group flags by package
+    let mut packages: Vec<FlagPackage<'a>> = Vec::new();
+    let mut package_index: HashMap<&'a str, usize> = HashMap::new();
+    for parsed_flags in parsed_flags_vec_iter {
+        for parsed_flag in parsed_flags.parsed_flag.iter() {
+            let index = *(package_index.entry(parsed_flag.package()).or_insert(packages.len()));
+            if index == packages.len() {
+                packages.push(FlagPackage::new(parsed_flag.package(), index as u32));
+            }
+            packages[index].insert(parsed_flag);
+        }
+    }
+
+    // calculate package flag value start offset, in flag value file, each boolean
+    // is stored as two bytes, the first byte will be the flag value. the second
+    // byte is flag info byte, which is a bitmask to indicate the status of a flag
+    let mut boolean_offset = 0;
+    for p in packages.iter_mut() {
+        p.boolean_offset = boolean_offset;
+        boolean_offset += 2 * p.boolean_flags.len() as u32;
+    }
+
+    packages
+}
+
+pub fn generate_storage_files<'a, I>(
+    container: &str,
+    parsed_flags_vec_iter: I,
+) -> Result<Vec<OutputFile>>
+where
+    I: Iterator<Item = &'a ProtoParsedFlags>,
+{
+    let packages = group_flags_by_package(parsed_flags_vec_iter);
+
+    // create and serialize package map
+    let package_table = PackageTable::new(container, &packages);
+    let package_table_file_path = PathBuf::from(r"package.map");
+    let package_table_file =
+        OutputFile { contents: package_table.as_bytes(), path: package_table_file_path };
+
+    Ok(vec![package_table_file])
+}
