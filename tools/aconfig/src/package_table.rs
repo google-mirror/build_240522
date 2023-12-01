e8c195275849c741f63d5751cac471f95c64bf37
Dennis Shen <dzshen@google.com>
2023-12-01 21:04:29 +0000

    aconfig: add capability to create storage files part I: package.map
    The package map file is a hashtable in file to store the following
    mapping:
    
    package name -> package id and flag value file start offset for this
    package
    
    So this is hashtable is used to get the starting region in the value
    file that stores flag values for this package. We also store the package
    id so that it is more efficient in the flags.map file which is the
    second hashtable in file to be created.
    
    1, [main.rs, commands.rs] add a new aconfig command called create-storage which takes a number
    of aconfig cache files that belong to a specific container and produce
    storage files.
    
    2, [create_storage.rs] a module that executes storage creation process.
    It first sort flags by package for the designated container. And then
    pass the sorted flag information to each file creator.
    
    3, [package_table.rs] package.map file creator, the PackageTabel struct
    is the top level struct that represents the package hash table. The
    "as_bytes" method is implemented to serialize the hash table in a fixed
    format.
    
    To ensure cross platform compatibility, we use fixed format
    serialization rather in memory representation serialization. All u32 are
    serialized with big endian encoding, and storage client when
    deserializing would also use big endian format. Rust strings are by default UTF8
    when serialized, so it is already cross platform compatible.
    
    Note 1: unit tests for create_stroage and package_table module will be
    added in next cl. Wanting to have this cl reviewed and agreed on first.
    Also, try to keep cl in a reasonable size.
    
    Note 2, flag.map and flag.val file creation will be added in further cls
    using a similar pattern.
    
    Change-Id: Ia7e9f68237ea903f295ac7891c923f6a39f3422d

---
+++ tools/aconfig/src/package_table.rs
@@ -0,0 +1,148 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+use crate::create_storage::{self, FlagPackage};
+
+pub struct PackageTableHeader<'a> {
+    pub version: u32,
+    pub container: &'a str,
+    pub file_size: u32,
+    pub num_packages: u32,
+    pub bucket_offset: u32,
+    pub node_offset: u32,
+}
+
+impl<'a> PackageTableHeader<'a> {
+    fn new(container: &'a str, num_packages: u32) -> Self {
+        PackageTableHeader {
+            version: create_storage::FILE_VERSION,
+            container,
+            file_size: 0,
+            num_packages,
+            bucket_offset: 0,
+            node_offset: 0,
+        }
+    }
+
+    fn as_bytes(&self) -> Vec<u8> {
+        let mut result = Vec::new();
+        result.extend_from_slice(&self.version.to_be_bytes());
+        let container_bytes = self.container.as_bytes();
+        result.extend_from_slice(&(container_bytes.len() as u32).to_be_bytes());
+        result.extend_from_slice(container_bytes);
+        result.extend_from_slice(&self.file_size.to_be_bytes());
+        result.extend_from_slice(&self.num_packages.to_be_bytes());
+        result.extend_from_slice(&self.bucket_offset.to_be_bytes());
+        result.extend_from_slice(&self.node_offset.to_be_bytes());
+        result
+    }
+}
+
+pub struct PackageTableNode<'a> {
+    pub package_name: &'a str,
+    pub package_id: u32,
+    pub boolean_offset: u32,
+    pub next_offset: u32,
+    pub start_offset: u32,
+    pub bucket_index: u32,
+}
+
+impl<'a> PackageTableNode<'a> {
+    fn new(package: &'a FlagPackage, num_buckets: u32) -> Self {
+        let bucket_index =
+            create_storage::get_bucket_index(&package.package_name.to_string(), num_buckets) as u32;
+        PackageTableNode {
+            package_name: package.package_name,
+            package_id: package.package_id,
+            boolean_offset: package.boolean_offset,
+            next_offset: 0,
+            start_offset: 0,
+            bucket_index,
+        }
+    }
+
+    fn as_bytes(&self) -> Vec<u8> {
+        let mut result = Vec::new();
+        let name_bytes = self.package_name.as_bytes();
+        result.extend_from_slice(&(name_bytes.len() as u32).to_be_bytes());
+        result.extend_from_slice(name_bytes);
+        result.extend_from_slice(&self.package_id.to_be_bytes());
+        result.extend_from_slice(&self.boolean_offset.to_be_bytes());
+        result.extend_from_slice(&self.next_offset.to_be_bytes());
+        result
+    }
+}
+
+pub struct PackageTable<'a> {
+    pub header: PackageTableHeader<'a>,
+    pub buckets: Vec<Option<u32>>,
+    pub nodes: Vec<PackageTableNode<'a>>,
+}
+
+impl<'a> PackageTable<'a> {
+    pub fn new(container: &'a str, packages: &'a Vec<FlagPackage<'a>>) -> Self {
+        // create table
+        let num_packages = packages.len() as u32;
+        let num_buckets =
+            create_storage::get_table_size(num_packages).expect("Number of packages is too large");
+        let mut table = PackageTable {
+            header: PackageTableHeader::new(container, num_packages),
+            buckets: vec![None; num_buckets as usize],
+            nodes: packages.iter().map(|pkg| PackageTableNode::new(pkg, num_buckets)).collect(),
+        };
+
+        // sort nodes by bucket index for efficiency
+        table.nodes.sort_by(|a, b| a.bucket_index.cmp(&b.bucket_index));
+
+        // fill all node offset
+        let mut offset = 0;
+        for i in 0..table.nodes.len() {
+            let node_bucket_idx = table.nodes[i].bucket_index;
+            let next_node_bucket_idx = if i + 1 < table.nodes.len() {
+                table.nodes[i + 1].bucket_index
+            } else {
+                num_buckets
+            };
+
+            table.nodes[i].start_offset = offset;
+            if table.buckets[node_bucket_idx as usize].is_none() {
+                table.buckets[node_bucket_idx as usize] = Some(offset);
+            }
+            offset += table.nodes[i].as_bytes().len() as u32;
+
+            if node_bucket_idx == next_node_bucket_idx {
+                table.nodes[i].next_offset = offset;
+            }
+        }
+
+        // fill table region offset
+        table.header.bucket_offset = table.header.as_bytes().len() as u32;
+        table.header.node_offset = table.header.bucket_offset + num_buckets * 4;
+        table.header.file_size = table.header.node_offset
+            + table.nodes.iter().map(|x| x.as_bytes().len()).sum::<usize>() as u32;
+
+        table
+    }
+
+    pub fn as_bytes(&self) -> Vec<u8> {
+        vec![
+            self.header.as_bytes(),
+            self.buckets.iter().map(|v| v.unwrap_or(0).to_be_bytes()).collect::<Vec<_>>().concat(),
+            self.nodes.iter().map(|v| v.as_bytes()).collect::<Vec<_>>().concat(),
+        ]
+        .concat()
+    }
+}
