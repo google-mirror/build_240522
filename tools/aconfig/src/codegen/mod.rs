1f09664d22e5d963693a1a60c7d23afcbe28dfb0
Zhi Dou <zhidou@google.com>
2023-12-14 21:17:13 +0000

    aconfig: add exported mode in c/c++ codegen
    This commit adds exported mode to c/c++ codegen.
    
    When the codegen mode is exported
    1. only flags with exported: true will be generated
    2. the generated getter should be in a read_write format and with
       default value as false, regardless the original permission and state
       of the flag
    
    This change moves process parsed_flags funciton into codegen crate. The
    process function is used to process parsed_flags based on the codegen
    mode.
    
    The template has also modified to keep readability.
    
    Bug: 316357680
    Test: atest aconfig.test aconfig.test.cpp aconfig.test.cpp.test_mode
    
    Change-Id: I511e05ab93b07a04236055d956d1926f4ed89f36

--- tools/aconfig/src/codegen/mod.rs
+++ tools/aconfig/src/codegen/mod.rs
@@ -18,6 +18,8 @@ pub mod cpp;
 pub mod java;
 pub mod rust;
 
+use crate::commands::CodegenMode;
+use crate::protos::{ProtoFlagPermission, ProtoFlagState, ProtoParsedFlag};
 use anyhow::{ensure, Result};
 
 pub fn is_valid_name_ident(s: &str) -> bool {
@@ -50,6 +52,28 @@ pub fn create_device_config_ident(package: &str, flag_name: &str) -> Result<Stri
     ensure!(is_valid_package_ident(package), "bad package");
     ensure!(is_valid_name_ident(flag_name), "bad flag name");
     Ok(format!("{}.{}", package, flag_name))
+}
+
+pub fn process_parsed_flags<I>(
+    parsed_flags_iter: I,
+    codegen_mode: CodegenMode,
+) -> Vec<ProtoParsedFlag>
+where
+    I: Iterator<Item = ProtoParsedFlag>,
+{
+    match codegen_mode {
+        CodegenMode::Exported => {
+            let mut parsed_flags: Vec<_> =
+                parsed_flags_iter.filter(|pf| pf.is_exported()).collect();
+            for flag in parsed_flags.iter_mut() {
+                flag.set_state(ProtoFlagState::DISABLED);
+                flag.set_permission(ProtoFlagPermission::READ_WRITE);
+                flag.set_is_fixed_read_only(false);
+            }
+            parsed_flags
+        }
+        _ => parsed_flags_iter.collect(),
+    }
 }
 
 #[cfg(test)]
@@ -121,5 +145,32 @@ mod tests {
             "com.foo.bar.some_flag",
             create_device_config_ident("com.foo.bar", "some_flag").unwrap()
         );
+    }
+
+    #[test]
+    fn test_process_parsed_flags_prod() {
+        let parsed_flags = crate::test::parse_test_flags();
+        let p_parsed_flags = process_parsed_flags(
+            parsed_flags.parsed_flag.clone().into_iter(),
+            CodegenMode::Production,
+        );
+        assert_eq!(parsed_flags.parsed_flag.len(), p_parsed_flags.len());
+        for (i, item) in p_parsed_flags.iter().enumerate() {
+            assert!(parsed_flags.parsed_flag[i].eq(item));
+        }
+    }
+
+    #[test]
+    fn test_process_parsed_flags_exported() {
+        let parsed_flags = crate::test::parse_test_flags();
+        let p_parsed_flags =
+            process_parsed_flags(parsed_flags.parsed_flag.into_iter(), CodegenMode::Exported);
+        assert_eq!(3, p_parsed_flags.len());
+        for flag in p_parsed_flags.iter() {
+            assert_eq!(ProtoFlagState::DISABLED, flag.state());
+            assert_eq!(ProtoFlagPermission::READ_WRITE, flag.permission());
+            assert!(!flag.is_fixed_read_only());
+            assert!(flag.is_exported());
+        }
     }
 }
