c3d69e3aec4846fab0b0c0344a40f77437f66e2f
Zhi Dou <zhidou@google.com>
2023-12-15 22:48:16 +0000

    aconfig: add new testing flag enabled_fixed_ro_exported
    This commit adds a new testing flag enabled_fixed_ro_exported to test
    the case of a exported and fixed_read_only flag.
    
    Test: atest aconfig.test aconfig.test.java AconfigJavaHostTest
    aconfig.test.cpp aconfig.test.cpp.test_mode aconfig.prod_mode.test.rust
    Bug: 316357680
    
    Change-Id: Iaedb8a6875166c6a6d24c7c3deee701a496b4964

--- tools/aconfig/src/codegen/mod.rs
+++ tools/aconfig/src/codegen/mod.rs
@@ -165,7 +165,7 @@ mod tests {
         let parsed_flags = crate::test::parse_test_flags();
         let p_parsed_flags =
             process_parsed_flags(parsed_flags.parsed_flag.into_iter(), CodegenMode::Exported);
-        assert_eq!(2, p_parsed_flags.len());
+        assert_eq!(3, p_parsed_flags.len());
         for flag in p_parsed_flags.iter() {
             assert_eq!(ProtoFlagState::DISABLED, flag.state());
             assert_eq!(ProtoFlagPermission::READ_WRITE, flag.permission());
