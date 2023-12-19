621652f3252b5aa3b27b9624c8e14b338f3a8c9a
Ronald Braunstein <rbraunstein@google.com>
2023-12-19 15:35:22 -0800

    New tool to concat owner data from soong output
    Still has some debugging output to console.
    Test: m all_teams
    
    Change-Id: I12c4b8c8f67363a1b034747aa3fd986aa128b10e

---
+++ tools/team_collector/team_collector.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+
+	"android/soong/android/team_proto"
+
+	"google.golang.org/protobuf/encoding/prototext"
+)
+
+// TODO(ron): remove?
+func readFileToString(filePath string) string {
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	data, err := io.ReadAll(file)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return string(data)
+}
+
+func main() {
+	inputFile := flag.String("inputFile", "", "Input file path, file of filenames")
+	outputFile := flag.String("outputFile", "", "Output file path")
+	flag.Parse()
+
+	if *inputFile == "" || *outputFile == "" {
+		fmt.Println("Usage: team_collector -inputFile <input file path> -outputFile <output file path>")
+		os.Exit(1)
+	}
+
+	// TODO(ron): is single input special cased?
+	inputFileData := strings.TrimRight(readFileToString(*inputFile), "\n")
+	filePaths := strings.Split(inputFileData, " ")
+	teams := make([]*team_proto.Team, len(filePaths))
+	for i, filePath := range filePaths {
+		// or ioutil.ReadFile?
+		teamProtoBytes, err := os.ReadFile(filePath)
+		if err != nil {
+			log.Fatal("Error processing file") // TOOD(ron) add filePath to msg.
+		}
+
+		c := new(team_proto.Team)
+		fmt.Printf("team file: %s\n", filePath)
+		if err := prototext.Unmarshal(teamProtoBytes, c); err != nil {
+			log.Fatalln("Failed to parse team file to B: ", err)
+		}
+
+		teams[i] = c
+	}
+
+	allTeams := team_proto.AllTeams{Teams: teams}
+
+	for _, p := range allTeams.Teams {
+		fmt.Println(p.String())
+	}
+
+	// Marshal allTeams  to outputFile.
+	os.WriteFile(*outputFile, []byte(allTeams.String()), 0755)
+}
