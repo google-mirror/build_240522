c2792f297bde809a3eee76d68431dd1ca01f339a
Zhi Dou <zhidou@google.com>
2023-12-20 14:23:42 +0000

    aconfig: update error message in modify_parsed_flags_based_on_mode
    This commit updates the error message in
    modify_parsed_flags_based_on_mode, when there is no certain mode flags
    in the corresponding library, and this change remove "." at the end of
    the error message to conform to existing standard in this project.
    
    Bug: 311152507
    Test: atest aconfig.test
    Change-Id: Ib120e7f7252981bbeb6ca1cf785da24f329507b4

--- tools/aconfig/src/commands.rs
+++ tools/aconfig/src/commands.rs
@@ -339,7 +339,7 @@ pub fn modify_parsed_flags_based_on_mode(
         }
     };
     if modified_parsed_flags.is_empty() {
-        bail!("{} library contains no exported flags.", codegen_mode);
+        bail!("{mode} library contains no {mode} flags", mode = codegen_mode);
     }
 
     Ok(modified_parsed_flags)
@@ -654,7 +654,7 @@ mod tests {
         let error =
             modify_parsed_flags_based_on_mode(parsed_flags, CodegenMode::Exported).unwrap_err();
         assert_eq!(
-            format!("{} library contains no exported flags.", CodegenMode::Exported),
+            format!("{mode} library contains no {mode} flags", mode = CodegenMode::Exported),
             format!("{:?}", error)
         );
     }
