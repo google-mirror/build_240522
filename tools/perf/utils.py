6645fb5584769afdebe266b6bf1fb8cf49084f37
Yu Liu <yudiliu@google.com>
2024-01-17 21:30:53 +0000

    Tweak the CUJ scripts to make it work in CI.
    Bug: 318706915
    Test: manual test
    Change-Id: I0982d1d724ec05aee7a0d6bdaa05497745421674

--- tools/perf/utils.py
+++ tools/perf/utils.py
@@ -15,7 +15,7 @@
 import os
 import pathlib
 
-DEFAULT_REPORT_DIR = "benchmarks"
+DEFAULT_REPORT_DIR = "cuj_out/benchmarks"
 
 def get_root():
     top_dir = os.environ.get("ANDROID_BUILD_TOP")
@@ -28,3 +28,15 @@ def get_root():
         d = d.parent
         if d == pathlib.Path("/"):
             return None
+
+def get_dist_dir():
+    dist_dir = os.getenv("DIST_DIR")
+    if dist_dir:
+        return pathlib.Path(dist_dir).resolve()
+    return get_out_dir().joinpath("dist")
+
+def get_out_dir():
+    out_dir = os.getenv("OUT_DIR")
+    if not out_dir:
+        out_dir = "out"
+    return pathlib.Path(out_dir).resolve()
