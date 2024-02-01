6ea540b2af64a2f9d22707de30a911f3f7bb9fe4
Dennis Shen <dzshen@google.com>
2024-02-01 14:43:07 +0000

    aconfig: add top level flag read lib api
    1, update android build soong targets as now libmemmap2 has been
    introduced to android code base.
    
    2, add three public apis for package offset query, flag offset query,
    and flag value query. These apis assume the stoarge location pb file is
    at /metadata/aconfig/aconfig_storage.pb. This file will be created by
    storage daemon. For testing purposes, we also expose impl version of
    these apis that accept a custom storage location pb file.
    
    Bug: 321077378
    Test: atest aconfig_storage_file.test
    Change-Id: I91b284e2dfcdb8ff3674d463ed1c13696d9ae020

--- tools/aconfig/aconfig_storage_file/src/test_utils.rs
+++ tools/aconfig/aconfig_storage_file/src/test_utils.rs
@@ -17,18 +17,68 @@
 use crate::protos::ProtoStorageFiles;
 use anyhow::Result;
 use protobuf::Message;
+use std::fs;
 use std::io::Write;
+use std::path::Path;
 use tempfile::NamedTempFile;
 
-pub fn get_binary_storage_proto_bytes(text_proto: &str) -> Result<Vec<u8>> {
+pub(crate) fn get_binary_storage_proto_bytes(text_proto: &str) -> Result<Vec<u8>> {
     let storage_files: ProtoStorageFiles = protobuf::text_format::parse_from_str(text_proto)?;
     let mut binary_proto = Vec::new();
     storage_files.write_to_vec(&mut binary_proto)?;
     Ok(binary_proto)
 }
 
-pub fn write_bytes_to_temp_file(bytes: &[u8]) -> Result<NamedTempFile> {
+pub(crate) fn write_bytes_to_temp_file(bytes: &[u8]) -> Result<NamedTempFile> {
     let mut file = NamedTempFile::new()?;
     let _ = file.write_all(&bytes);
     Ok(file)
+}
+
+fn has_same_content(file1: &Path, file2: &Path) -> Result<bool> {
+    let bytes1 = fs::read(file1)?;
+    let bytes2 = fs::read(file2)?;
+    if bytes1.len() != bytes2.len() {
+        return Ok(false);
+    }
+    for (i, &b1) in bytes1.iter().enumerate() {
+        if b1 != bytes2[i] {
+            return Ok(false);
+        }
+    }
+    Ok(true)
+}
+
+pub(crate) fn create_temp_storage_files_for_test() {
+    let file_paths = [
+        ("./tests/package.map", "./tests/tmp.ro.package.map"),
+        ("./tests/flag.map", "./tests/tmp.ro.flag.map"),
+        ("./tests/flag.val", "./tests/tmp.ro.flag.val"),
+    ];
+    for (file_path, copied_file_path) in file_paths.into_iter() {
+        let file_path = Path::new(&file_path);
+        let copied_file_path = Path::new(&copied_file_path);
+        if copied_file_path.exists() && !has_same_content(file_path, copied_file_path).unwrap() {
+            fs::remove_file(copied_file_path).unwrap();
+        }
+        if !copied_file_path.exists() {
+            fs::copy(file_path, copied_file_path).unwrap();
+        }
+    }
+}
+
+pub(crate) fn set_temp_storage_files_to_read_only() {
+    let file_paths = [
+        "./tests/tmp.ro.package.map",
+        "./tests/tmp.ro.flag.map",
+        "./tests/tmp.ro.flag.val",
+    ];
+    for file_path in file_paths.into_iter() {
+        let file_path = Path::new(&file_path);
+        let mut perms = fs::metadata(file_path).unwrap().permissions();
+        if !perms.readonly() {
+            perms.set_readonly(true);
+            fs::set_permissions(file_path, perms).unwrap();
+        }
+    }
 }
