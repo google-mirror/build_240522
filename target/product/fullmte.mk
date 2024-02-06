46983bc4cffa20cfbf1a33dba47477040ea21ef7
Mitch Phillips <mitchp@google.com>
2024-02-06 15:21:48 +0100

    Re-enable MTE stack on fullmte builds.
    The new toolchain has been rolled, and so we can re-enable stack MTE
    instrumentation.
    
    Bug: 320448268
    Bug: 292478827
    Test: atest bionic-unit-tests bionic-unit-tests-static \
          CtsTaggingHostTestCases debuggerd_test CtsGwpAsanTestCases \
          gwp_asan_unittest CtsHostsideWebViewTests # on husky_fullmte
    Change-Id: Ie238a015edba3c6ca2e1266cee9142b0d8927cc2

--- target/product/fullmte.mk
+++ target/product/fullmte.mk
@@ -20,9 +20,10 @@
 # For more details, see:
 # https://source.android.com/docs/security/test/memory-safety/arm-mte
 ifeq ($(filter memtag_heap,$(SANITIZE_TARGET)),)
-  # TODO(b/292478827): Re-enable memtag_stack when new toolchain rolls.
-  SANITIZE_TARGET := $(strip $(SANITIZE_TARGET) memtag_heap)
+  SANITIZE_TARGET := $(strip $(SANITIZE_TARGET) memtag_heap memtag_stack)
   SANITIZE_TARGET_DIAG := $(strip $(SANITIZE_TARGET_DIAG) memtag_heap)
 endif
+# SYNC MTE in all native processes by default.
 PRODUCT_PRODUCT_PROPERTIES += persist.arm64.memtag.default=sync
+# Enlarged malloc/free stack trace buffer size in the heap allocator.
 PRODUCT_SCUDO_ALLOCATION_RING_BUFFER_SIZE := 131072
