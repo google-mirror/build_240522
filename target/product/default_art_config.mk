078f2e2a01fe7f70f8b3c0dc32114551fd8e5528
Nishant Panwar <nishantpanwar@google.com>
2024-02-10 09:00:04 +0000

    Add framework-pdf-pre-v jar in MediaProvider
    Bug: b/324667310
    Test: presubmit and m
    Change-Id: I92aabf297508f2c885a721d11d40b5262b6847fe

--- target/product/default_art_config.mk
+++ target/product/default_art_config.mk
@@ -74,6 +74,7 @@ PRODUCT_APEX_BOOT_JARS := \
     com.android.media:updatable-media \
     com.android.mediaprovider:framework-mediaprovider \
     com.android.mediaprovider:framework-pdf \
+    com.android.mediaprovider:framework-pdf-pre-v \
     com.android.ondevicepersonalization:framework-ondevicepersonalization \
     com.android.os.statsd:framework-statsd \
     com.android.permission:framework-permission \
