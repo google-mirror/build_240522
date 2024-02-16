dc34687dded10f8f93f1d472b48a648da13fcc56
Kadir Çetinkaya <kadircet@google.com>
2024-02-16 12:14:08 +0000

    Add CC analysis support to ide_query
    Introduces ide_query_cc_analyzer, which figures out relevant build targets that needs to be built for a given C++ source or header file.
    Once these targets are built, it analyzes the sources in question and reports any generated files that are used back.
    
    Full ide_query integration relies on this binary also being available in prebuilts clang-tools, it'll be done in a future patch.
    
    Change-Id: Ib0ef6da7a2bc8ecf66940b326e037fb1ee230bf9

--- tools/ide_query/ide_query.sh
+++ tools/ide_query/ide_query.sh
@@ -7,6 +7,17 @@ require_top
 # Ensure cogsetup (out/ will be symlink outside the repo)
 . ${TOP}/build/make/cogsetup.sh
 
+case $(uname -s) in
+    Linux)
+      export PREBUILTS_CLANG_TOOLS_ROOT="${TOP}/prebuilts/clang-tools/linux-x86/"
+      PREBUILTS_GO_ROOT="${TOP}/prebuilts/go/linux-x86/"
+      ;;
+    *)
+      echo "Only supported for linux hosts" >&2
+      exit 1
+      ;;
+esac
+
 export ANDROID_BUILD_TOP=$TOP
 export OUT_DIR=${OUT_DIR}
-exec "${TOP}/prebuilts/go/linux-x86/bin/go" "run" "ide_query" "$@"
+exec "${PREBUILTS_GO_ROOT}/bin/go" "run" "ide_query" "$@"
