cc6fc7f9311667da3c068637b6fe2c2947f8e96f
Ted Bauer <tedbauer@google.com>
2024-03-04 22:45:29 +0000

    aflags: add initial enable and disable commands
    Bug: 324436145
    Test: adb shell aflags enable
    Change-Id: I6b4af4bccbbc6000a9dcb78c3130f836b0f85bd0

--- tools/aconfig/aflags/src/main.rs
+++ tools/aconfig/aflags/src/main.rs
@@ -16,7 +16,7 @@
 
 //! `aflags` is a device binary to read and write aconfig flags.
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
 
 mod device_config_source;
@@ -60,11 +60,12 @@ struct Flag {
     container: String,
     value: String,
     permission: FlagPermission,
-    value_picked_from: ValuePickedFrom,
+    override_type: ValuePickedFrom,
 }
 
 trait FlagSource {
     fn list_flags() -> Result<Vec<Flag>>;
+    fn override_flag(namespace: &str, package: &str, name: &str, value: &str) -> Result<()>;
 }
 
 const ABOUT_TEXT: &str = "Tool for reading and writing flags.
@@ -78,6 +79,7 @@ Rows in the table from the `list` command follow this format:
   * `value`: the value read from the flag.
   * `provenance`: one of:
     + `default`: the flag value comes from its build-time default.
+    + `manual`: the flag value comes from a local manual override.
     + `server`: the flag value comes from a server override.
   * `permission`: read-write or read-only.
   * `container`: the container for the flag, configured in its definition.
@@ -94,13 +96,19 @@ struct Cli {
 enum Command {
     /// List all aconfig flags on this device.
     List,
+
+    /// Enable an aconfig flag on this device, on the next boot.
+    Enable { package: String, name: String },
+
+    /// Disable an aconfig flag on this device, on the next boot.
+    Disable { package: String, name: String },
 }
 
 struct PaddingInfo {
     longest_package_col: usize,
     longest_name_col: usize,
     longest_val_col: usize,
-    longest_value_picked_from_col: usize,
+    longest_override_type_col: usize,
     longest_permission_col: usize,
 }
 
@@ -114,15 +122,46 @@ fn format_flag_row(flag: &Flag, info: &PaddingInfo) -> String {
     let val = flag.value.to_string();
     let p2 = info.longest_val_col + 1;
 
-    let value_picked_from = flag.value_picked_from.to_string();
-    let p3 = info.longest_value_picked_from_col + 1;
+    let override_type = flag.override_type.to_string();
+    let p3 = info.longest_override_type_col + 1;
 
     let perm = flag.permission.to_string();
     let p4 = info.longest_permission_col + 1;
 
     let container = &flag.container;
 
-    format!("{pkg:p0$}{name:p1$}{val:p2$}{value_picked_from:p3$}{perm:p4$}{container}\n")
+    format!("{pkg:p0$}{name:p1$}{val:p2$}{override_type:p3$}{perm:p4$}{container}\n")
+}
+
+fn set_flag(package: &str, name: &str, value: &str) -> Result<String> {
+    let flags_binding = DeviceConfigSource::list_flags()?;
+    let flag =
+        flags_binding.iter().find(|f| f.name == name && f.package == package).ok_or(anyhow!(
+            "no aconfig flag '{}/{}'. Does the flag have an .aconfig definition?",
+            package,
+            name
+        ))?;
+
+    if let FlagPermission::ReadOnly = flag.permission {
+        return Err(anyhow!(
+            "could not write flag '{}/{}', it is read-only for the current release configuration.",
+            package,
+            name
+        ));
+    }
+
+    DeviceConfigSource::override_flag(&flag.namespace, package, name, value)?;
+
+    let status_display = match value {
+        "true" => "enabled",
+        "false" => "disabled",
+        _ => return Err(anyhow!("unsupported override type {}", value)),
+    };
+
+    Ok(format!(
+        "Successfully {} flag {}/{}. Reboot the device to apply the change.",
+        status_display, package, name
+    ))
 }
 
 fn list() -> Result<String> {
@@ -131,9 +170,9 @@ fn list() -> Result<String> {
         longest_package_col: flags.iter().map(|f| f.package.len()).max().unwrap_or(0),
         longest_name_col: flags.iter().map(|f| f.name.len()).max().unwrap_or(0),
         longest_val_col: flags.iter().map(|f| f.value.to_string().len()).max().unwrap_or(0),
-        longest_value_picked_from_col: flags
+        longest_override_type_col: flags
             .iter()
-            .map(|f| f.value_picked_from.to_string().len())
+            .map(|f| f.override_type.to_string().len())
             .max()
             .unwrap_or(0),
         longest_permission_col: flags
@@ -155,6 +194,8 @@ fn main() {
     let cli = Cli::parse();
     let output = match cli.command {
         Command::List => list(),
+        Command::Enable { package, name } => set_flag(&package, &name, "true"),
+        Command::Disable { package, name } => set_flag(&package, &name, "false"),
     };
     match output {
         Ok(text) => println!("{text}"),
