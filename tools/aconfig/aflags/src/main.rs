acec9fbc819d246308929381d29d9c9303193eed
Ted Bauer <tedbauer@google.com>
2024-03-04 22:45:29 +0000

    aflags: add initial enable and disable commands
    Bug: 324436145
    Test: adb shell aflags enable
    Change-Id: I6b4af4bccbbc6000a9dcb78c3130f836b0f85bd0

--- tools/aconfig/aflags/src/main.rs
+++ tools/aconfig/aflags/src/main.rs
@@ -16,7 +16,7 @@
 
 //! `aflags` is a device binary to read and write aconfig flags.
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use clap::Parser;
 
 mod device_config_source;
@@ -63,8 +63,15 @@ struct Flag {
     value_picked_from: ValuePickedFrom,
 }
 
+impl Flag {
+    fn qualified_name(&self) -> String {
+        format!("{}.{}", self.package, self.name)
+    }
+}
+
 trait FlagSource {
     fn list_flags() -> Result<Vec<Flag>>;
+    fn override_flag(namespace: &str, qualified_name: &str, value: &str) -> Result<()>;
 }
 
 const ABOUT_TEXT: &str = "Tool for reading and writing flags.
@@ -94,6 +101,18 @@ struct Cli {
 enum Command {
     /// List all aconfig flags on this device.
     List,
+
+    /// Enable an aconfig flag on this device, on the next boot.
+    Enable {
+        /// <package>.<flag_name>
+        qualified_name: String,
+    },
+
+    /// Disable an aconfig flag on this device, on the next boot.
+    Disable {
+        /// <package>.<flag_name>
+        qualified_name: String,
+    },
 }
 
 struct PaddingInfo {
@@ -123,6 +142,33 @@ fn format_flag_row(flag: &Flag, info: &PaddingInfo) -> String {
     let container = &flag.container;
 
     format!("{pkg:p0$}{name:p1$}{val:p2$}{value_picked_from:p3$}{perm:p4$}{container}\n")
+}
+
+fn set_flag(qualified_name: &str, value: &str) -> Result<String> {
+    let flags_binding = DeviceConfigSource::list_flags()?;
+    let flag = flags_binding.iter().find(|f| f.qualified_name() == qualified_name).ok_or(
+        anyhow!("no aconfig flag '{}'. Does the flag have an .aconfig definition?", qualified_name),
+    )?;
+
+    if let FlagPermission::ReadOnly = flag.permission {
+        return Err(anyhow!(
+            "could not write flag '{}', it is read-only for the current release configuration.",
+            qualified_name,
+        ));
+    }
+
+    DeviceConfigSource::override_flag(&flag.namespace, qualified_name, value)?;
+
+    let status_display = match value {
+        "true" => "enabled",
+        "false" => "disabled",
+        _ => return Err(anyhow!("unsupported override type {}", value)),
+    };
+
+    Ok(format!(
+        "{} flag '{}'. Reboot the device to apply the change.",
+        status_display, qualified_name
+    ))
 }
 
 fn list() -> Result<String> {
@@ -155,9 +201,11 @@ fn main() {
     let cli = Cli::parse();
     let output = match cli.command {
         Command::List => list(),
+        Command::Enable { qualified_name } => set_flag(&qualified_name, "true"),
+        Command::Disable { qualified_name } => set_flag(&qualified_name, "false"),
     };
     match output {
-        Ok(text) => println!("{text}"),
+        Ok(text) => println!("Success: {text}"),
         Err(msg) => println!("Error: {}", msg),
     }
 }
