a21e16733b8914976ab6191337406f97fd5640c5
Nelson Li <nelsonli@google.com>
2024-03-07 07:43:35 +0000

    Move frameworks/base/Android.mk to build/core
    1. Move the work of `declare-1p-copy-files` to
      `build/core/tasks/meta-lic.mk`.
    2. Remove `$(LOCAL_PATH)/pathmap.mk` because the variables defined in
       it are no longer used.
    3. The variable `ONE_SHOT_MAKEFILE` is deprecated and can be removed
       directly.
    4. Move the work of `offline-sdk-docs` to
       `build/core/tasks/offline-sdk-docs.mk` because Soong cannot specify
       the output path to `$(OUT_DOCS)/`.
    5. Move the work of `framework-doc-stubs` to `build/core/Makefile`
       because Soong cannot specify the output path to
       `framework-doc-stubs-metadata_intermediates`, and because
       `SDK_METADATA_FILES` is a dependency of the SDK, it must be moved
       before the SDK is built.
    6. `ANDROID_BUILD_EMBEDDED` can be removed because the `.mk` files that
       use this variable are currently not used.
    
    Bug: 322732227
    Test: m sdk docs offline-sdk-docs framework-doc-stubs
    Change-Id: Ieef40808a0bbaa63a73491ddde0949ae1780e615

---
+++ core/tasks/offline-sdk-docs.mk
@@ -0,0 +1,10 @@
+# sdk.atree needs to copy the whole dir: $(OUT_DOCS)/offline-sdk to the final zip.
+# So keep offline-sdk-timestamp target here, and unzip offline-sdk-docs.zip to
+# $(OUT_DOCS)/offline-sdk.
+$(OUT_DOCS)/offline-sdk-timestamp: $(OUT_DOCS)/offline-sdk-docs-docs.zip
+	$(hide) rm -rf $(OUT_DOCS)/offline-sdk
+	$(hide) mkdir -p $(OUT_DOCS)/offline-sdk
+	( unzip -qo $< -d $(OUT_DOCS)/offline-sdk && touch -f $@ ) || exit 1
+
+.PHONY: docs offline-sdk-docs
+docs offline-sdk-docs: $(OUT_DOCS)/offline-sdk-timestamp
