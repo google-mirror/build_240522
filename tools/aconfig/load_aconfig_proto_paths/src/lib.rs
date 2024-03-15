10987810a2a33b74e8ed98bcdb5ba435b094c87b
Ted Bauer <tedbauer@google.com>
2024-03-15 17:04:06 +0000

    aflags: read protos from all containers
    Create one library for reading protos from all containers, instead of
    having numerous libraries perform the same logic. For Java, we will
    create a similar library reusing the same
    partition_aconfig_flags_paths.txt.
    
    Bug: 324436145
    Test: adb shell aflags list # Confirm that various containers appear
    Change-Id: I924e281a50f9a609e1c07c03267eebe3dce52752

---
+++ tools/aconfig/load_aconfig_proto_paths/src/lib.rs
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2024 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+use anyhow::Result;
+use std::path::Path;
+
+use std::fs;
+
+/// Determine all paths that contain an aconfig protobuf file.
+pub fn load_paths() -> Result<Vec<String>> {
+    let mut result: Vec<String> =
+        include!("../partition_aconfig_flags_paths.txt").map(|s| s.to_string()).to_vec();
+    for dir in fs::read_dir("/apex")? {
+        let dir = dir?;
+
+        // Only scan the currently active version of each mainline module; skip the @version dirs.
+        if dir.file_name().as_encoded_bytes().iter().any(|&b| b == b'@') {
+            continue;
+        }
+
+        let path = format!("/apex/{}/etc/aconfig_flags.pb", dir.file_name().to_string_lossy());
+        if Path::new(&path).exists() {
+            result.push(path);
+        }
+    }
+
+    Ok(result)
+}
