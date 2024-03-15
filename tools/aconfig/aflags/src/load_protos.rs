4b4e87185323d3f106262fb03ee0f0cc611ecd14
Ted Bauer <tedbauer@google.com>
2024-03-15 17:04:06 +0000

    aflags: read protos from all containers
    Create one library for reading protos from all containers, instead of
    having numerous libraries perform the same logic. For Java, we will
    create a similar library reusing the same
    partition_aconfig_flags_paths.txt.
    
    Bug: 324436145
    Test: adb shell aflags list # Confirm that various containers appear
    Change-Id: I924e281a50f9a609e1c07c03267eebe3dce52752

---
+++ tools/aconfig/aflags/src/load_protos.rs
@@ -0,0 +1,59 @@
+use crate::{Flag, FlagPermission, FlagValue, ValuePickedFrom};
+use aconfig_protos::ProtoFlagPermission as ProtoPermission;
+use aconfig_protos::ProtoFlagState as ProtoState;
+use aconfig_protos::ProtoParsedFlag;
+use aconfig_protos::ProtoParsedFlags;
+use anyhow::Result;
+use load_aconfig_proto_paths;
+use std::{fs, str};
+
+// TODO(b/329875578): use container field directly instead of inferring.
+fn infer_container(path: &str) -> String {
+    path.strip_prefix("/apex/")
+        .or_else(|| path.strip_prefix('/'))
+        .unwrap_or(path)
+        .strip_suffix("/etc/aconfig_flags.pb")
+        .unwrap_or(path)
+        .to_string()
+}
+
+fn convert_parsed_flag(path: &str, flag: &ProtoParsedFlag) -> Flag {
+    let namespace = flag.namespace().to_string();
+    let package = flag.package().to_string();
+    let name = flag.name().to_string();
+
+    let value = match flag.state() {
+        ProtoState::ENABLED => FlagValue::Enabled,
+        ProtoState::DISABLED => FlagValue::Disabled,
+    };
+
+    let permission = match flag.permission() {
+        ProtoPermission::READ_ONLY => FlagPermission::ReadOnly,
+        ProtoPermission::READ_WRITE => FlagPermission::ReadWrite,
+    };
+
+    Flag {
+        namespace,
+        package,
+        name,
+        container: infer_container(path),
+        value,
+        staged_value: None,
+        permission,
+        value_picked_from: ValuePickedFrom::Default,
+    }
+}
+
+pub(crate) fn load() -> Result<Vec<Flag>> {
+    let mut result = Vec::new();
+
+    let paths = load_aconfig_proto_paths::load_paths()?;
+    for path in paths {
+        let bytes = fs::read(path.clone())?;
+        let parsed_flags: ProtoParsedFlags = protobuf::Message::parse_from_bytes(&bytes)?;
+        for flag in parsed_flags.parsed_flag {
+            result.push(convert_parsed_flag(&path, &flag));
+        }
+    }
+    Ok(result)
+}
