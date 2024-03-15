bf77f2a6fd757d00a8c6945e13c3151ba56b8641
Justin Yun <justinyun@google.com>
2024-03-15 22:50:04 +0900

    Define vendor default header file for versioning macros
    LLNDK as a vendor module may define APIs for a specific vendor
    version. These newly added APIs must be annotated with
    __INTRODUCED_IN_LLNDK.
    
    All vendor modules can use LLNDK APIs. To use the versioned APIs, the
    vendor modules must guard the callers with API_LEVEL_AT_LEAST to call
    the versioned APIs only if they are available.
    
    The default header defined in VENDOR_DEFAULT_LLNDK_VERSIONING_HEADER
    provides these macros and included in all vendor modules by default.
    
    Bug: 302113279
    Test: lunch cf_x86_64-trunk_staging-userdebug && m && m cts
    Change-Id: I258455b7f6a421c685a64de27181a9b4c1233383

--- core/config.mk
+++ core/config.mk
@@ -849,6 +849,9 @@ ifdef PRODUCT_SHIPPING_API_LEVEL
   endif
 endif
 
+# Vendor modules include a default header file for LLNDK versioning macros.
+VENDOR_DEFAULT_LLNDK_VERSIONING_HEADER := system/core/libvendorsupport/include_llndk/android/llndk-versioning.h
+
 # The default key if not set as LOCAL_CERTIFICATE
 ifdef PRODUCT_DEFAULT_DEV_CERTIFICATE
   DEFAULT_SYSTEM_DEV_CERTIFICATE := $(PRODUCT_DEFAULT_DEV_CERTIFICATE)
