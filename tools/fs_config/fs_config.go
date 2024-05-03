65b97da888ffce816c8dc1ffc8859b728b866629
Jiyong Park <jiyong@google.com>
2024-05-03 16:55:32 +0900

    Move fs_config_[files|dirs]_<partition> to soong
    Note that not all partitions are moved to Soong. Soong doesn't yet
    support *_dlkm partitions and the oem partition.
    
    This change also removes dead code for support VNDK snapshot.
    
    Finally, as a minor clean up, the leading '_' is dropped from the module
    names _fs_config_*. There's no need to hide the module names.
    
    Bug: 337993745
    Test: m
    Change-Id: I5beec5c6734291b9361f870fc1a8dba19def062e

--- tools/fs_config/fs_config.go
+++ tools/fs_config/fs_config.go
@@ -16,12 +16,14 @@ package fs_config
 
 import (
 	"android/soong/android"
+	"fmt"
 )
 
 var pctx = android.NewPackageContext("android/soong/fs_config")
 
 func init() {
 	android.RegisterModuleType("target_fs_config_gen_filegroup", targetFSConfigGenFactory)
+	android.RegisterModuleType("fs_config", fsConfigFactory)
 }
 
 // target_fs_config_gen_filegroup is used to expose the files pointed to by TARGET_FS_CONFIG_GEN to
@@ -57,4 +59,108 @@ func (t *targetFSConfigGen) GenerateAndroidBuildActions(ctx android.ModuleContex
 
 func (t *targetFSConfigGen) Srcs() android.Paths {
 	return t.paths
+}
+
+// fs_config is used to create fs_config_dirs or fs_config_files files for the partition where this
+// module is installed to.
+func fsConfigFactory() android.Module {
+	module := &fsConfig{}
+	module.AddProperties(&module.properties)
+	android.InitAndroidArchModule(module, android.DeviceSupported, android.MultilibCommon)
+	return module
+}
+
+type fsConfig struct {
+	android.ModuleBase
+	properties fsConfigProperties
+
+	installDir android.InstallPath
+	outputFile android.OutputPath
+}
+
+type fsConfigProperties struct {
+	// dirs or files
+	Type string
+}
+
+type configType string
+
+const (
+	configTypeDirs    configType = "dirs"
+	configTypeFiles   configType = "files"
+	configTypeInvalid configType = "invalid"
+)
+
+func (c configType) fileName() string {
+	return "fs_config_" + string(c)
+}
+
+func (f *fsConfig) configType(ctx android.ModuleContext) configType {
+	configType := f.properties.Type
+	switch configType {
+	case "dirs":
+		return configTypeDirs
+	case "files":
+		return configTypeFiles
+	default:
+		ctx.PropertyErrorf("type", "should be either dirs or files, but got %s", configType)
+		return configTypeInvalid
+	}
+}
+
+func (f *fsConfig) DepsMutator(ctx android.BottomUpMutatorContext) {
+}
+
+func (f *fsConfig) GenerateAndroidBuildActions(ctx android.ModuleContext) {
+	fsConfigHeader := android.PathForSource(ctx, "system/core/libcutils/include/private/android_filesystem_config.h")
+	linuxCapabilityHeader := android.PathForSource(ctx, "bionic/libc/kernel/uapi/linux/capability.h")
+	configType := f.configType(ctx)
+	partition := f.PartitionTag(ctx.DeviceConfig())
+
+	f.installDir = android.PathForModuleInstall(ctx, "etc")
+	f.outputFile = android.PathForModuleOut(ctx, configType.fileName()).OutputPath
+
+	builder := android.NewRuleBuilder(pctx, ctx)
+	cmd := builder.Command()
+	cmd.BuiltTool("fs_config_generator").
+		Text("fsconfig").
+		FlagWithInput("--aid-header ", fsConfigHeader).
+		FlagWithInput("--capability-header ", linuxCapabilityHeader).
+		FlagWithArg("--partition ", partition)
+	if partition == "system" {
+		cmd.FlagWithArg("--all-partitions ", "vendor,oem,odm,vendor_dlkm,odm_dlkm,system_dlkm")
+	}
+
+	if configType == configTypeFiles {
+		cmd.Flag("--files")
+	} else {
+		cmd.Flag("--dirs")
+	}
+
+	cmd.FlagWithOutput("--out_file ", f.outputFile)
+
+	if ret := ctx.DeviceConfig().TargetFSConfigGen(); len(ret) != 0 {
+		cmd.Inputs(android.PathsForSource(ctx, ret))
+	} else {
+		cmd.Text("/dev/null")
+	}
+
+	builder.Build("fs_config_"+string(configType), fmt.Sprintf("Building %s", f.BaseModuleName()))
+}
+
+var _ android.AndroidMkEntriesProvider = (*fsConfig)(nil)
+
+func (f *fsConfig) AndroidMkEntries() []android.AndroidMkEntries {
+	return []android.AndroidMkEntries{
+		android.AndroidMkEntries{
+			Class:      "ETC",
+			OutputFile: android.OptionalPathForPath(f.outputFile),
+			ExtraEntries: []android.AndroidMkExtraEntriesFunc{
+				func(ctx android.AndroidMkExtraEntriesContext, entries *android.AndroidMkEntries) {
+					entries.SetString("LOCAL_MODULE_PATH", f.installDir.String())
+					entries.SetString("LOCAL_INSTALLED_MODULE_STEM", f.outputFile.Base())
+				},
+			},
+		},
+	}
 }
